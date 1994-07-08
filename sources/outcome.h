bdad433916324d8e7b9869ae5b71fcb5379890a7
magyar <magyar@3fbfd45b-e82e-0410-bb8e-8470640bbd7f>
1994-07-08 17:26:15 +0000

    switched to gMap<double *> to avoid ambiguities

--- sources/outcome.h
+++ sources/outcome.h
@@ -14,12 +14,12 @@
 class Outcome    {
   private:
     gString name;
-    gMap<double> values;
+    gMap<double *> values;
 
   public:
     Outcome(void)   { }
     Outcome(const Outcome &);
-    ~Outcome()  { }
+    ~Outcome();
 
     Outcome &operator=(const Outcome &);
     void SetOutcomeName(const gString &s)  {name = s;}
@@ -40,51 +40,6 @@ class Outcome    {
     void SetOutcome(const gVector<double> &v);
 };
 
-inline Outcome::Outcome(const Outcome &o)
-{
-  name = o.name;
-  values = o.values;
-}
-
-inline Outcome &Outcome::operator=(const Outcome &o)
-{
-  name = o.name;
-  values = o.values;
-  return *this;
-}
-
-inline gVector<double>
-Outcome::GetOutcomeVector(int num_players) const
-{
-  gVector<double> outVect(1, num_players);
-  for (uint i = 1; i <= num_players; i++)
-    outVect[i] = (*this)[i];
-  return outVect;
-}
-
-inline double Outcome::operator[](int player) const
-{
-  if (values.Contains(player)) 
-    return values[player];
-
-  return 0;  // If no value found for that player
-  
-}
-
-inline void Outcome::SetOutcome(int player, double value) 
-{
-  if (value != 0)
-    values.Insert(value, player);
-  else  if (values.Contains(player)) values.Remove(player);
-}
-
-inline void Outcome::SetOutcome(const gVector<double> &vals)
-{
-  for (uint i = 1; i <= vals.Length(); i++)
-    if (vals[i] != 0) 
-      values.Insert(vals[i], i);
-    else if (values.Contains(i))
-      values.Remove(i);
-}
   
 #endif    // OUTCOME_H
+
