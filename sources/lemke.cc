00cb24ab14306e331c7cbee333f58f4f6bdee5f7
rdm <rdm@3fbfd45b-e82e-0410-bb8e-8470640bbd7f>
1994-09-26 12:18:00 +0000

    initialized 0 entries in tableau

--- sources/lemke.cc
+++ sources/lemke.cc
@@ -2,8 +2,8 @@
 #include "normal.h"
 #include "gtableau.h"
 #include "gambitio.h" 
-#include "rational.h"
-//#define Rational double
+//#include "rational.h"
+#define Rational double
 
 NormalForm::Lemke(int dup_strat)
 {
@@ -30,6 +30,12 @@ NormalForm::Lemke(int dup_strat)
     }
   }
   
+  for(i=1;i<=n1;i++) { 
+    for(j=1;j<=n1;j++) {T.Entry(i,j)=(Rational)0;}
+
+  for(i=n1+1;i<=n;i++) { 
+    for(j=n1+1;j<=n;j++) {T.Entry(i,j)=(Rational)0;}
+
   for(i=1;i<=n1;i++) { 
     for(j=1;j<=n2;j++) {
       s.SetStrategy(strategies[1]->GetStrategy(i));
