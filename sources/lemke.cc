97047ba5b8717ed62a525b02fb8b0367ee2587cf
magyar <magyar@3fbfd45b-e82e-0410-bb8e-8470640bbd7f>
1994-09-26 12:33:04 +0000

    Fixed typo causing compilation errors

--- sources/lemke.cc
+++ sources/lemke.cc
@@ -29,12 +29,13 @@ NormalForm::Lemke(int dup_strat)
       if(x<min)min=x;
     }
   }
-  
-  for(i=1;i<=n1;i++) { 
+
+  for(i=1;i<=n1;i++) 
     for(j=1;j<=n1;j++) {T.Entry(i,j)=(Rational)0;}
 
-  for(i=n1+1;i<=n;i++) { 
+  for(i=n1+1;i<=n;i++)  
     for(j=n1+1;j<=n;j++) {T.Entry(i,j)=(Rational)0;}
+
 
   for(i=1;i<=n1;i++) { 
     for(j=1;j<=n2;j++) {
