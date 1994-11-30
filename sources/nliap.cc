7dc33f4ffb59e287cec22f62a3f4f144797118ef
rdm <rdm@3fbfd45b-e82e-0410-bb8e-8470640bbd7f>
1994-11-30 16:05:04 +0000

    temp version that compiles but doesn't work

--- sources/nliap.cc
+++ sources/nliap.cc
@@ -1,12 +1,15 @@
 //#
 //# FILE: liap.cc -- Liapunov module
 //#
-//# $Id$
+//# @(#)liap.cc	1.7 11/10/94
 //#
 
+#define MAXIT 10
+#define TOL (T)(.00000001)
+
+#include <stdlib.h>
 #include "gambitio.h"
 #include "normal.h"
-#include "nfrep.h"
 #include "normiter.h"
 #include "rational.h"
 #include "mixed.h"
@@ -25,14 +28,16 @@ public:
 template <class T> class LiapModule 
 : public gBFunct_nDim<T>, public BaseLiap, public SolutionModule {
 private:
-  const NFRep<T> &rep;
+  const NormalForm<T> &rep;
 public:
-  LiapModule(const NFRep<T> &N,gOutput &ofile,gOutput &efile,int plev) 
+  LiapModule(const NormalForm<T> &N,gOutput &ofile,gOutput &efile,int plev) 
     :  SolutionModule(ofile,efile,plev),rep(N),
   gBFunct_nDim<T>(N.ProfileLength()){ SetPlev(plev);}
   int Liap(int) ;
   virtual ~LiapModule() {}
-  T operator()(const gVector<T> &x) const;
+
+  T LiapValue(const MixedProfile<T> &p) const;
+  T operator()(const gVector<T> &x);
   int Liap(MixedProfile<T> &p);
   int Nevals(void) {return nevals;}
   int Nits(void) {return nits;}
@@ -43,27 +48,33 @@ public:
 template <class T> int LiapModule<T>::Liap(int number)
 {
   int n=0;
+  T tmp,sum;
   MixedProfile<T> p(rep.Centroid());
-  n+=Liap(p);
-/*
-  for(int i=1;i<number;i++)
-    {
-      p.Randomize();
-      n+=Liap(p);
+  n+=(Liap(p));
+  gout << " n = " << n;
+  int it = 0;
+  while(n<number && it < MAXIT) {
+    it++;
+    for(int i=1;i<rep.NumPlayers();i++) {
+      sum = (T)(0);
+      for(int j=1;j<rep.NumStrats(i);j++) {
+	tmp=(T)(2);
+	while (tmp + sum > ((T)(1)) ) tmp = (T)((T)rand()/(T)INT_MAX);
+	p[i][j]=tmp;
+	sum+= tmp;
+      }
+      p[i][j]=(T)(1)-sum;
     }
-  */
+    n+=(Liap(p));
+  }
   return n;
 };
 
 template <class T> int LiapModule<T>::Liap(MixedProfile<T> &p)
 {
-// Ted -- we need a constructor in gvector to construct v from p,
-// so we can replace all of this with dVector v(p);
-// I wasn't sure where it would belong, since we don't want to include
-// mixed.h in gvector.h
-  
   gVector<T> v(rep.ProfileLength());
   int k=1;
+
   
   for(int i=1;i<=rep.NumPlayers();i++)
     {
@@ -74,21 +85,20 @@ template <class T> int LiapModule<T>::Liap(MixedProfile<T> &p)
     }
   if(plev>=3)
     gout << "\nv= " << v;
-  MinPowell(v);
-  gout << "\nv= " << v;
-  return 1;
+  T val; 
+  val = MinPowell(v);
+  gout << "\nv= " << v << " f = " << val;
+  if(val < (T) ( (T)(1) / (T)(100000) ) ) return 1;
+  return 0;
 };
 
 template <class T>  
-T LiapModule<T>::operator()(const gVector<T> &x) const
+T LiapModule<T>::operator()(const gVector<T> &x)
 {
   assert(x.Length()==rep.ProfileLength());
 
-//  gout << "\nloc 7: x= " << x;
-  
   MixedProfile<T> m(rep);
-  
-//  gout << "\nloc 8: x= " << x;
+
   int k=1;
   for(int i=1;i<=rep.NumPlayers();i++)
     {
@@ -98,8 +108,7 @@ T LiapModule<T>::operator()(const gVector<T> &x) const
 	k++;
       }
     }
-//  gout << "\nloc 9: m= " << m;
-  return rep.LiapValue(m);
+  return LiapValue(m);
   
 };
 
@@ -107,7 +116,7 @@ T LiapModule<T>::operator()(const gVector<T> &x) const
 #define BIG2 ((T) 100)
 
 template <class T>  
-T NFRep<T>::LiapValue(const MixedProfile<T> &p) const
+T LiapModule<T>::LiapValue(const MixedProfile<T> &p) const
 {
   int i,j,num;
   MixedProfile<T> tmp(p);
@@ -115,16 +124,16 @@ T NFRep<T>::LiapValue(const MixedProfile<T> &p) const
   T x,result,avg,sum;
   
   result= (T) 0;
-  for(i=1;i<=NumPlayers();i++) {
-    payoff = gVector<T>(NumStrats(i));
+  for(i=1;i<=rep.NumPlayers();i++) {
+    payoff = gVector<T>(rep.NumStrats(i));
     payoff=(T) 0;
     tmp[i]=(T) 0;
     avg=sum= (T) 0;
 	// then for each strategy for that player set it to 1 and evaluate
-    for(j = 1; j <= NumStrats(i); j++) {
+    for(j = 1; j <= rep.NumStrats(i); j++) {
       tmp[i][j]= (T) 1;
       x=p[i][j];
-      payoff[j] = Payoff(i,tmp);
+      payoff[j] = rep.Payoff(i,tmp);
 //      gout << "\np[" << i << "][" << j << "] = " << payoff[j];
 //      gout << "\ntmp = " << tmp;
       avg+=x*payoff[j];
@@ -134,7 +143,7 @@ T NFRep<T>::LiapValue(const MixedProfile<T> &p) const
       tmp[i][j]= (T) 0;
     }
     tmp[i]=p[i];
-    for(j=1;j<=NumStrats(i);j++) {
+    for(j=1;j<=rep.NumStrats(i);j++) {
       x=payoff[j]-avg;
       x = (x > 0 ? x : 0);
       result += x*x;          // add penalty if not best response
@@ -146,10 +155,13 @@ T NFRep<T>::LiapValue(const MixedProfile<T> &p) const
 }
 
 int LiapSolver::Liap(void)
+//int LiapModule::Liap(int number, int plev, gOutput &out, gOutput &err,
+//		   int &nevals, int &nits)
 {
   BaseLiap *T;
   gOutput *outfile = &gout, *errfile = &gerr;
 
+  gout << "\nLiapSolver::Liap() Loc 1";
   if (params.outfile != "")
     outfile = new gFileOutput((char *) params.outfile);
   if (params.errfile != "" && params.errfile != params.outfile)
@@ -157,28 +169,33 @@ int LiapSolver::Liap(void)
   if (params.errfile != "" && params.errfile == params.outfile)
     errfile = outfile;
 
-  switch (Type())  {
+  gout << "\nLiapSolver::Liap() Loc 2";
+  switch (nf.Type())   {
     case DOUBLE:   
-      T = new LiapModule<double>((NFRep<double> &) *GetRep(), *outfile,
-			         *errfile, params.plev);
+      T = new LiapModule<double>((NormalForm<double> &) nf, 
+				 *outfile, *errfile, params.plev);
       break;
 /*
     case RATIONAL:  
-      T = new LiapModule<Rational>((NFRep<Rational> &) *GetRep(),
-				   *outfile, *errfile, params.plev);
+      T = new LiapModule<Rational>((NFRep<Rational> &) *data, out, err, plev);
       break; 
 */
   }
+  gout << "\nLiapSolver::Liap() Loc 3";
+
   T->Liap(params.number);
-  nits = T->Nits();
-  nevals = T->Nevals();
+  gout << "\nLiapSolver::Liap() Loc 4";
+  nits=T->Nits();
+  nevals= T->Nevals();
 
   if (params.outfile != "")
     delete outfile;
   if (params.errfile != "" && params.errfile != params.outfile)
     delete errfile;
-  
+
+  delete T;
   return 1;
+
 }
 
 
