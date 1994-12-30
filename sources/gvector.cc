997060233e3fc5ff6b7e62342b2469583b44e10a
bruce <bruce@3fbfd45b-e82e-0410-bb8e-8470640bbd7f>
1994-12-30 19:23:03 +0000

    Initial revision

---
+++ sources/gvector.cc
@@ -0,0 +1,36 @@
+//#
+//# FILE: gvector.cc -- Instantiation of vector types
+//#
+//# $Id$
+//#
+
+#ifdef __GNUG__
+// this pragma is not necessary with g++ 2.6.3 /w -fno-implicit-templates
+// #pragma implementation "gmatrix.h"
+#elif defined(__BORLANDC__)
+#pragma option -Jgd
+#else
+#error Unsupported compiler type.
+#endif   // __GNUG__, __BORLANDC__
+
+#include "gvector.imp"
+#include "rational.h"
+
+
+#ifdef __GNUG__
+// explicitly instantiate classes and non-member functions
+template class gVector<int>;
+template class gVector<double>;
+template class gVector<gInteger>;
+template class gVector<gRational>;
+
+template gOutput & operator<< (gOutput&, const gVector<int>&);
+template gOutput & operator<< (gOutput&, const gVector<double>&);
+template gOutput & operator<< (gOutput&, const gVector<gInteger>&);
+template gOutput & operator<< (gOutput&, const gVector<gRational>&);
+#elif defined(__BORLANDC__)
+// whatever
+#error Explicit instantiation needs to be done for Borland C++
+#else
+#error Unsupported compiler type.
+#endif
