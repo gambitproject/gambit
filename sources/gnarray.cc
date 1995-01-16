1e8caa682829932a7f4e5815a20467e77b245979
magyar <magyar@3fbfd45b-e82e-0410-bb8e-8470640bbd7f>
1995-01-16 16:37:24 +0000

    Changes to satisfy BC

--- sources/gnarray.cc
+++ sources/gnarray.cc
@@ -7,8 +7,15 @@
 #include "gnarray.imp"
 #include "rational.h"
 
-template class gNArray<double>;
-template class gNArray<gRational>;
+#ifdef __GNUG__
+#define TEMPLATE template
+#elif defined __BORLANDC__
+#define TEMPLATE
+#pragma option -Jgd
+#endif   // __GNUG__, __BORLANDC__
+
+TEMPLATE class gNArray<double>;
+TEMPLATE class gNArray<gRational>;
 
 
 
