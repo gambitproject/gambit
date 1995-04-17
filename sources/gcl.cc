0f2cd61bb284bcd9d52fa2d8a242a0351df4a4d1
magyar <magyar@3fbfd45b-e82e-0410-bb8e-8470640bbd7f>
1995-04-17 14:58:30 +0000

    Initial revision

---
+++ sources/gcl.cc
@@ -0,0 +1,22 @@
+#include "rational.h"
+#include "gstring.h"
+#include "glist.h"
+#include "gsm.h"
+#include "gcompile.h"
+
+main()
+{
+  char buffer[256];
+  int command = 1;
+
+  GCLCompiler C;
+  
+  while (1)   {
+    strcpy(buffer, "");
+    gout << "GCL" << command++ << ": ";
+    fgets(buffer, 255, stdin);
+    if (feof(stdin))   break;
+    C.Parse(buffer);
+  }
+}
+
