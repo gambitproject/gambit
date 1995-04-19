9849001dfb23d2cee13dcb5277e37624711b73e0
magyar <magyar@3fbfd45b-e82e-0410-bb8e-8470640bbd7f>
1995-04-19 15:37:43 +0000

    Initial revision

---
+++ sources/garray.cc
@@ -0,0 +1,38 @@
+//#
+//# FILE: garray.cc -- Instantiations for common gArray classes
+//#
+//# $Id$
+//#
+
+#include "garray.imp"
+#include "basic.h"
+#include "gstring.h"
+#include "rational.h"
+
+#ifdef __GNUG__
+#define TEMPLATE template
+#elif defined __BORLANDC__
+#pragma option -Jgd
+#define TEMPLATE
+#endif   // __GNUG__, __BORLANDC__
+
+
+TEMPLATE class gArray<bool>;
+TEMPLATE class gArray<int>;
+TEMPLATE class gArray<long>;
+TEMPLATE class gArray<float>;
+TEMPLATE class gArray<double>;
+TEMPLATE class gArray<gRational>;
+TEMPLATE class gArray<gString>;
+TEMPLATE class gArray<gArray<int> >;
+TEMPLATE class gArray<gArray<double> >;
+
+TEMPLATE gOutput &operator<<(gOutput &, const gArray<bool> &);
+TEMPLATE gOutput &operator<<(gOutput &, const gArray<int> &);
+TEMPLATE gOutput &operator<<(gOutput &, const gArray<long> &);
+TEMPLATE gOutput &operator<<(gOutput &, const gArray<float> &);
+TEMPLATE gOutput &operator<<(gOutput &, const gArray<double> &);
+TEMPLATE gOutput &operator<<(gOutput &, const gArray<gRational> &);
+TEMPLATE gOutput &operator<<(gOutput &, const gArray<gString> &);
+TEMPLATE gOutput &operator<<(gOutput &, const gArray<gArray<int> > &);
+TEMPLATE gOutput &operator<<(gOutput &, const gArray<gArray<double> > &);
---
+++ sources/garray.h
@@ -0,0 +1,56 @@
+//#
+//# FILE: garray.h -- A basic bounds-checked array type
+//#	
+//# $Id$
+//#
+
+#ifndef GARRAY_H
+#define GARRAY_H
+
+#include <stdlib.h>
+#include <assert.h>
+#include "gambitio.h"
+
+template <class T> class gArray  {
+ private:
+  int length;
+  T *data;
+
+ public:
+//
+// Constructs a gArray of length 'len'
+//
+  gArray(int len = 0);
+//
+// duplicate the input gArray<T> constant referrence
+//
+  gArray(const gArray<T> &);
+//
+// Destruct and deallocates gArray
+//
+  ~gArray();
+//
+// Copies data from input gArray
+//
+  gArray<T> &operator=(const gArray<T> &);
+
+//
+// return length in the invoking gArray<T>
+//
+  int Length(void) const;
+
+	
+//
+// Check bounds of index. Exit program if out of bounds
+//
+  const T &operator[](int index) const;
+  T &operator[](int index);
+//
+// Output data of the array
+//
+  virtual void Dump(gOutput &) const;
+};
+
+template <class T> gOutput &operator<<(gOutput &, const gArray<T> &);
+
+#endif	//# GARRAY_H
