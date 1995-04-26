1b51b9ca7e80439bd8ebe1cf22bc1ce13250bac6
magyar <magyar@3fbfd45b-e82e-0410-bb8e-8470640bbd7f>
1995-04-26 16:46:56 +0000

    Added DisplayNfg[]

--- sources/nfgfunc.cc
+++ sources/nfgfunc.cc
@@ -6,6 +6,13 @@
 
 #include "gsm.h"
 
+Portion *GSM_DisplayNfg(Portion **param)
+{
+  BaseNormalForm *N = &((Nfg_Portion *) param[0])->Value();
+
+  N->DisplayNfg(gout);
+  return new Nfg_Portion(*N);
+}
 
 Portion *GSM_ReadNfgFile(Portion **param)
 {
@@ -23,7 +30,12 @@ void Init_nfgfunc(GSM *gsm)
 {
   FuncDescObj *FuncObj;
 
+  FuncObj = new FuncDescObj("DisplayNfg", GSM_DisplayNfg, 1);
+  FuncObj->SetParamInfo(0, "N", porNFG, NO_DEFAULT_VALUE);
+  gsm->AddFunction(FuncObj);
+
   FuncObj = new FuncDescObj("ReadNfgFile", GSM_ReadNfgFile, 1);
   FuncObj->SetParamInfo(0, "file", porSTRING, NO_DEFAULT_VALUE);
   gsm->AddFunction(FuncObj);
 }
+
