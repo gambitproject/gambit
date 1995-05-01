ac9f55204c7276e0855d10e9cdf16fd83d370ad9
egrayver <egrayver@3fbfd45b-e82e-0410-bb8e-8470640bbd7f>
1995-05-01 17:40:16 +0000

    Initial revision

---
+++ sources/efgnfgi.cc
@@ -0,0 +1,13 @@
+#include "efgnfgi.h"
+
+#ifdef __GNUG__
+#define TEMPLATE template
+#elif defined __BORLANDC__
+#pragma option -Jgd
+#define TEMPLATE
+#endif   // __GNUG__, __BORLANDC__
+TEMPLATE class EfgNfgInterface<double> ;
+TEMPLATE class EfgNfgInterface<gRational> ;
+#ifdef __BORLANDC__
+#pragma -Jgx
+#endif
