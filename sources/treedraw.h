e9d5fbafaf890f8d92e50f0185a7987d76c19531
egrayver <egrayver@3fbfd45b-e82e-0410-bb8e-8470640bbd7f>
1995-05-01 17:54:48 +0000

    *** empty log message ***

--- sources/treedraw.h
+++ sources/treedraw.h
@@ -1,10 +1,14 @@
 #ifndef TREEDRAW_H
 #define TREEDRAW_H
+#include "gblock.h"
 #include "treecons.h"
-class TreeDrawParams   {
+#include "gambdraw.h"
+
+class TreeDrawSettings:public GambitDrawSettings
+{
 	private:
 	// These are user-definable
-  // Size info for different elements of the tree
+	// Size info for different elements of the tree
 		int branch_length, node_length, y_spacing,x_origin, y_origin;
 		int outcome_length;
   // Colors
@@ -20,74 +24,74 @@ class TreeDrawParams   {
 		Bool	flashing_cursor;
 	// Zoom mode;
 		float zoom_factor;
-  // These are used internally
+	// These are used internally
 		int x_scroll,y_scroll,xsteps,ysteps;
-		typedef struct {int pl;int cl;} player_colors_struct;
-		wxList *player_colors;
-  public:
-    TreeDrawParams(void);
-
+	// Private functions
+		typedef struct {int what_font;TreeDrawSettings *draw_settings;} draw_params_legend_struct;
+		static void draw_params_legends_func(wxButton &ob,wxCommandEvent &ev);
+	public:
+	// Constructor
+		TreeDrawSettings(void);
+	// Control the coordinates of upper left corner
 		void 	SetXOrigin(int xo)        { x_origin = xo; }
-    int 	XOrigin(void) const        { return x_origin; }
-
-    void 	SetYOrigin(int yo)        { y_origin = yo; }
-    int 	YOrigin(void) const        { return y_origin; }
-
-		int 	BranchLength(void) 			   { return branch_length; }
-    void 	SetBranchLength(int _l)	 { branch_length=_l;}		
-		int 	NodeLength(void) 			     { return node_length; }
-    void 	SetNodeLength(int _l)		 { node_length=_l;}
-		int 	YSpacing(void) 			       { return y_spacing; }
-    void 	SetYSpacing(int _l)			 { y_spacing=_l;}
-		int 	CursorColor(void) 			   { return cursor_color; }
-    void 	SetCursorColor(int _cl)	 { cursor_color=_cl;}
-
-		void	SetShowInfosets(Bool _s) {show_infosets=_s;}
+		int 	XOrigin(void) const       { return x_origin; }
+		void 	SetYOrigin(int yo)        { y_origin = yo; }
+		int 	YOrigin(void) const       { return y_origin; }
+	// Size of the various tree parts
+		int 	BranchLength(void) 			  { return branch_length; }
+		void 	SetBranchLength(int l)	 	{ branch_length=l;}
+		int 	NodeLength(void) 			    { return node_length; }
+		void 	SetNodeLength(int l)		 	{ node_length=l;}
+		int 	YSpacing(void) 			      { return y_spacing; }
+		void 	SetYSpacing(int l)			 	{ y_spacing=l;}
+	// Controls the display of infoset lines (turn of to speed up)
+		void	SetShowInfosets(Bool _s) 	{show_infosets=_s;}
 		int		ShowInfosets(void) {return show_infosets;}
-		void	SetOutcomeLength(int _l) {outcome_length=_l;}
-		int		OutcomeLength(void) {return outcome_length;}
-		void	SetPlayerColor(int pl,int cl);
-		int		GetPlayerColor(int pl);
-
-		void	SetLabelNodeAbove(int _l)	{node_above_label=_l;}
+		void	SetOutcomeLength(int l) 	{outcome_length=l;}
+		int		OutcomeLength(void) 			{return outcome_length;}
+	// Legends
+		void	SetLabelNodeAbove(int l)	{node_above_label=l;}
 		int		LabelNodeAbove(void)			{return node_above_label;}
-		void	SetLabelNodeBelow(int _l)	{node_below_label=_l;}
+		void	SetLabelNodeBelow(int l)	{node_below_label=l;}
 		int		LabelNodeBelow(void)			{return node_below_label;}
-		void	SetLabelBranchAbove(int _l)	{branch_above_label=_l;}
+		void	SetLabelBranchAbove(int l)	{branch_above_label=l;}
 		int		LabelBranchAbove(void)			{return branch_above_label;}
-		void	SetLabelBranchBelow(int _l)	{branch_below_label=_l;}
+		void	SetLabelBranchBelow(int l)	{branch_below_label=l;}
 		int		LabelBranchBelow(void)			{return branch_below_label;}
-		void	SetLabelNodeTerminal(int _l) {node_terminal_label=_l;}
+		void	SetLabelNodeTerminal(int l) {node_terminal_label=l;}
 		int		LabelNodeTerminal(void) {return node_terminal_label;}
-
-		void		SetNodeAboveFont(wxFont *_f)
-			{if (node_above_font) delete node_above_font;node_above_font=_f;}
+	// Controls the fonts
+		void		SetNodeAboveFont(wxFont *f)
+			{if (node_above_font) delete node_above_font;node_above_font=f;}
 		wxFont 	*NodeAboveFont(void) {return node_above_font;}
-		void		SetNodeBelowFont(wxFont *_f)
-			{if (node_below_font) delete node_below_font;node_below_font=_f;}
+		void		SetNodeBelowFont(wxFont *f)
+			{if (node_below_font) delete node_below_font;node_below_font=f;}
 		wxFont 	*NodeBelowFont(void) {return node_below_font;}
-		void		SetBranchAboveFont(wxFont *_f)
-			{if (branch_above_font) delete branch_above_font;branch_above_font=_f;}
+		void		SetBranchAboveFont(wxFont *f)
+			{if (branch_above_font) delete branch_above_font;branch_above_font=f;}
 		wxFont 	*BranchAboveFont(void) {return branch_above_font;}
-		void		SetBranchBelowFont(wxFont *_f)
-			{if (branch_below_font) delete branch_below_font;branch_below_font=_f;}
+		void		SetBranchBelowFont(wxFont *f)
+			{if (branch_below_font) delete branch_below_font;branch_below_font=f;}
 		wxFont 	*BranchBelowFont(void) {return branch_below_font;}
-		void		SetNodeTerminalFont(wxFont *_f)
-			{if (node_terminal_font) delete node_terminal_font; node_terminal_font=_f;}
+		void		SetNodeTerminalFont(wxFont *f)
+			{if (node_terminal_font) delete node_terminal_font; node_terminal_font=f;}
 		wxFont	*NodeTerminalFont(void) {return node_terminal_font;}
-
-		void		SetFlashingCursor(Bool _f)	{flashing_cursor=_f;}
+	// Controls the cursor style [flashing or non-flashing]
+		void		SetFlashingCursor(Bool f)	{flashing_cursor=f;}
 		Bool		FlashingCursor(void) {return flashing_cursor;}
-
-		void		SetZoom(float _z);
+	// Control the color of the cursor
+		int 	CursorColor(void) 			  { return cursor_color; }
+		void 	SetCursorColor(int _cl)	 	{ cursor_color=_cl;}
+	// Set the zoom
+		void		SetZoom(float z);
 		float   Zoom(void)				{return zoom_factor;}
 
-		// Functions to set the various options
+	// Functions to set the various options
 		void	SetOptions(void);
 		void	SetLegends(void);
-		void	SaveOptions(char *f=NULL);	// pass a filename to read from that file
-		void	LoadOptions(char *f=NULL);
-		// Functions for internal use by gambit
+		void	SaveOptions(char *f=0);
+		void	LoadOptions(char *f);
+	// Functions for internal use by gambit
 		void set_x_scroll(int xs) {x_scroll=xs;}
 		int get_x_scroll(void) {return x_scroll;}
 		void set_y_scroll(int ys) {y_scroll=ys;}
