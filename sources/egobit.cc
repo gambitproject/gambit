9da5f070ca1c7d76f2eb61e72fa3190e7279ab71
magyar <magyar@3fbfd45b-e82e-0410-bb8e-8470640bbd7f>
1995-06-01 11:16:36 +0000

    Now uses truncated profiles

--- sources/egobit.cc
+++ sources/egobit.cc
@@ -10,6 +10,10 @@
 #include "gdpvect.h"
 #include "gmatrix.h"
 
+#include "behav.h"
+#include "player.h"
+#include "infoset.h"
+
 //-------------------------------------------------------------------------
 //                     EFGobitParams<T>: Member functions
 //-------------------------------------------------------------------------
@@ -31,7 +35,7 @@ class EFGobitFunc : public GobitFunc<T>, public gBFunctMin<T>  {
   private:
     T Lambda;
     gPVector<T> probs;
-    gDPVector<T> p, pp, cpay;
+    BehavProfile<T> p, pp, cpay;
     gMatrix<T> xi;
     const ExtForm<T> &E;
     
@@ -41,13 +45,13 @@ class EFGobitFunc : public GobitFunc<T>, public gBFunctMin<T>  {
   public:
     EFGobitFunc(const ExtForm<T> &EF, const GobitParams<T> &P);
     EFGobitFunc(const ExtForm<T> &EF, const GobitParams<T> &P,
-	      const gDPVector<T> &s);
+	      const BehavProfile<T> &s);
     virtual ~EFGobitFunc();
 
     void Init(void);
     // These two are inherited virtual functions from GobitFunc
     void Optimize(T Lambda, int &iter, T &value);
-    void Output(gOutput &f,int format) const;
+    void Output(gOutput &f, int format = 0) const;
 };
 
 //-------------------------------------------------------------------------
@@ -56,9 +60,9 @@ class EFGobitFunc : public GobitFunc<T>, public gBFunctMin<T>  {
 
 template <class T>
 EFGobitFunc<T>::EFGobitFunc(const ExtForm<T> &EF, const GobitParams<T> &P)
-  :gBFunctMin<T>(EF.ProfileLength(),P.tolOpt,P.maxitsOpt,
-		 P.tolBrent,P.maxitsBrent), E(EF), p(EF.Dimensionality()),
-		 pp(EF.Dimensionality()), cpay(EF.Dimensionality()),
+  :gBFunctMin<T>(EF.ProfileLength(true), P.tolOpt, P.maxitsOpt,
+		 P.tolBrent,P.maxitsBrent), E(EF),
+		 p(EF, true), pp(EF, true), cpay(EF),
 		 probs(EF.Dimensionality().Lengths()),
 		 xi(p.Length(), p.Length())
 {
@@ -69,10 +73,10 @@ EFGobitFunc<T>::EFGobitFunc(const ExtForm<T> &EF, const GobitParams<T> &P)
 
 template <class T>
 EFGobitFunc<T>::EFGobitFunc(const ExtForm<T> &EF, const GobitParams<T> &P,
-			  const gDPVector<T> &s)
-  :gBFunctMin<T>(EF.ProfileLength(),P.tolOpt,P.maxitsOpt,
-		 P.tolBrent,P.maxitsBrent), E(EF), p(EF.Dimensionality()),
-		 pp(EF.Dimensionality()), cpay(EF.Dimensionality()),
+			    const BehavProfile<T> &s)
+  :gBFunctMin<T>(EF.ProfileLength(true), P.tolOpt, P.maxitsOpt,
+		 P.tolBrent,P.maxitsBrent), E(EF),
+		 p(EF, true), pp(EF, true), cpay(EF),
 		 probs(EF.Dimensionality().Lengths()),
 		 xi(p.Length(), p.Length())
 {
@@ -100,29 +104,26 @@ template <class T> T EFGobitFunc<T>::Value(const gVector<T> &v)
   static const T PENALTY1 = (T) 10000.0;
   static const T PENALTY2 = (T) 100.0;
 
-  p = v;
+  (gVector<T> &) p = v;
   T val((T) 0), prob, psum, z;
 
-  E.CondPayoff(p, cpay,probs);
+  E.CondPayoff(p, cpay, probs);
 
-//  gout << "\np = " << p;
-//  gout << "\ncpay = " << cpay;
-  
   for (int pl = 1; pl <= E.NumPlayers(); pl++)  {
-    for (int iset = 1; iset <= E.NumInfosets(1, pl); iset++)  {
+    Player *player = E.PlayerList()[pl];
+    for (int iset = 1; iset <= player->NumInfosets(); iset++)  {
       prob = (T) 0;
       psum = (T) 0;
       
-      for (int act = 1; act <= E.NumActions(1, pl, iset); act++)  {
+      Infoset *s = player->InfosetList()[iset];
+      for (int act = 1; act <= s->NumActions(); act++)  {
 	z = Lambda * cpay(pl, iset, act);
-//	z = probs(pl,iset) * Lambda * cpay(pl, iset, act);
-//	assert(z < (T) 600);
 	z = exp(z);
 	psum += z;
 	cpay(pl, iset, act) = z;
       }
       
-      for (act = 1; act <= E.NumActions(1, pl, iset); act++)  {
+      for (act = 1; act < s->NumActions(); act++)  {
 	z = p(pl, iset, act);
 	prob += z;
 	if (z < (T) 0)
@@ -132,7 +133,14 @@ template <class T> T EFGobitFunc<T>::Value(const gVector<T> &v)
       }
       
       z = (T) 1 - prob;
-      val += PENALTY2 * z * z;
+      if (z < (T) 0)
+	val += PENALTY1 * z * z;
+      z -= cpay(pl, iset, act) / psum;
+      val += z * z;
+
+/*  This is obsoleted by the constraint that sum(prob) = 1  */
+/*      z = (T) 1 - prob;
+      val += PENALTY2 * z * z;  */
     }
   }
 
@@ -150,35 +158,36 @@ template <class T> void EFGobitFunc<T>::Optimize(T Lam, int &iter, T &value)
   Powell(pp, xi, iter, value);
 }
 
-template <class T> void EFGobitFunc<T>
-::Output(gOutput &f, int format = 0) const
+template <class T> void EFGobitFunc<T>::Output(gOutput &f, int format) const
 {
-  int pl, iset, act,nisets;
+  int pl, iset, act, nisets;
 
       // Header information
-  if(format==3) {
+  if (format==3) {
     nisets=0;
     for (pl = 1; pl <= E.NumPlayers(); pl++)  
-      for (iset = 1; iset <= E.NumInfosets(1, pl); iset++)
-	nisets++;
+      nisets += E.PlayerList()[pl]->NumInfosets();
     f << nisets;
     for (pl = 1; pl <= E.NumPlayers(); pl++)  
-      for (iset = 1; iset <= E.NumInfosets(1, pl); iset++)  
-	f << " " << E.NumActions(1,pl,iset);
+      for (iset = 1; iset <= E.PlayerList()[pl]->NumInfosets(); iset++)  
+	f << " " << E.PlayerList()[pl]->InfosetList()[iset]->NumActions();
     f << "\n";
   }
-  else if(format==2) {
+  else if (format==2) {
     int numcols = 2+E.ProfileLength();
     f << "\n" << numcols;
-    for(int i=1;i<=numcols;i++) f << " " << i;
+    for(int i=1;i<=numcols;i++) f << ' ' << i;
   }
       // PXI output
-  else if(format==1) {
+  else if (format==1) {
     f<< " ";
     for (pl = 1; pl <= E.NumPlayers(); pl++)  
-      for (iset = 1; iset <= E.NumInfosets(1, pl); iset++)  
-	for(act = 1;act <= E.NumActions(1,pl,iset);act++)
-	  f << pp(pl,iset,act) << " ";
+      for (iset = 1; iset <= E.PlayerList()[pl]->NumInfosets(); iset++)  { 
+	T prob = (T) 0.0;
+	for (act = 1; act < E.PlayerList()[pl]->InfosetList()[iset]->NumActions(); prob += pp(pl, iset, act++))
+	  f << pp(pl,iset,act) << ' ';
+	f << ((T) 1 - prob) << ' ';
+      }
   }
   else  f << " pp = " << pp;
 
@@ -188,13 +197,14 @@ template <class T> void EFGobitFunc<T>
 //                  EFGobitModule<T>: Member functions
 //------------------------------------------------------------------------
 
-template <class T>EFGobitModule<T>
-::EFGobitModule(const ExtForm<T> &EF, EFGobitParams<T> &p)
+template <class T>
+EFGobitModule<T>::EFGobitModule(const ExtForm<T> &EF, EFGobitParams<T> &p)
   : GobitModule<T>(p), E(EF)
 { }
 
-template <class T>EFGobitModule<T>
-::EFGobitModule(const ExtForm<T> &EF, EFGobitParams<T> &p, gDPVector<T> &s)
+template <class T>
+EFGobitModule<T>::EFGobitModule(const ExtForm<T> &EF, EFGobitParams<T> &p,
+				gDPVector<T> &s)
   : GobitModule<T>(p,s), E(EF)
 { }
 
@@ -203,9 +213,8 @@ template <class T> EFGobitModule<T>::~EFGobitModule()
 
 template <class T> GobitFunc<T> *EFGobitModule<T>::CreateFunc(void)
 {
-  if(start) {
-    gDPVector<T> s(E.Dimensionality());
-    s=*start;
+  if (start) {
+    BehavProfile<T> s(E, (gDPVector<T> &) *start);
     return new EFGobitFunc<T>(E, params, s); 
   }
   return new EFGobitFunc<T>(E, params);
