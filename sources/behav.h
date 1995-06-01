4474b56785616ed02a2f12548840f7c0ce7e423d
magyar <magyar@3fbfd45b-e82e-0410-bb8e-8470640bbd7f>
1995-06-01 11:14:59 +0000

    Initial revision

---
+++ sources/behav.h
@@ -0,0 +1,51 @@
+//#
+//# FILE: behav.h -- Behavioral strategy profile classes
+//#
+//# $Id$
+//#
+
+#ifndef BEHAV_H
+#define BEHAV_H
+
+#include "basic.h"
+
+class BaseExtForm;
+
+class BaseBehavProfile   {
+  protected:
+    const BaseExtForm *E;
+    bool truncated;
+
+    BaseBehavProfile(const BaseExtForm &, bool trunc);
+    BaseBehavProfile &operator=(const BaseBehavProfile &);
+
+  public:
+    virtual ~BaseBehavProfile();
+
+    DataType Type(void) const;
+    virtual bool IsPure(void) const = 0;
+    virtual bool IsPure(int pl) const = 0;
+    bool IsTruncated(void) const    { return truncated; }
+};
+
+
+#include "gdpvect.h"
+
+template <class T> class ExtForm;
+
+template <class T> class BehavProfile
+  : public BaseBehavProfile, public gDPVector<T>  {
+  public:
+    BehavProfile(const ExtForm<T> &, bool truncated = false);
+    BehavProfile(const ExtForm<T> &, const gDPVector<T> &);
+    ~BehavProfile();
+
+    BehavProfile<T> &operator=(const BehavProfile<T> &);
+
+    bool IsPure(void) const;
+    bool IsPure(int pl) const;
+};
+
+template <class T> gOutput &operator<<(gOutput &f, const BehavProfile<T> &p);
+
+#endif   // BEHAV_H
