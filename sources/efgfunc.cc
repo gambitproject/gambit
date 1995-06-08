d9af068f48798c640e70639998ba07efe1847b22
magyar <magyar@3fbfd45b-e82e-0410-bb8e-8470640bbd7f>
1995-06-08 11:46:28 +0000

    Added most solution algorithms

--- sources/efgfunc.cc
+++ sources/efgfunc.cc
@@ -5,8 +5,141 @@
 //#
 
 #include "gsm.h"
+#include "extform.h"
+
+#include "egobit.h"
+#include "eliap.h"
+
+template <class T> class Behav_List_Portion : public List_Portion   {
+  public:
+    Behav_List_Portion(ExtForm<double> *, const gList<BehavProfile<T> > &);
+};
+
+Behav_List_Portion<double>::Behav_List_Portion(ExtForm<double> *E,
+			       const gList<BehavProfile<double> > &list)
+{
+  _DataType = porBEHAV_DOUBLE;
+  for (int i = 1; i <= list.Length(); i++)
+    Append(new Behav_Portion<double>(list[i]));
+}
+
+//
+// GobitEfg: Parameter assignments:
+// 0   E            EFG 
+// 1   pxifile      TEXT
+// 2   minLam       *DOUBLE
+// 3   maxLam       *DOUBLE
+// 4   delLam       *DOUBLE
+// 5   maxitsOpt    *INTEGER
+// 6   maxitsBrent  *INTEGER
+// 7   tolOpt       *DOUBLE
+// 8   tolBrent     *DOUBLE
+// 9   time         *REF(DOUBLE)
+// 
+Portion *GSM_GobitEfg(Portion **param)
+{
+  EFGobitParams<double> EP;
+ 
+  gFileOutput f(((gString_Portion *) param[1])->Value());
+
+  if (f.IsValid())    EP.pxifile = &f;
+  
+  EFGobitModule<double> M(((Efg_Portion<double> *) param[0])->Value(), EP);
+  M.Gobit(1);
+
+  return new numerical_Portion<gInteger>(1);
+}
+
+Portion *GSM_LiapEfg(Portion **param)
+{
+  ExtForm<double> &E = ((Efg_Portion<double> *) param[0])->Value();
+
+  EFLiapParams<double> LP;
+  EFLiapModule<double> LM(E, LP);
+  LM.Liap(1);
+
+  return new Behav_List_Portion<double>(&E, LM.GetSolutions());
+}
+
+Portion *GSM_ReadEfg(Portion **param)
+{
+  gFileInput f(((gString_Portion *) param[0])->Value());
+  
+  if (f.IsValid())   {
+    ExtForm<double> *E = new ExtForm<double>;
+    ReadEfgFile(f, E);
+    return new Efg_Portion<double>(*E);
+  }
+  else
+    return 0;
+}
+
+Portion *GSM_WriteEfg(Portion **param)
+{
+  ExtForm<double> *E = &((Efg_Portion<double> *) param[0])->Value();
+  gFileOutput f(((gString_Portion *) param[1])->Value());
+  
+  if (f.IsValid())   {
+    E->WriteEfgFile(f);
+    return new Efg_Portion<double>(*E);
+  }
+  else
+    return 0;
+}
 
 void Init_efgfunc(GSM *gsm)
 {
+  FuncDescObj *FuncObj;
+
+  FuncObj = new FuncDescObj("ReadEfg");
+  FuncObj->SetFuncInfo(GSM_ReadEfg, 1);
+  FuncObj->SetParamInfo(GSM_ReadEfg, 0, "file", porSTRING, NO_DEFAULT_VALUE);
+  gsm->AddFunction(FuncObj);
+
+  FuncObj = new FuncDescObj("WriteEfg");
+  FuncObj->SetFuncInfo(GSM_WriteEfg, 2);
+  FuncObj->SetParamInfo(GSM_WriteEfg, 0, "E", porEFG_DOUBLE, NO_DEFAULT_VALUE);
+  FuncObj->SetParamInfo(GSM_WriteEfg, 1, "file", porSTRING, NO_DEFAULT_VALUE);
+  gsm->AddFunction(FuncObj);
+
+  FuncObj = new FuncDescObj("GobitEfg");
+  FuncObj->SetFuncInfo(GSM_GobitEfg, 2);
+  FuncObj->SetParamInfo(GSM_GobitEfg, 0, "E", porEFG_DOUBLE, NO_DEFAULT_VALUE);
+  FuncObj->SetParamInfo(GSM_GobitEfg, 1, "pxifile", porSTRING, NO_DEFAULT_VALUE);
+/*
+  FuncObj->SetParamInfo(GSM_GobitEfg, 2, "minLam", porDOUBLE,
+		        new numerical_Portion<double>(.01));
+  FuncObj->SetParamInfo(GSM_GobitEfg, 3, "maxLam", porDOUBLE,
+		        new numerical_Portion<double>(30));
+  FuncObj->SetParamInfo(GSM_GobitEfg, 4, "delLam", porDOUBLE,
+		        new numerical_Portion<double>(.01));
+  FuncObj->SetParamInfo(GSM_GobitEfg, 5, "maxitsOpt", porINTEGER,
+		        new numerical_Portion<gInteger>(200));
+  FuncObj->SetParamInfo(GSM_GobitEfg, 6, "maxitsBrent", porINTEGER,
+		        new numerical_Portion<gInteger>(100));
+  FuncObj->SetParamInfo(GSM_GobitEfg, 7, "tolOpt", porDOUBLE,
+		        new numerical_Portion<double>(1.0e-10));
+  FuncObj->SetParamInfo(GSM_GobitEfg, 8, "tolBrent", porDOUBLE,
+		        new numerical_Portion<double>(2.0e-10));
+  FuncObj->SetParamInfo(GSM_GobitEfg, 9, "time", porDOUBLE,
+			new numerical_Portion<double>(0), PASS_BY_REFERENCE);
+  */
+  gsm->AddFunction(FuncObj);
+
+  FuncObj = new FuncDescObj("LiapEfg");
+  FuncObj->SetFuncInfo(GSM_LiapEfg, 1);
+  FuncObj->SetParamInfo(GSM_LiapEfg, 0, "E", porEFG_DOUBLE, NO_DEFAULT_VALUE);
+  gsm->AddFunction(FuncObj);
 }
+
+
+
+#ifdef __GNUG__
+#define TEMPLATE template
+#elif defined __BORLANDC__
+#define TEMPLATE
+#pragma option -Jgd
+#endif   // __GNUG__, __BORLANDC__
+
+TEMPLATE class Behav_List_Portion<double>;
 
--- sources/nfgfunc.cc
+++ sources/nfgfunc.cc
@@ -15,8 +15,6 @@ Portion *GSM_DisplayNfg(Portion **param)
 }
 
 
-#include "lemke.h"
-
 template <class T> class Mixed_List_Portion : public List_Portion   {
   public:
     Mixed_List_Portion(NormalForm<double> *, const gList<gPVector<T> > &);
@@ -28,20 +26,6 @@ Mixed_List_Portion<double>::Mixed_List_Portion(NormalForm<double> *N,
   _DataType = porMIXED_DOUBLE;
   for (int i = 1; i <= list.Length(); i++)
     Append(new Mixed_Portion<double>(MixedProfile<double>(*N, list[i])));
-}
-
-Portion *GSM_Lemke(Portion **param)
-{
-  NormalForm<double> *N = &((Nfg_Portion<double> *) param[0])->Value();
-
-  LemkeParams LP;
-  LemkeModule<double> LS(*N, LP);
-  LS.Lemke();
-
-  ((numerical_Portion<double> *) param[2])->Value() = (double) LS.Time();
-  ((numerical_Portion<gInteger> *) param[3])->Value() = LS.NumPivots();
-
-  return new Mixed_List_Portion<double>(N, LS.GetSolutions());
 }
 
 #include "enum.h"
@@ -57,6 +41,65 @@ Portion *GSM_Enum(Portion **param)
   return new Mixed_List_Portion<double>(N, EM.GetSolutions());
 }
 
+#include "ngobit.h"
+
+Portion *GSM_GobitNfg(Portion **param)
+{
+  NFGobitParams<double> EP;
+
+  gFileOutput f(((gString_Portion *) param[1])->Value());
+
+  if (f.IsValid())    EP.pxifile = &f;
+
+  NFGobitModule<double> M(((Nfg_Portion<double> *) param[0])->Value(), EP);
+  M.Gobit(1);
+
+  return new numerical_Portion<gInteger>(1);
+}
+
+#include "lemke.h"
+
+Portion *GSM_Lemke(Portion **param)
+{
+  NormalForm<double> *N = &((Nfg_Portion<double> *) param[0])->Value();
+
+  LemkeParams LP;
+  LemkeModule<double> LS(*N, LP);
+  LS.Lemke();
+
+/*
+  ((numerical_Portion<double> *) param[2])->Value() = (double) LS.Time();
+  ((numerical_Portion<gInteger> *) param[3])->Value() = LS.NumPivots();
+  */
+  return new Mixed_List_Portion<double>(N, LS.GetSolutions());
+}
+
+#include "nliap.h"
+
+Portion *GSM_LiapNfg(Portion **param)
+{
+  NormalForm<double> &N = ((Nfg_Portion<double> *) param[0])->Value();
+
+  NFLiapParams<double> LP;
+  NFLiapModule<double> LM(N, LP);
+  LM.Liap(1);
+
+  return new Mixed_List_Portion<double>(&N, LM.GetSolutions());
+}
+
+#include "simpdiv.h"
+
+Portion *GSM_Simpdiv(Portion **param)
+{
+  NormalForm<double> &N = ((Nfg_Portion<double> *) param[0])->Value();
+
+  SimpdivParams SP;
+  SimpdivModule<double> SM(N, SP);
+  SM.Simpdiv();
+
+  return new Mixed_List_Portion<double>(&N, SM.GetSolutions());
+}
+
 Portion *GSM_ReadNfg(Portion **param)
 {
   gFileInput f(((gString_Portion *) param[0])->Value());
@@ -69,39 +112,55 @@ Portion *GSM_ReadNfg(Portion **param)
     return 0;
 }
 
+
 void Init_nfgfunc(GSM *gsm)
 {
   FuncDescObj *FuncObj;
 
-  FuncObj = new FuncDescObj( "DisplayNfg" );
-  FuncObj->SetFuncInfo( GSM_DisplayNfg, 1 );
-  FuncObj->SetParamInfo( GSM_DisplayNfg, 0, "N", 
-			porNFG, NO_DEFAULT_VALUE);
+  FuncObj = new FuncDescObj("DisplayNfg");
+  FuncObj->SetFuncInfo(GSM_DisplayNfg, 1);
+  FuncObj->SetParamInfo(GSM_DisplayNfg, 0, "N", porNFG, NO_DEFAULT_VALUE);
   gsm->AddFunction(FuncObj);
 
-  FuncObj = new FuncDescObj( "Enum" );
-  FuncObj->SetFuncInfo( GSM_Enum, 1 );
-  FuncObj->SetParamInfo( GSM_Enum, 0, "N", 
-			porNFG, NO_DEFAULT_VALUE);
+  FuncObj = new FuncDescObj("Enum");
+  FuncObj->SetFuncInfo(GSM_Enum, 1);
+  FuncObj->SetParamInfo(GSM_Enum, 0, "N", porNFG, NO_DEFAULT_VALUE);
   gsm->AddFunction(FuncObj);
 
-  FuncObj = new FuncDescObj( "Lemke" );
-  FuncObj->SetFuncInfo( GSM_Lemke, 4 );
-  FuncObj->SetParamInfo( GSM_Lemke, 0, "N", 
-			porNFG, NO_DEFAULT_VALUE);
-  FuncObj->SetParamInfo( GSM_Lemke, 1, "nequilib", 
+  FuncObj = new FuncDescObj("GobitNfg");
+  FuncObj->SetFuncInfo(GSM_GobitNfg, 2);
+  FuncObj->SetParamInfo(GSM_GobitNfg, 0, "N", porNFG_DOUBLE, NO_DEFAULT_VALUE);
+  FuncObj->SetParamInfo(GSM_GobitNfg, 1, "pxifile", porSTRING,
+			NO_DEFAULT_VALUE);
+  gsm->AddFunction(FuncObj);
+
+  FuncObj = new FuncDescObj("Lemke");
+  FuncObj->SetFuncInfo(GSM_Lemke, 1);
+  FuncObj->SetParamInfo(GSM_Lemke, 0, "N", porNFG_DOUBLE, NO_DEFAULT_VALUE);
+/*
+  FuncObj->SetParamInfo(GSM_Lemke, 1, "nequilib", 
 			porINTEGER, new numerical_Portion<gInteger>( 0 ) );
-  FuncObj->SetParamInfo( GSM_Lemke, 2, "time", 
+  FuncObj->SetParamInfo(GSM_Lemke, 2, "time", 
 			porDOUBLE, new numerical_Portion<double>( 0 ),
 			PASS_BY_REFERENCE);
-  FuncObj->SetParamInfo( GSM_Lemke, 3, "npivots", 
+  FuncObj->SetParamInfo(GSM_Lemke, 3, "npivots", 
 			porINTEGER, new numerical_Portion<gInteger>( 0 ) );
+  */
   gsm->AddFunction(FuncObj);
 
-  FuncObj = new FuncDescObj( "ReadNfg" );
-  FuncObj->SetFuncInfo( GSM_ReadNfg, 1 );
-  FuncObj->SetParamInfo( GSM_ReadNfg, 0, "file", 
-			porSTRING, NO_DEFAULT_VALUE);
+  FuncObj = new FuncDescObj("LiapNfg");
+  FuncObj->SetFuncInfo(GSM_LiapNfg, 1);
+  FuncObj->SetParamInfo(GSM_LiapNfg, 0, "N", porNFG_DOUBLE, NO_DEFAULT_VALUE);
+  gsm->AddFunction(FuncObj);
+
+  FuncObj = new FuncDescObj("Simpdiv");
+  FuncObj->SetFuncInfo(GSM_Simpdiv, 1);
+  FuncObj->SetParamInfo(GSM_Simpdiv, 0, "N", porNFG_DOUBLE, NO_DEFAULT_VALUE);
+  gsm->AddFunction(FuncObj);
+
+  FuncObj = new FuncDescObj("ReadNfg");
+  FuncObj->SetFuncInfo(GSM_ReadNfg, 1);
+  FuncObj->SetParamInfo(GSM_ReadNfg, 0, "file",	porSTRING, NO_DEFAULT_VALUE);
   gsm->AddFunction(FuncObj);
 
 }
