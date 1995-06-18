1de440792e0289b7fcf0839a209cbd7d6dac6e2c
egrayver <egrayver@3fbfd45b-e82e-0410-bb8e-8470640bbd7f>
1995-06-18 21:30:02 +0000

    Initial revision

---
+++ sources/gnullsts.cc
@@ -0,0 +1,43 @@
+//
+// File gnullsts: instantiation of a null gStatus.  This gStatus does NOTHING!
+//
+// $Id$
+//
+
+#include "gstatus.h"
+
+class gNullStatus : public gStatus
+{
+public:
+	~gNullStatus(void) { ;}
+	// functions for gProgress::gOutput
+	int GetWidth(void) { ;}
+	gOutput &SetWidth(int w) { ;}
+	int GetPrec(void) { ;}
+	gOutput &SetPrec(int p) { ;}
+	gOutput &SetExpMode(void) { ;}
+	gOutput &SetFloatMode(void) { ;}
+	char GetRepMode(void) { ;}
+
+	gOutput &operator<<(int x) { ;}
+	gOutput &operator<<(unsigned int x) { ;}
+	gOutput &operator<<(long x) { ;}
+	gOutput &operator<<(char x) { ;}
+	gOutput &operator<<(double x) { ;}
+	gOutput &operator<<(float x) { ;}
+	gOutput &operator<<(const char *x) { ;}
+	gOutput &operator<<(const void *x) { ;}
+
+	bool IsValid(void) const { ;}
+	// functions for gProgress
+	void	SetProgress(double p) { ;}
+	// functions for gSignal
+	bool Get(void) const {return false;}
+	void Reset(void) { ;}
+};
+
+gNullStatus _gstatus;
+gStatus &gstatus=_gstatus;
+gSignal &gbreak=_gstatus;
+
+
