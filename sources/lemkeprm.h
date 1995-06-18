93a3a5e1fc7846c0a81ce2b01fb9a9add7b6599d
egrayver <egrayver@3fbfd45b-e82e-0410-bb8e-8470640bbd7f>
1995-06-18 22:09:45 +0000

    Initial revision

---
+++ sources/lemkeprm.h
@@ -0,0 +1,42 @@
+#ifndef LEMKEPRM_H
+#define LEMKEPRM_H
+
+#include "outprm.h"
+
+class LemkeSolveParamsDialog : public OutputParamsDialog
+{
+private:
+	int plev,nequilib,maxdepth,dup_strat;
+public:
+	LemkeSolveParamsDialog(wxWindow *parent=0);
+	~LemkeSolveParamsDialog(void);
+	void GetParams(LemkeParams &P);
+};
+
+
+LemkeSolveParamsDialog::LemkeSolveParamsDialog(wxWindow *parent)
+														:OutputParamsDialog("Lemke Params",parent)
+
+{
+dup_strat=0;nequilib=0;maxdepth=0;nequilib=1;
+Form()->Add(wxMakeFormBool("All Solutions",&dup_strat));
+Form()->Add(wxMakeFormNewLine());
+Form()->Add(wxMakeFormShort("# Equ",&nequilib));
+Form()->Add(wxMakeFormShort("Max depth",&maxdepth));
+
+// Now add the basic stuff
+MakeOutputFields(OUTPUT_FIELD);
+Go();
+}
+
+LemkeSolveParamsDialog::~LemkeSolveParamsDialog(void)
+{}
+
+void LemkeSolveParamsDialog::GetParams(LemkeParams &P)
+{
+P.dup_strat=!dup_strat;P.nequilib=nequilib;P.maxdepth=maxdepth;
+// Output stuff
+P.plev=TraceLevel();P.output=OutFile();
+
+}
+#endif
