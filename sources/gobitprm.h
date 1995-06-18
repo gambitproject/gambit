eeee31e95a2624cff17073b0b414484bf87e337a
egrayver <egrayver@3fbfd45b-e82e-0410-bb8e-8470640bbd7f>
1995-06-18 22:12:24 +0000

    *** empty log message ***

--- sources/gobitprm.h
+++ sources/gobitprm.h
@@ -1,42 +1,29 @@
-#include "wx.h"
-#include "wx_form.h"
-#include "wxmisc.h"
+#ifndef GOBITPRM_H
+#define GOBITPRM_H
+
+#include "outprm.h"
 
 #define DATA_TYPE_LOG 1
 template <class T>
-class GobitSolveParamsDialog : public MyDialogBox
+class GobitSolveParamsDialog : public PxiParamsDialog
 {
 private:
 	float minLam, maxLam, delLam, tolOpt, tolBrent;
 	int maxitsOpt,maxitsBrent,nequilib;
-	int type, plev;
-	char *outfile,*pxifile;
-	Bool run_pxi;
-	char *pxi_command;
 public:
-	GobitSolveParamsDialog(void);
+	GobitSolveParamsDialog(wxWindow *parent);
 	~GobitSolveParamsDialog(void);
-	GobitParams<T> GetParams(void);
-	int RunPXI(void) {return run_pxi;}
-	char *PXICommand(void) {return pxi_command;}
+	void GetParams(GobitParams<T> &P);
 };
 
 
 //******************************** Constructor/main ************************
 template <class T>
-GobitSolveParamsDialog<T>::GobitSolveParamsDialog(void)
-												:MyDialogBox(NULL,"Gobit Params")
+GobitSolveParamsDialog<T>::GobitSolveParamsDialog(wxWindow *parent)
+												:PxiParamsDialog("gobit","Gobit Params",parent)
 {
-// The default output filename is a temp file name
-outfile=new char[250];pxifile=new char[250];pxi_command=new char[250];
-strcpy(outfile,"gobit.xxx");strcpy(pxi_command,"pxi");
-char	t_outfile[250];
-wxGetTempFileName("gobit",t_outfile);
-strcpy(pxifile,FileNameFromPath(t_outfile));
-minLam=0.01;maxLam=30.0;delLam=.01;type=DATA_TYPE_LOG;
+minLam=0.01;maxLam=30.0;delLam=.01;nequilib=1;
 tolOpt=.0001;tolBrent=0.0001;maxitsBrent=100;maxitsOpt=200;
-nequilib=1;
-run_pxi=FALSE;
 Form()->Add(wxMakeFormShort("# Equilibria",&nequilib));
 Form()->Add(wxMakeFormNewLine());
 Form()->Add(wxMakeFormFloat("L Start",&minLam,wxFORM_DEFAULT,NULL,NULL,wxVERTICAL,100));
@@ -48,53 +35,43 @@ Form()->Add(wxMakeFormFloat("Tolerance 1-D",&tolBrent,wxFORM_DEFAULT,NULL,NULL,w
 Form()->Add(wxMakeFormNewLine());
 Form()->Add(wxMakeFormShort("Iterations n-D",&maxitsOpt,wxFORM_DEFAULT,NULL,NULL,wxVERTICAL,100));
 Form()->Add(wxMakeFormShort("Iterations 1-D",&maxitsBrent,wxFORM_DEFAULT,NULL,NULL,wxVERTICAL,100));
-Form()->Add(wxMakeFormNewLine());
-Form()->Add(wxMakeFormString("OutFile",&outfile,wxFORM_DEFAULT,NULL,NULL,wxVERTICAL));
-Form()->Add(wxMakeFormString("PxiFile",&pxifile,wxFORM_DEFAULT,NULL,NULL,wxVERTICAL));
-Form()->Add(wxMakeFormNewLine());
-wxStringList *type_list=new wxStringList;
-type_list->Add("Log plot");type_list->Add("Arith plot");
-char *type_str=new char[20];
-strcpy(type_str,"Log plot");
-wxFormItem *type_item=wxMakeFormString("Plot Type",&type_str,wxFORM_RADIOBOX,
-			 new wxList(wxMakeConstraintStrings(type_list), 0));
-Form()->Add(type_item);
-Form()->Add(wxMakeFormNewLine());
-run_pxi=FALSE;
-Form()->Add(wxMakeFormBool("Run PXI",&run_pxi));
-Form()->Add(wxMakeFormString("PXI Command",&pxi_command));
-Form()->Add(wxMakeFormNewLine());
-Form()->AssociatePanel(this);
-Go1();
-type=wxListFindString(type_list,type_str);
+// Now add the basic stuff
+MakePxiFields();
+MakeOutputFields();
+Go();
 }
 
 template <class T>
 GobitSolveParamsDialog<T>::~GobitSolveParamsDialog(void)
-{
-delete [] outfile;
-}
+{ }
+
 template <class T>
-GobitParams<T> GobitSolveParamsDialog<T>::GetParams(void)
+void GobitSolveParamsDialog<T>::GetParams(GobitParams<T> &P)
 {
-GobitParams<T> P;
+// Gobit stuff
 P.minLam=minLam;P.maxLam=maxLam;P.delLam=delLam;
 P.tolBrent=tolBrent;P.maxitsBrent=maxitsBrent;
 P.nequilib=nequilib;
-//P.tolPOW=tolOpt;P.maxitsPOW=maxitsOpt;
-//P.outfile=pxifile;P.errfile=outfile;
-P.type=type;P.plev=0;
-return P;
+P.tolOpt=tolOpt;P.maxitsOpt=maxitsOpt;
+// Pxi stuff
+P.type=PxiType();P.pxifile=PxiFile();
+// Output stuff
+P.plev=TraceLevel();P.outfile=OutFile();P.errfile=ErrFile();
 }
 
-#ifdef __GNUG__
-#define TEMPLATE template
-#elif defined __BORLANDC__
-#pragma option -Jgd
-#define TEMPLATE
-#endif   // __GNUG__, __BORLANDC__
-TEMPLATE class GobitSolveParamsDialog<double> ;
-TEMPLATE class GobitSolveParamsDialog<gRational> ;
-#ifdef __BORLANDC__
-#pragma -Jgx
+#ifdef GOBIT_INST		// need this so we only create this once
+	#ifdef __GNUG__
+		#define TEMPLATE template
+	#elif defined __BORLANDC__
+		#pragma option -Jgd
+		#define TEMPLATE
+	#endif   // __GNUG__, __BORLANDC__
+	TEMPLATE class GobitSolveParamsDialog<double> ;
+  #include "rational.h"
+	TEMPLATE class GobitSolveParamsDialog<gRational> ;
+	#ifdef __BORLANDC__
+		#pragma option -Jgx
+	#endif
+#endif
+
 #endif
--- sources/liapprm.h
+++ sources/liapprm.h
@@ -1,70 +1,64 @@
-#include "wx.h"
-#include "wx_form.h"
-#include "wxmisc.h"
+#ifndef LIAPPRM_H
+#define LIAPPRM_H
+#include "outprm.h"
 
-#define DATA_TYPE_LOG 1
 template <class T>
-class LiapSolveParamsDialog : public MyDialogBox
+class LiapSolveParamsDialog : public OutputParamsDialog
 {
 private:
 	float tolOpt, tolBrent;
 	int maxitsOpt,maxitsBrent,nequilib,ntries;
-	int plev;
-	char *outfile,*errfile;
 public:
-	LiapSolveParamsDialog(void);
+	LiapSolveParamsDialog(wxWindow *parent);
 	~LiapSolveParamsDialog(void);
-	LiapParams<T> GetParams(void);
+	void GetParams(LiapParams<T> &P);
 };
 
 
 //******************************** Constructor/main ************************
 template <class T>
-LiapSolveParamsDialog<T>::LiapSolveParamsDialog(void)
-												:MyDialogBox(NULL,"Liap Params")
+LiapSolveParamsDialog<T>::LiapSolveParamsDialog(wxWindow *parent)
+												:OutputParamsDialog("Liap Params",parent)
 {
-// The default output filename is a temp file name
-outfile=new char[250];errfile=new char[250];
-strcpy(outfile,"liap.out");strcpy(errfile,"liap.err");
 tolOpt=.0001;tolBrent=0.0001;maxitsBrent=100;maxitsOpt=200;
 nequilib=1;ntries=10;
-Form()->Add(wxMakeFormShort("# Equilibria",&nequilib));
-Form()->Add(wxMakeFormShort("# Tries",&ntries));
+Form()->Add(wxMakeFormShort("# Equilibria",&nequilib,wxFORM_DEFAULT,NULL,NULL,wxVERTICAL));
+Form()->Add(wxMakeFormShort("# Tries",&ntries,wxFORM_DEFAULT,NULL,NULL,wxVERTICAL));
 Form()->Add(wxMakeFormNewLine());
 Form()->Add(wxMakeFormFloat("Tolerance n-D",&tolOpt,wxFORM_DEFAULT,NULL,NULL,wxVERTICAL,100));
 Form()->Add(wxMakeFormFloat("Tolerance 1-D",&tolBrent,wxFORM_DEFAULT,NULL,NULL,wxVERTICAL,100));
 Form()->Add(wxMakeFormNewLine());
 Form()->Add(wxMakeFormShort("Iterations n-D",&maxitsOpt,wxFORM_DEFAULT,NULL,NULL,wxVERTICAL,100));
 Form()->Add(wxMakeFormShort("Iterations 1-D",&maxitsBrent,wxFORM_DEFAULT,NULL,NULL,wxVERTICAL,100));
-Form()->Add(wxMakeFormNewLine());
-Form()->Add(wxMakeFormString("OutFile",&outfile,wxFORM_DEFAULT,NULL,NULL,wxVERTICAL));
-Form()->Add(wxMakeFormString("ErrFile",&errfile,wxFORM_DEFAULT,NULL,NULL,wxVERTICAL));
+
+// Now add the basic stuff
+MakeOutputFields();
 Go();
 }
 
 template <class T>
 LiapSolveParamsDialog<T>::~LiapSolveParamsDialog(void)
-{
-delete [] outfile;delete [] errfile;
-}
+{}
 template <class T>
-LiapParams<T> LiapSolveParamsDialog<T>::GetParams(void)
+void LiapSolveParamsDialog<T>::GetParams(LiapParams<T> &P)
 {
-LiapParams<T> P;
 P.tolBrent=tolBrent;P.maxitsBrent=maxitsBrent;
 P.nequilib=nequilib;P.ntries=ntries;
-P.outfile=new gFileOutput(outfile);P.errfile=new gFileOutput(errfile);
-P.plev=0;
-return P;
+// Output stuff
+P.plev=TraceLevel();P.outfile=OutFile();P.errfile=ErrFile();
 }
-#ifdef __GNUG__
-#define TEMPLATE template
-#elif defined __BORLANDC__
-#pragma option -Jgd
-#define TEMPLATE
-#endif   // __GNUG__, __BORLANDC__
-TEMPLATE class LiapSolveParamsDialog<double> ;
-TEMPLATE class LiapSolveParamsDialog<gRational> ;
-#ifdef __BORLANDC__
-#pragma -Jgx
+#ifdef LIAP_INST 			// need this so we only create this once
+	#ifdef __GNUG__
+		#define TEMPLATE template
+	#elif defined __BORLANDC__
+		#pragma option -Jgd
+		#define TEMPLATE
+	#endif   // __GNUG__, __BORLANDC__
+	TEMPLATE class LiapSolveParamsDialog<double> ;
+	TEMPLATE class LiapSolveParamsDialog<gRational> ;
+	#ifdef __BORLANDC__
+		#pragma option -Jgx
+	#endif
+#endif
+
 #endif
