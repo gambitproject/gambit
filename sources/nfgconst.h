fdc8b56291693251d2b9f8353f0474ccbe83e2b1
egrayver <egrayver@3fbfd45b-e82e-0410-bb8e-8470640bbd7f>
1995-07-04 17:01:08 +0000

    Initial revision

---
+++ sources/nfgconst.h
@@ -0,0 +1,15 @@
+// File: nfgconst.h -- defines various constants for NormShow menu events
+// $Id$
+
+#ifndef NFGCONST_H
+#define NFGCONST_H
+// must make sure it none of the menus are a power of two
+#define NFG_SOLVE_SOLVE_MENU				100
+#define NFG_SOLVE_INSPECT_MENU			101
+#define NFG_SOLVE_PAYOFF_MENU				102 // this is not a real menu=DoubleClick
+#define	NFG_SOLVE_REMOVE_MENU				103
+
+#define NFG_DISPLAY_COLORS					200
+#define NFG_DISPLAY_OPTIONS					201
+#define NFG_DISPLAY_ACCELS					202
+#endif
