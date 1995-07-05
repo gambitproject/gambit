4dc4e8925c923098f0ce5c504ef9366112bf4b17
egrayver <egrayver@3fbfd45b-e82e-0410-bb8e-8470640bbd7f>
1995-07-05 22:37:19 +0000

    Initial revision

---
+++ sources/sprdaccl.h
@@ -0,0 +1,19 @@
+// File: sprdaccl.h -- gives names to various SpreadSheet3D events.
+// $Id$
+#ifndef SPRDACCL_H
+#define SPRDACCL_H
+
+#include "accels.h"
+#include "sprconst.h"
+
+#define 	NUM_SPREAD_EVENTS	5
+AccelEvent spread_events[NUM_SPREAD_EVENTS]=
+{
+AccelEvent("Print",OUTPUT_MENU),
+AccelEvent("Close",CLOSE_MENU),
+AccelEvent("Options",OPTIONS_MENU),
+AccelEvent("Help",HELP_MENU_CONTENTS),
+AccelEvent("HelpAbout",HELP_MENU_ABOUT)
+};
+
+#endif
