27ec95c3d2438f9491d9e7a8a482ada4b32f5869
rdm <rdm@3fbfd45b-e82e-0410-bb8e-8470640bbd7f>
1995-07-06 14:37:28 +0000

    *** empty log message ***

--- sources/tableau.h
+++ sources/tableau.h
@@ -9,7 +9,6 @@
 
 // includes
 
-#include "normal.h"
 #include "rational.h"
 #include "ludecomp.h"
 #include "gtableau.h"
@@ -18,61 +17,64 @@ template <class T> class DumbTableau;
 template <class T> class Tableau;
 template <class T> class Basis;
 
+
 template <class T> class DumbTableau {
 friend class Tableau<T>;
 friend class Basis<T>;
 protected:
-  int n1, n2;
   gMatrix<T> A;
   gVector<T> b;
 public:
   DumbTableau(void);
-  DumbTableau(int n1, int n2);
-  DumbTableau(const NormalForm<T> &);
+  DumbTableau(int rl, int rh, int cl, int ch);
   DumbTableau(const gMatrix<T>&, const gVector<T>&);
   ~DumbTableau();
+
   int MinRow(void) const;
   int MinCol(void) const;
   int MaxRow(void) const;
   int MaxCol(void) const;
-
+  T &Set_A(int r,int c);
+  T &Set_b(int r);
+  void Dump(gOutput &) const;
 };
 
 
 template <class T>
 class Basis {
-  friend class Tableau<T>;
- protected:
+friend class Tableau<T>;
+protected:
   const DumbTableau<T> *dtab;
   gBlock<int> label;    
- public:
+public:
   Basis(void);
+  Basis(int min, int max);
   Basis(const DumbTableau<T> &); // initial Tableau is unit matrix
   Basis(const DumbTableau<T> &, const gBlock<int> &);
-    // use negative index convention
+      // use negative index convention
   Basis<T>& operator=(const Basis<T>&);
   Basis(const Basis<T> &);
   ~Basis();
-
+  
   void NewBasis();
   void NewBasis(const gBlock<int> &);
   void change(int index, int label);
-    // change Basis member index to label
+      // change Basis member index to label
   void swap(int,int); // switch two Basis elements
-
+  
   bool member(int label) const;
-    // return true iff label is a Basis member
+      // return true iff label is a Basis member
   int find(int label) const;
-    // finds Basis index corresponding to label number,
-    // fails assert if label not in Basis
+      // finds Basis index corresponding to label number,
+      // fails assert if label not in Basis
   int Label(int index) const;
-    // finds label of variable corresponding to Basis index
+      // finds label of variable corresponding to Basis index
   void BasisSelect(const gVector<T>&rowv, gVector<T> &colv) const;
-    // select Basis elements according to Tableau rows and cols
+      // select Basis elements according to Tableau rows and cols
   void BasisSelect(const gVector<T>&unitv,
 		   const gVector<T>&rowv,
 		   gVector<T>&colv
-		   ) const; // as above, but unit column elements nonzero
+		 ) const; // as above, but unit column elements nonzero
   void Dump(gOutput &) const;
 };
 
@@ -80,7 +82,7 @@ template <class T> class Tableau {
  private:
   //gVector<T> tmpcol; // temporary column vector, to avoid allocation
 
-  void SolveDual();
+//  void SolveDual();
 
   bool ColIndex(int) const;
   bool RowIndex(int) const;
@@ -90,15 +92,15 @@ template <class T> class Tableau {
   LUdecomp<T> B;
   gVector<T> solution;
 
-  gVector<T> unitcost;
-  gVector<T> cost;
-  gVector<T> dual;
-  bool costdefined;
+//  gVector<T> unitcost;
+//  gVector<T> cost;
+//  gVector<T> dual;
+//  bool costdefined;
   bool creator;
 
  public:
   Tableau(DumbTableau<T> &);
-  Tableau(const NormalForm<T> &);
+  Tableau(int minr, int maxr, int minc, int maxc); // creates dtab
   Tableau(DumbTableau<T> &, const Basis<T> &);
   Tableau(DumbTableau<T> &, const gVector<T> &cost, const Basis<T> &);
     // unit column cost == 0
@@ -116,14 +118,17 @@ template <class T> class Tableau {
   int MaxRow() const;
 
   // cost-based functions
+/*
   void SetCost(const gVector<T>& ); // unit column cost := 0
   void SetCost(const gVector<T>&, const gVector<T>& ); // DumbTableau row
   void GetCost(gVector<T>&, gVector<T>& ) const; // DumbTableau row
   void ClearCost(); // make cost undefined
-	T TotalCost() const; // cost of current solution
-	T RelativeCost(int) const; // negative index convention
-	void RelativeCostVector(gVector<T> &, gVector<T> &) const; // DumbTableau row
-	void DualVector(gVector<T> &) const; // column vector
+  T TotalCost() const; // cost of current solution
+  T RelativeCost(int) const; // negative index convention
+  void RelativeCostVector(gVector<T> &, gVector<T> &) const; 
+      // DumbTableau row
+  void DualVector(gVector<T> &) const; // column vector
+*/
 
   // non-cost-based functions
   void BasisVector(gVector<T> &) const; // column vector
@@ -140,6 +145,7 @@ template <class T> class Tableau {
   void GetBasis( Basis<T> & ) const; // return Basis for current Tableau
 
   // raw Tableau functions
+  void Refactor();
   void Solve(const gVector<T> &, gVector<T> &) const;
   void SolveT(const gVector<T> &, gVector<T> &) const;
   void Multiply(const gVector<T> &, gVector<T>& ) const;
