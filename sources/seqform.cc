e1fe4b69df4232c35f1edb71c28505de5e14eced
rdm <rdm@3fbfd45b-e82e-0410-bb8e-8470640bbd7f>
1995-07-10 10:28:44 +0000

    Initial revision

---
+++ sources/seqform.cc
@@ -0,0 +1,180 @@
+//#
+//# FILE: lemke1.cc -- Sequence Form module
+//#
+//# $Id$ 
+//#
+
+#include "rational.h"
+#include "gwatch.h"
+#include "gdpvect.h"
+
+#include "seqform.h"
+#include "behav.h"
+#include "player.h"
+#include "infoset.h"
+
+#include "glist.imp"
+template class gList<gDPVector<double> >;
+template class gList<gDPVector<gRational> >;
+template class gNode<gDPVector<double> >;
+template class gNode<gDPVector<gRational> >;
+
+
+//---------------------------------------------------------------------------
+//                        LemkeBasis: member functions
+//---------------------------------------------------------------------------
+ 
+//---------------------------------------------------------------------------
+//                        SeqFormParams: member functions
+//---------------------------------------------------------------------------
+
+SeqFormParams::SeqFormParams(void) 
+  :  plev(0), nequilib(0), output(&gnull)
+{ }
+
+//
+// Lemke is the most important routine.
+// It implements the Lemke-Howson algorithm, as refined by Eaves.
+// It is assumed that the starting point is a complementary basic
+// feasible solution.  If not it returns 0 without doing anything.
+//
+
+
+template <class T> int SeqFormModule<T>::Lemke(int dup)
+{
+  BFS<T> cbfs((T) 0);
+  int i;
+  
+  if (EF.NumPlayers() != 2 || !params.output)   return 0;
+  
+  gWatch watch;
+  
+  List = BFS_List();
+  
+  SFTableau<T> B(EF);
+  B.LCPPath();
+  Add_BFS(B);
+  
+  if (params.plev >= 2)  {
+    for (i = 1; i <= List.Length(); i++)   {
+      List[i].Dump(*params.output);
+      (*params.output) << "\n";
+    }
+  }
+  gVector<T> sol(B.MinRow(),B.MaxRow());
+  gDPVector<T> profile(EF.Dimensionality());
+  B.BasisVector(sol);
+  B.GetProfile(profile,sol,EF.RootNode(),1,1);
+//  gout << "\nprofile = " << profile << "\n";
+  solutions.Flush();
+  solutions.Append(profile);
+    
+//  if(params.plev >= 1)
+//    (*params.output) << "\nN Pivots = " << npivots << "\n";
+  
+  time = watch.Elapsed();
+  return List.Length();
+}
+
+template <class T> int SeqFormModule<T>::Add_BFS(const SFTableau<T> &B)
+{
+  BFS<T> cbfs((T) 0);
+  gVector<T> v(B.MinRow(), B.MaxRow());
+  B.BasisVector(v);
+
+  for (int i = B.MinCol(); i <= B.MaxCol(); i++)
+    if (B.Member(i)) {
+      cbfs.Define(i, v[B.Find(i)]);
+    }
+
+  if (List.Contains(cbfs))  return 0;
+//  if(params.plev >=2) (*params.output) << "\nFound CBFS";
+//  (*params.output)  << "\nB = ";
+//  B.Dump(*params.output);
+//  (*params.output)  << "\ncbfs = ";
+//  cbfs.Dump(*params.output );
+  List.Append(cbfs);
+  return 1;
+}
+
+//-------------------------------------------------------------------------
+//                   SeqFormModule<T>: Returning solutions
+//-------------------------------------------------------------------------
+
+template <class T> 
+const gList<gDPVector<T> > &SeqFormModule<T>::GetSolutions(void) const
+{
+  return solutions;
+}
+
+template <class T> int SeqFormModule<T>::NumPivots(void) const
+{
+  return npivots;
+}
+
+//-------------------------------------------------------------------------
+//                    SeqFormModule<T>: Member functions
+//-------------------------------------------------------------------------
+
+template <class T>
+SeqFormModule<T>::SeqFormModule(const ExtForm<T> &E, const SeqFormParams &p)
+  : EF(E), params(p), npivots(0), 
+    List()
+{ }
+
+template <class T> SeqFormModule<T>::~SeqFormModule()
+{ }
+
+template <class T> double SeqFormModule<T>::Time(void) const
+{
+  return time;
+}
+
+#ifdef __GNUG__
+template class SeqFormModule<double>;
+template class SeqFormModule<gRational>;
+#elif defined __BORLANDC__
+#pragma option -Jgd
+class SeqFormModule<double>;
+class SeqFormModule<gRational>;
+#pragma option -Jgx
+#endif   // __GNUG__, __BORLANDC__
+
+
+//-------------------------------------------------------------------------
+//                    Convenience functions for Sequence Form
+//-------------------------------------------------------------------------
+
+template <class T>
+int SeqForm(const ExtForm<T> &E, const SeqFormParams &p,
+	  gList<gDPVector<T> > &solutions,
+	  int &npivots, gRational &time)
+{ 
+  SeqFormModule<T> SM(E, p);
+  int result = SM.Lemke();
+
+  npivots = SM.NumPivots();
+  time = SM.Time();
+  
+//  solutions = SM.GetSolutions();
+
+  return result;
+}
+
+#ifdef __GNUG__
+template int SeqForm(const ExtForm<double> &, const SeqFormParams &,
+		   gList<gDPVector<double> > &, int &, gRational &);
+template int SeqForm(const ExtForm<gRational> &, const SeqFormParams &,
+		   gList<gDPVector<gRational> > &, int &, gRational &);
+#elif defined __BORLANDC__
+#pragma option -Jgd
+int SeqForm(const ExtForm<double> &, const SeqFormParams &,
+	  gList<gDPVector<double> > &, int &, gRational &);
+int SeqForm(const ExtForm<gRational> &, const SeqFormParams &,
+	  gList<gDPVector<gRational> > &, int &, gRational &);
+#pragma option -Jgx
+#endif   // __GNUG__, __BORLANDC__
+
+
+
+
---
+++ sources/seqform.h
@@ -0,0 +1,63 @@
+//#
+//# FILE: seqform.h -- Interface to Sequence Form solution module
+//#
+//# $Id$ 
+//#
+
+#ifndef SEQFORM_H
+#define SEQFORM_H
+
+#include "extform.h"
+#include "rational.h"
+#include "glist.h"
+#include "gdpvect.h"
+#include "sftab.h"
+
+class SeqFormParams     {
+  public:
+    int  plev, nequilib, maxdepth;
+    gOutput *output;
+    
+    SeqFormParams(void);
+};
+
+//
+// The general-purpose interface to the solver routines
+//
+
+template <class T> class SeqFormModule  {
+private:
+  const ExtForm<T> &EF;
+  const SeqFormParams &params;
+  int npivots;
+  double time;
+  BFS_List List;
+  gList< gDPVector<T> > solutions;
+
+public:
+  SeqFormModule(const ExtForm<T> &E, const SeqFormParams &p);
+  virtual ~SeqFormModule();
+  
+  int Lemke(int dup = 0);
+  
+  int Add_BFS(const SFTableau<T> &B);
+  int NumPivots(void) const;
+  double Time(void) const;
+  
+  const gList<gDPVector<T> > &GetSolutions() const;
+};
+
+//
+// Convenience functions for "one-shot" evaluations
+//
+
+/*
+template <class T> int SeqForm(const ExtForm<T> &N, const SeqFormParams &p,
+			     gList<gDPVector<T> > &solutions,
+			     int &npivots, gRational &time);
+*/				   
+
+#endif    // SEQFORM_H
+
+
+
