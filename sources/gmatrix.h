d3db324d240bb0c8a99b24728525996bb8e07b76
magyar <magyar@3fbfd45b-e82e-0410-bb8e-8470640bbd7f>
1995-07-24 15:01:04 +0000

    Improved structuring of matrix and rectangular array classes

--- sources/gmatrix.h
+++ sources/gmatrix.h
@@ -19,73 +19,44 @@ template <class T> class gMatrix : public gRectArray<T>  {
     gMatrix(int rows, int cols);
     gMatrix(int rows, int cols, int minrows);
     gMatrix(int rl, int rh, int cl, int ch);
-    gMatrix(const gMatrix<T> &M);
+    gMatrix(const gMatrix<T> &);
     virtual ~gMatrix();
 
-    gMatrix<T> &operator=(const gMatrix<T> &M);
+    gMatrix<T> &operator=(const gMatrix<T> &);
 
-  // +,- operators
-  gMatrix<T> operator+(const gMatrix<T> &) const;
-  gMatrix<T> operator-(const gMatrix<T> &) const;
-  gMatrix<T> &operator+=(const gMatrix<T> &);
-  gMatrix<T> &operator-=(const gMatrix<T> &);
+       // ADDITIVE OPERATORS
+    gMatrix<T> operator+(const gMatrix<T> &) const;
+    gMatrix<T> operator-(const gMatrix<T> &) const;
+    gMatrix<T> &operator+=(const gMatrix<T> &);
+    gMatrix<T> &operator-=(const gMatrix<T> &);
 
-  // *,/ operators
-  // "in-place" column multiply
-  void CMultiply(const gVector<T>&, gVector<T>&) const;
-  // "in-place" row (transposed) multiply
-  void RMultiply(const gVector<T>&, gVector<T>&) const;
-  gMatrix<T> operator*(const gMatrix<T> &) const;
-  gVector<T> operator*(const gVector<T> &) const;
-  friend gVector<T> operator*(const gVector<T> &, const gMatrix<T>&);
-  gMatrix<T> operator*(T) const;
-  gMatrix<T> &operator*=(const gMatrix<T> &M);
-  gMatrix<T> &operator*=(T);
 
-  gMatrix<T> operator/(T) const;
-  gMatrix<T> &operator/=(T);
+       // MULTIPLICATIVE OPERATORS
+       // "in-place" column multiply
+    void CMultiply(const gVector<T> &, gVector<T> &) const;
+       // "in-place" row (transposed) multiply
+    void RMultiply(const gVector<T> &, gVector<T> &) const;
+    gMatrix<T> operator*(const gMatrix<T> &) const;
+    gVector<T> operator*(const gVector<T> &) const;
+    friend gVector<T> operator*(const gVector<T> &, const gMatrix<T> &);
+    gMatrix<T> operator*(const T &) const;
+    gMatrix<T> &operator*=(const gMatrix<T> &);
+    gMatrix<T> &operator*=(const T &);
 
-  // comparison functions
-  int operator==(const gMatrix<T> &) const;
-  int operator!=(const gMatrix<T> &M) const;
-  int operator==( T ) const;
-  int operator!=( T s ) const;
+    gMatrix<T> operator/(const T &) const;
+    gMatrix<T> &operator/=(const T &);
 
-  // manipulation functions
-  void SwitchRow(int, const gVector<T> &); // redundant (identical to SetRow)
-  void SwitchRowWithVector(int, gVector<T> &);
-  void SwitchRows(int, int);
-  gVector<T> GetRow(int) const;
-  void GetRow(int, gVector<T>&) const;
-  void SetRow(int, const gVector<T>&);
+       // COMPARISON OPERATORS
+    bool operator==(const gMatrix<T> &) const;
+    bool operator!=(const gMatrix<T> &) const;
+    bool operator==(const T &) const;
+    bool operator!=(const T &) const;
+
   void MakeIdent(void);  // set matrix to identity matrix
-
-  void SwitchColumn(int, const gVector<T> &); // redundant (identical to SetColumn)
-  void SwitchColumnWithVector(int, gVector<T> &);
-  void SwitchColumns(int, int);
-  gVector<T> GetColumn(int) const;
-  void GetColumn(int, gVector<T>&) const;
-  void SetColumn(int, const gVector<T>&);
-
   void Pivot(int, int);
-
-
-  // check functions
-    // check for correct row index
-  int CheckRow(int row) const;
-    // check row vector for correct column boundaries
-  int CheckRow(const gVector<T> &v) const;
-    // check for correct column index
-  int CheckColumn(int col) const;
-    // check column vector for correct row boundaries
-  int CheckColumn(const gVector<T> &v) const;
-    // check row and column indices
-  int Check(int row,int col) const;
-    // check matrix for same row and column boundaries
-  int CheckBounds(const gMatrix<T> &m) const;
 };
 
-template <class T> gOutput &operator<<(gOutput &to, const gMatrix<T> &M);
+template <class T> gOutput &operator<<(gOutput &, const gMatrix<T> &);
 
 #endif     // GMATRIX_H
 
--- sources/grarray.h
+++ sources/grarray.h
@@ -9,14 +9,29 @@
 
 #include "gambitio.h"
 
+template <class T> class gArray;
+
 template <class T> class gRectArray    {
   protected:
     int minrow, maxrow, mincol, maxcol;
     T **data;
 
+      // RANGE CHECKING FUNCTIONS
+      // check for correct row index
+    bool CheckRow(int row) const;
+      // check row vector for correct column boundaries
+    bool CheckRow(const gArray<T> &) const;
+      // check for correct column index
+    bool CheckColumn(int col) const;
+      // check column vector for correct row boundaries
+    bool CheckColumn(const gArray<T> &) const;
+      // check row and column indices
     bool Check(int row, int col) const;
+      // check matrix for same row and column boundaries
+    bool CheckBounds(const gRectArray<T> &) const;
 
   public:
+       // CONSTRUCTORS, DESTRUCTOR, CONSTRUCTIVE OPERATORS
     gRectArray(void);
     gRectArray(int nrows, int ncols);
     gRectArray(int minr, int maxr, int minc, int maxc);
@@ -25,24 +40,36 @@ template <class T> class gRectArray    {
 
     gRectArray<T> &operator=(const gRectArray<T> &);
 
+       // GENERAL DATA ACCESS
     int NumRows(void) const;
     int NumColumns(void) const;
     int MinRow(void) const;
     int MaxRow(void) const;
     int MinCol(void) const;
     int MaxCol(void) const;
+    virtual void Dump(gOutput &) const;
     
+       // INDEXING OPERATORS
     T &operator()(int r, int c);
     const T &operator()(int r, int c) const;
 
+       // ROW AND COLUMN ROTATION OPERATORS
     void RotateUp(int lo, int hi);
     void RotateDown(int lo, int hi);
     void RotateLeft(int lo, int hi);
     void RotateRight(int lo, int hi);
 
+       // ROW MANIPULATION FUNCTIONS
+    void SwitchRow(int, gArray<T> &);
     void SwitchRows(int, int);
+    void GetRow(int, gArray<T> &) const;
+    void SetRow(int, const gArray<T> &);
 
-    virtual void Dump(gOutput &) const;
+       // COLUMN MANIPULATION FUNCTIONS
+    void SwitchColumn(int, gArray<T> &);
+    void SwitchColumns(int, int);
+    void GetColumn(int, gArray<T> &) const;
+    void SetColumn(int, const gArray<T> &);
 };
 
 template <class T> gOutput &operator<<(gOutput &, const gRectArray<T> &);
