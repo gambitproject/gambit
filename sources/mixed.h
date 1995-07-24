832e64f449f8fa1aaf9e2d78b40259478f03e3e7
magyar <magyar@3fbfd45b-e82e-0410-bb8e-8470640bbd7f>
1995-07-24 13:06:22 +0000

    Rearranged mixed profile stuff for compilation convenience

--- sources/mixed.h
+++ sources/mixed.h
@@ -13,17 +13,19 @@ class BaseNormalForm;
 
 class BaseMixedProfile     {
   protected:
-    BaseNormalForm &N;
+    const BaseNormalForm *N;
+    bool truncated;
+
+    BaseMixedProfile(const BaseNormalForm &, bool trunc);
+    BaseMixedProfile &operator=(const BaseMixedProfile &);
 
   public:
-    BaseMixedProfile(BaseNormalForm &);
     virtual ~BaseMixedProfile();
-
-    BaseMixedProfile &operator=(const BaseMixedProfile &);
 
     DataType Type(void) const;
     virtual bool IsPure(void) const = 0;
     virtual bool IsPure(int pl) const = 0;
+    bool IsTruncated(void) const    { return truncated; }
 };
 
 
@@ -34,8 +36,8 @@ template <class T> class NormalForm;
 template <class T> class MixedProfile 
   : public BaseMixedProfile, public gPVector<T>  {
   public:
-    MixedProfile(NormalForm<T> &);
-    MixedProfile(NormalForm<T> &, const gPVector<T> &);
+    MixedProfile(const NormalForm<T> &, bool truncated = false);
+    MixedProfile(const NormalForm<T> &, const gPVector<T> &);
     ~MixedProfile();
 
     MixedProfile<T> &operator=(const MixedProfile<T> &);
@@ -43,5 +45,7 @@ template <class T> class MixedProfile
     bool IsPure(void) const;
     bool IsPure(int pl) const;
 };
+
+template <class T> gOutput &operator<<(gOutput &f, const MixedProfile<T> &);
 
 #endif    // MIXED_H
