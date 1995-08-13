1bcf804ba9e22ce6d37951e8873a9b83d6434e5e
rdm <rdm@3fbfd45b-e82e-0410-bb8e-8470640bbd7f>
1995-08-13 12:49:10 +0000

    split BFS into its own files, bfs.h and bfs.cc

--- sources/bfs.cc
+++ sources/bfs.cc
@@ -9,7 +9,37 @@
 #include "garray.imp"
 #include "gblock.imp"
 #include "gmap.imp"
-#include "gtableau.imp"
+#include "bfs.h"
+
+//--------------------------------------------------------------------------
+//                     class BFS<T>: member functions
+//--------------------------------------------------------------------------
+
+template <class T> BFS<T>::BFS(void) : gOrdMap<int, T>((T) 0)  { }
+
+template <class T> BFS<T>::BFS(const T &d) : gOrdMap<int, T>((T) d)  { }
+
+template <class T> BFS<T>::BFS(const BFS<T> &m) : gOrdMap<int, T>(m)  { }
+
+template <class T> int BFS<T>::operator==(const BFS<T> &M) const
+{
+  if (length != M.length)  return 0;
+
+  for (int i = 0; i < length; i++)
+    if (keys[i] != M.keys[i])  return 0;
+
+  return 1;
+}
+
+template <class T> int BFS<T>::operator!=(const BFS<T> &M) const
+{
+  return !(*this == M);
+}
+
+template <class T> gOutput &operator<<(gOutput &f, const BFS<T> &M)
+{
+  return f;
+}
 
 #ifdef __GNUG__
 #define TEMPLATE template
