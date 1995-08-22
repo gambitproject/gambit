6b7b2ac4c7fa00d7d213cfe54b9e59503423b57c
nelson <nelson@3fbfd45b-e82e-0410-bb8e-8470640bbd7f>
1995-08-22 15:40:05 +0000

    Initial revision

---
+++ sources/nfgiter.h
@@ -0,0 +1,56 @@
+//#
+//# FILE: nfgiter.h -- Normal form iterator class
+//#
+//# $Id$
+//#
+
+#ifndef NFGITER_H
+#define NFGITER_H
+
+template <class T> class gArray;
+template <class T> class Nfg;
+template <class T> class ContIter;
+class StrategyProfile;
+
+//
+// This class is useful for iterating around the normal form.
+//
+
+template <class T> class NfgIter    {
+private:
+  const Support *stratset;
+  Nfg<T> *N;
+  gArray<int> current_strat;
+  StrategyProfile *profile;
+  
+public:
+  NfgIter(const Support *s);
+  NfgIter(const NfgIter<T> &);
+//  NfgIter(const ContIter<T> &);
+  ~NfgIter();
+  
+  NfgIter<T> &operator=(const NfgIter<T> &);
+  
+  void First(void);
+  int Next(int p);
+  int Set(int p, int s);
+  
+  void Get(gArray<int> &t) const;
+  void Set(const gArray<int> &t);
+  
+  long GetIndex(void) const;
+  
+  const T &Payoff(int p) const;
+  void SetPayoff(int p, const T &);
+//  void Payoff(gVector<T> &value) const;
+  
+//  const gArray<int> *const GetActions(int pl) const;
+
+  Strategy *GetStrategy(int pl, int num);
+};
+
+#endif   // NFGITER_H
+
+
+
+
