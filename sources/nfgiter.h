0817d9e37aac195ee02ae56bc6e73d7a3f03e3ad
magyar <magyar@3fbfd45b-e82e-0410-bb8e-8470640bbd7f>
1995-08-23 12:07:49 +0000

    *** empty log message ***

--- sources/nfgiter.h
+++ sources/nfgiter.h
@@ -11,6 +11,7 @@ template <class T> class gArray;
 template <class T> class Nfg;
 template <class T> class ContIter;
 class StrategyProfile;
+class Support;
 
 //
 // This class is useful for iterating around the normal form.
