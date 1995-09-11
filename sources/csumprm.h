e1762dee8e09f59a98bdddde8597cb0d231ac30e
egrayver <egrayver@3fbfd45b-e82e-0410-bb8e-8470640bbd7f>
1995-09-11 23:13:26 +0000

    Initial revision

---
+++ sources/csumprm.h
@@ -0,0 +1,50 @@
+// $Id$
+#ifndef ZSUMPRM_H
+#define ZSUMPRM_H
+
+#include "algdlgs.h"
+
+class LPSolveParamsDialog : public OutputParamsDialog
+{
+private:
+	int stopAfter;
+	void SaveDefaults(void);
+public:
+	LPSolveParamsDialog(wxWindow *parent=0);
+	~LPSolveParamsDialog(void);
+	void GetParams(ZSumParams &P);
+};
+
+
+LPSolveParamsDialog::LPSolveParamsDialog(wxWindow *parent)
+														:OutputParamsDialog("LP Params",parent,LP_HELP)
+
+{
+stopAfter=0;
+wxGetResource(PARAMS_SECTION,"LP-StopAfter",&stopAfter,defaults_file);
+
+Form()->Add(wxMakeFormShort("# Equilibria",&stopAfter));
+Form()->Add(wxMakeFormNewLine());
+
+// Now add the basic stuff
+MakeOutputFields();
+Go();
+}
+
+void LPSolveParamsDialog::SaveDefaults(void)
+{
+if (!Default()) return;
+wxWriteResource(PARAMS_SECTION,"LP-StopAfter",stopAfter,defaults_file);
+}
+
+LPSolveParamsDialog::~LPSolveParamsDialog(void)
+{SaveDefaults();}
+
+void LPSolveParamsDialog::GetParams(ZSumParams &P)
+{
+P.stopAfter=stopAfter;
+// Output stuff
+P.trace=TraceLevel();P.tracefile=OutFile();
+}
+
+#endif
