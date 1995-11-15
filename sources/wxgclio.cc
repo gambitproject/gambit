12402c4d2ce2a79fc170ac0b46d521e3b03f942f
egrayver <egrayver@3fbfd45b-e82e-0410-bb8e-8470640bbd7f>
1995-11-15 17:09:31 +0000

    *** empty log message ***

--- sources/wxgclio.cc
+++ sources/wxgclio.cc
@@ -5,6 +5,7 @@
 // $Id$
 #include "wx.h"
 #pragma hdr_stop
+#include "wx_timer.h"
 #include "wxgclio.h"
 
 void gcl_main(void);
@@ -17,12 +18,26 @@ class gWxGclIoApp: public wxApp
 
 gWxGclIoApp GclIo;
 gWxGclIo gclio;
+
+class  gWxGclIoQuit:public wxTimer
+{
+private:
+	wxFrame *target;
+public:
+	gWxGclIoQuit(wxFrame *f) : target(f)
+	{Start(2000); /* 2 second delay */}
+	void Notify(void)
+	{Stop();target->OnClose();}
+};
+
+wxTimer *quit_timer=0;
 wxFrame *gWxGclIoApp::OnInit(void)
 {
 wxFrame *frame=gclio.Create();
 gclio<<"Gambit GCL / Windows interface Ready\n";
 gcl_main();
 gclio<<"*********** Gambit GCL Terminated. Close the window *************\n";
+quit_timer=new gWxGclIoQuit(frame);
 return frame;
 }
 
