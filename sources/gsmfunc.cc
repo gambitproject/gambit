90c56c2d1ab5cfea49a88636eb91daabb945312a
gwu <gwu@3fbfd45b-e82e-0410-bb8e-8470640bbd7f>
1995-12-21 23:18:59 +0000

    *** empty log message ***

--- sources/gsmfunc.cc
+++ sources/gsmfunc.cc
@@ -874,25 +874,28 @@ bool CallFuncObj::SetCurrParamIndex( const gString& param_name )
       f_index++ )
   {
     name_match_found = false;
-    for( index = 0; index < _FuncInfo[ f_index ].NumParams; index++ )
+    if( _FuncMatch[ f_index ] )
     {
-      if( _FuncInfo[ f_index ].ParamInfo[ index ].Name == param_name )
+      for( index = 0; index < _FuncInfo[ f_index ].NumParams; index++ )
       {
-	name_match_found = true;
-	if( result == PARAM_NOT_FOUND )
+	if( _FuncInfo[ f_index ].ParamInfo[ index ].Name == param_name )
 	{
-	  result = index;
-	  TempFuncIndex = f_index;
-	  times_found++;
-	}
-	else if( result == index )
-	{
-	  times_found++;
-	}
-	else // ( result != index )
-	{
-	  result = PARAM_AMBIGUOUS;
-	  break;
+	  name_match_found = true;
+	  if( result == PARAM_NOT_FOUND )
+	  {
+	    result = index;
+	    TempFuncIndex = f_index;
+	    times_found++;
+	  }
+	  else if( result == index )
+	  {
+	    times_found++;
+	  }
+	  else // ( result != index )
+	  {
+	    result = PARAM_AMBIGUOUS;
+	    break;
+	  }
 	}
       }
     }
