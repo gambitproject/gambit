f10b1438d24a8713b67232b09ec2ecd470412942
egrayver <egrayver@3fbfd45b-e82e-0410-bb8e-8470640bbd7f>
1996-05-01 22:21:02 +0000

    Initial revision

---
+++ sources/purenprm.h
@@ -0,0 +1,37 @@
+// File: purenprm.h -- parameter file for PureNash GUI.
+// $Id$
+#ifndef PURENPRM_H
+#define PURENPRM_H
+
+#include "algdlgs.h"
+
+class PureNashParamsSettings : public OutputParamsSettings
+{
+protected:
+	void SaveDefaults(void) { };
+public:
+	PureNashParamsSettings(void) { };
+	~PureNashParamsSettings() {SaveDefaults();}
+};
+
+class PureNashSolveParamsDialog : public OutputParamsDialog,public PureNashParamsSettings
+{
+public:
+	PureNashSolveParamsDialog(wxWindow *parent=0,bool subgames=false);
+};
+
+#ifdef PUREN_PRM_INST		// instantiate only once
+PureNashSolveParamsDialog::PureNashSolveParamsDialog(wxWindow *parent,bool subgames)
+														:OutputParamsDialog("PureNash Params",parent)
+
+{
+
+if (!subgames)
+	Add(wxMakeFormMessage("No parameters for PureNash"));
+else
+	MakeOutputFields(((subgames) ? SPS_FIELD : 0));
+Go();
+}
+#endif
+
+#endif
