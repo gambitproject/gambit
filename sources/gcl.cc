ac32c613f476a3f68a694ba809e0f9ccd2e136a9
egrayver <egrayver@3fbfd45b-e82e-0410-bb8e-8470640bbd7f>
1996-05-06 15:25:40 +0000

    *** empty log message ***

--- sources/gcl.cc
+++ sources/gcl.cc
@@ -5,19 +5,50 @@
 #include "ggrstack.h"
 #include "gcompile.h"
 #include <signal.h>
+#include <values.h>
+#include <math.h>
 
 typedef void (*fptr)(int);
 
 void SigFPEHandler(int a)
 {
+if (a==SIGFPE)
+	gerr<<"A floating point error has occured!  The results returned may be invalid\n";
 signal(SIGFPE, (fptr)SigFPEHandler);  //  reinstall signal handler
-gerr<<"A floating point error has occured!  The results returned may be invalid\n";
 }
-#ifdef wx_msw // if running under MsWin, different entry point (wxgclio.cc)
-void gcl_main(void)
+
+#ifdef __BORLANDC__ // this handler is defined differently windows
+int _RTLENTRY _matherr (struct exception *e)
 #else
-main()                                               
+int matherr(struct exception *e)
 #endif
+{
+char *whyS [] =
+{
+		"argument domain error",
+		"argument singularity ",
+		"overflow range error ",
+		"underflow range error",
+		"total loss of significance",
+		"partial loss of significance"
+};
+
+char errMsg[ 80 ];
+sprintf (errMsg,
+			"%s (%8g,%8g): %s\n", e->name, e->arg1, e->arg2, whyS [e->type - 1]);
+gerr<<errMsg;
+
+if (e->type == SING)
+	if (!strcmp(e->name,"log"))
+		if(e->arg1 == 0.0) {
+e->retval = LN_MINDOUBLE;
+return 1;
+		}
+
+return 0;
+}
+
+int main(int ,char **)
 {
 	// Set up the error handling functions:
 	signal(SIGFPE, (fptr)SigFPEHandler);
@@ -25,5 +56,7 @@ main()
 	GCLCompiler C;
 
 	C.Parse();
+
+	return 0;
 }
 
