e6850cbfe25f1712b2d7f399ac7c3c312d80a33f
rdm <rdm@3fbfd45b-e82e-0410-bb8e-8470640bbd7f>
1996-05-31 11:33:22 +0000

    minor changes

--- sources/enum.cc
+++ sources/enum.cc
@@ -130,16 +130,10 @@ template <class T> int EnumModule<T>::Enum(void)
 
   for( iter2.GoFirst(),iter2++; ! iter2.PastEnd(); iter2++ ) {
     bfs1 = iter2.GetValue();
-//  for(i=2;i<=v2 && !params.status.Get();i++) {
     params.status.SetProgress((double)(i-2)/(double)v2);
 //    gout << "\nProgress = " << (double)(i-2)/(double)v2;
-//    bfs1 = verts2[i];
-//    bfs1 = poly2.VertexList()[i];
     for( iter1.GoFirst(),iter1++; ! iter1.PastEnd(); iter1++ ) {
       bfs2 = iter1.GetValue();
-//    for(j=2;j<=v1;j++) {
-//      bfs2 = verts1[j];
-//      bfs2 = poly1.VertexList()[j];
       
       // check if solution is nash 
       // need only check complementarity, since it is feasible
