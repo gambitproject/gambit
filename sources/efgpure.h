b7766b6c12d63fabfd57d8e9b2e0fb60a5c83250
magyar <magyar@3fbfd45b-e82e-0410-bb8e-8470640bbd7f>
1996-07-17 09:51:32 +0000

    Initial revision

---
+++ sources/efgpure.h
@@ -0,0 +1,34 @@
+//
+// FILE: efgpure.h -- Interface to pure-strategy Nash equilibrium finder
+//
+// $Id$
+//
+
+#ifndef EFGPURE_H
+#define EFGPURE_H
+
+#include "efg.h"
+#include "behavsol.h"
+#include "glist.h"
+
+#include "subsolve.h"
+
+
+template <class T> int FindPureNash(const Efg<T> &N,
+				    gList<BehavSolution<T> > &);
+
+
+template <class T> class EfgPSNEBySubgame : public SubgameSolver<T>  {
+  private:
+    int SolveSubgame(const Efg<T> &, gList<BehavSolution<T> > &);
+    int AlgorithmID() const { return id_PURENASHSUB; }    
+
+  public:
+    EfgPSNEBySubgame(const Efg<T> &E, int max = 0);
+    virtual ~EfgPSNEBySubgame();
+};
+
+
+#endif    // EFGPURE_H
+
+
