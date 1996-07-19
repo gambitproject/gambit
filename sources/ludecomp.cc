e7b874e7aacdcb93be74ebdebb1ca8d4e44fe290
nelson <nelson@3fbfd45b-e82e-0410-bb8e-8470640bbd7f>
1996-07-19 08:51:54 +0000

    *** empty log message ***

--- sources/ludecomp.cc
+++ sources/ludecomp.cc
@@ -20,7 +20,6 @@
 TEMPLATE class EtaMatrix< double >;
 TEMPLATE class gList< EtaMatrix< double > >;
 TEMPLATE class gNode< EtaMatrix< double > >;
-TEMPLATE class gListIter< EtaMatrix< double > >;
 TEMPLATE class LUdecomp< double >;
 
 TEMPLATE gOutput& operator<<( gOutput&, const EtaMatrix< double > &); 
@@ -29,7 +28,6 @@ TEMPLATE gOutput& operator<<( gOutput&, const EtaMatrix< double > &);
 TEMPLATE class EtaMatrix< gRational >;
 TEMPLATE class gList< EtaMatrix< gRational > >;
 TEMPLATE class gNode< EtaMatrix< gRational > >;
-TEMPLATE class gListIter< EtaMatrix< gRational > >;
 TEMPLATE class LUdecomp< gRational >;
 
 TEMPLATE gOutput& operator<<( gOutput&, const EtaMatrix< gRational > &); 
