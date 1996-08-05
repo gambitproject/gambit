bcff19b705f3f30b952f81f12cc5fc7f75697b96
egrayver <egrayver@3fbfd45b-e82e-0410-bb8e-8470640bbd7f>
1996-08-05 18:33:07 +0000

    Initial revision

---
+++ sources/msolnsf.cc
@@ -0,0 +1,164 @@
+// File: msolnsf.cc -- Actual code to implement sorting and filtering of
+// mixed solutions
+
+#include "msolnsf.h"
+#include "gsmincl.h"
+/****************************************************************************
+												MIXED SOLUTION SORTER FILTER OPTIONS
+****************************************************************************/
+
+// All possible algorithms that could create a behav solution and their corresponding ids
+// Note that the indexing should start from 1, not 0
+char *MSolnSortFilterOptions::filter_cr_str[NUM_MCREATORS+1] =
+				{ "Error","PureNash","Lemke","Liap","GobitGrid","Gobit",
+					"Simpdiv","Enum","ZSum","User" };
+NfgAlgType MSolnSortFilterOptions::filter_cr_id[NUM_MCREATORS+1] =
+				{NfgAlg_USER,NfgAlg_PURENASH,NfgAlg_LEMKE,NfgAlg_LIAP,NfgAlg_GOBITGRID,NfgAlg_GOBIT,
+				NfgAlg_SIMPDIV,NfgAlg_ENUM,NfgAlg_ZSUM,NfgAlg_USER};
+char *MSolnSortFilterOptions::filter_tri_str[3+1] = {"E", "Yes", "No", "DK" };
+TriState	MSolnSortFilterOptions::filter_tri_id[3+1] = {T_DONTKNOW, T_YES,T_NO,T_DONTKNOW};
+// Constructor
+MSolnSortFilterOptions::MSolnSortFilterOptions(void):filter_cr(NUM_MCREATORS),
+												filter_nash(3),filter_perfect(3),filter_proper(3)
+{
+sort_by=MSORT_BY_ID;
+int i;
+for (i=1;i<=NUM_MCREATORS;i++) filter_cr[i]=true;
+for (i=1;i<=3;i++)
+	{filter_nash[i]=true;filter_perfect[i]=true;filter_proper[i]=true;}
+}
+
+MSolnSortFilterOptions &MSolnSortFilterOptions::operator=(const MSolnSortFilterOptions &o)
+{
+sort_by=o.sort_by;filter_cr=o.filter_cr;filter_nash=o.filter_nash;
+filter_perfect=o.filter_perfect;filter_proper=o.filter_proper;
+return (*this);
+}
+
+MSolnSortFilterOptions::~MSolnSortFilterOptions()
+{ }
+// Data Access
+int &MSolnSortFilterOptions::SortBy(void) {return sort_by;}
+int	MSolnSortFilterOptions::SortBy(void) const {return sort_by;}
+gArray<bool> &MSolnSortFilterOptions::FilterCr(void) {return filter_cr;}
+const gArray<bool> &MSolnSortFilterOptions::FilterCr(void) const {return filter_cr;}
+gArray<bool> &MSolnSortFilterOptions::FilterNash(void) {return filter_nash;}
+const gArray<bool> &MSolnSortFilterOptions::FilterNash(void) const {return filter_nash;}
+gArray<bool> &MSolnSortFilterOptions::FilterPerfect(void) {return filter_perfect;}
+const gArray<bool> &MSolnSortFilterOptions::FilterPerfect(void) const {return filter_perfect;}
+gArray<bool> &MSolnSortFilterOptions::FilterProper(void) {return filter_proper;}
+const gArray<bool> &MSolnSortFilterOptions::FilterProper(void) const {return filter_proper;}
+
+
+/****************************************************************************
+												BEHAV SOLUTION SORTER FILTER
+****************************************************************************/
+// Constructor
+template <class T>
+MSolnSorterFilter<T>::MSolnSorterFilter(gSortList<MixedSolution<T> > &solns,MSolnSortFilterOptions &options_):
+	gListSorter<MixedSolution<T> >(solns),gListFilter<MixedSolution<T> >(solns),
+	options(options_)
+{
+Filter();
+Sort();
+}
+
+// Filtering function
+template <class T>
+bool MSolnSorterFilter<T>::Passes(const MixedSolution<T> &a) const
+{
+int i;
+for (i=1;i<=NUM_MCREATORS;i++)
+	if (options.filter_cr_id[i]==a.Creator() && !options.FilterCr()[i])
+		return false;
+for (i=1;i<=3;i++)
+{
+	if (options.filter_tri_id[i]==a.IsNash() && !options.FilterNash()[i])
+		return false;
+	if (options.filter_tri_id[i]==a.IsPerfect() && !options.FilterPerfect()[i])
+		return false;
+	if (options.filter_tri_id[i]==a.IsProper() && !options.FilterProper()[i])
+		return false;
+}
+return true;
+}
+
+// Sorting function
+template <class T>
+CompareResult MSolnSorterFilter<T>::Compare(const MixedSolution<T> &a,const MixedSolution<T> &b) const
+{
+switch (options.SortBy())
+{
+case MSORT_BY_ID:
+			return Equal;
+case MSORT_BY_CREATOR:
+			if (NameNfgAlgType(a.Creator())>NameNfgAlgType(b.Creator())) return GreaterThan;
+			if (NameNfgAlgType(a.Creator())<NameNfgAlgType(b.Creator())) return LessThan;
+			return Equal;
+case MSORT_BY_NASH:
+			if (a.IsNash()==b.IsNash())
+				return Equal;
+			else
+				if (a.IsNash()>b.IsNash())
+					return GreaterThan;
+				else
+					return LessThan;
+case MSORT_BY_PERFECT:
+			if (a.IsPerfect()==b.IsPerfect())
+				return Equal;
+			else
+				if (a.IsPerfect()>b.IsPerfect())
+					return GreaterThan;
+				else
+					return LessThan;
+case MSORT_BY_PROPER:
+			if (a.IsProper()==b.IsProper())
+				return Equal;
+			else
+				if (a.IsProper()>b.IsProper())
+					return GreaterThan;
+				else
+					return LessThan;
+case MSORT_BY_GVALUE:
+			if (a.GobitValue()==b.GobitValue())
+				return Equal;
+			else
+				if (a.GobitValue()>b.GobitValue())
+					return GreaterThan;
+				else
+					return LessThan;
+case MSORT_BY_GLAMBDA:
+			if (a.GobitLambda()==b.GobitLambda())
+				return Equal;
+			else
+				if (a.GobitLambda()>b.GobitLambda())
+					return GreaterThan;
+				else
+					return LessThan;
+case MSORT_BY_LVALUE:
+			if (a.LiapValue()==b.LiapValue())
+				return Equal;
+			else
+				if (a.LiapValue()>b.LiapValue())
+					return GreaterThan;
+				else
+					return LessThan;
+default: assert(0 && "Unknown sort criterion");
+}
+return Equal;
+}
+
+#ifdef __GNUG__
+#define TEMPLATE template
+#elif defined __BORLANDC__
+#pragma option -Jgd
+#define TEMPLATE
+#endif   // __GNUG__, __BORLANDC__
+#include "rational.h"
+#include "gslist.imp"
+TEMPLATE class gSortList<MixedSolution<double> >;
+TEMPLATE class gSortList<MixedSolution<gRational> >;
+
+TEMPLATE class MSolnSorterFilter<double>;
+TEMPLATE class MSolnSorterFilter<gRational>;
+
---
+++ sources/msolnsf.h
@@ -0,0 +1,65 @@
+// File: bsolnsf.h -- Behav Solution Sorter and Filter
+// $Id$
+#ifndef MSOLNSF_H
+#define MSOLNSF_H
+#include "garray.h"
+#include "gslist.h"
+#include "mixedsol.h"
+// Sorting and filtering options
+class MSolnSortFilterOptions
+{
+private:
+	int sort_by;
+	gArray<bool> filter_cr,filter_nash,filter_perfect,filter_proper;
+	char *defaults_file;
+public:
+// these variables are mostly for GUI use.  they may not belong here
+	static char *filter_cr_str[];
+	static NfgAlgType filter_cr_id[];
+	static char *filter_tri_str[];
+	static TriState filter_tri_id[];
+// Contructor
+	MSolnSortFilterOptions(void);
+	MSolnSortFilterOptions &operator=(const MSolnSortFilterOptions &o);
+	virtual ~MSolnSortFilterOptions();
+// Data access, must have a const version to avoid errors/warnings
+	int &SortBy(void);
+	int	SortBy(void) const;
+	gArray<bool> &FilterCr(void);
+	const gArray<bool> &FilterCr(void) const;
+	gArray<bool> &FilterNash(void);
+	const gArray<bool> &FilterNash(void) const;
+	gArray<bool> &FilterPerfect(void);
+	const gArray<bool> &FilterPerfect(void) const;
+	gArray<bool> &FilterProper(void);
+	const gArray<bool> &FilterProper(void) const;
+};
+
+// Implement sorting and filtering
+template <class T>
+class MSolnSorterFilter:public gListSorter<MixedSolution<T> >,
+												public gListFilter<MixedSolution<T> >
+{
+private:
+	MSolnSortFilterOptions &options;
+protected:
+	// Overload the comparison functions
+	bool Passes(const MixedSolution<T> &a) const;
+	CompareResult Compare(const MixedSolution<T> &a,const MixedSolution<T> &b) const;
+public:
+	MSolnSorterFilter(gSortList<MixedSolution<T> > &solns,MSolnSortFilterOptions &options_);
+};
+
+#define		MSOLN_SHOW_SECT	"Mixed-Soln-Show"
+#define		NUM_MCREATORS		9
+
+#define		MSORT_BY_ID				1		// this order must correspond to that in GUI dialogs
+#define		MSORT_BY_CREATOR	2
+#define		MSORT_BY_NASH			3
+#define		MSORT_BY_PERFECT	4
+#define		MSORT_BY_PROPER			5
+#define		MSORT_BY_GVALUE		6
+#define		MSORT_BY_GLAMBDA	7
+#define		MSORT_BY_LVALUE		8
+
+#endif
