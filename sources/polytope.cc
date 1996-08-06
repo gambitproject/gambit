0cc2b8a4fc9bf8cb2561607647fdc2e4212ec0cc
mclennan <mclennan@3fbfd45b-e82e-0410-bb8e-8470640bbd7f>
1996-08-06 13:17:56 +0000

    Initial revision

---
+++ sources/polytope.cc
@@ -0,0 +1,33 @@
+//
+// FILE: polytope.cc -- Instantiation of gPolytope class
+//
+// @(#)polytope.cc	1.26 06 Aug 1996
+//
+
+#include "polytope.imp"
+#include "rational.h"
+
+#ifdef __GNUG__
+#define TEMPLATE template
+#elif defined __BORLANDC__
+#define TEMPLATE
+#pragma option -Jgd
+#endif   // __GNUG__, __BORLANDC__
+
+#include "glist.imp"
+TEMPLATE class gList<index_pair*>;
+TEMPLATE class gNode<index_pair*>;
+TEMPLATE class gList<gVector<int> >;
+TEMPLATE class gNode<gVector<int> >;
+TEMPLATE class gList<gVector<gRational> >;
+TEMPLATE class gNode<gVector<gRational> >;
+
+TEMPLATE class gPolytope<int>;
+TEMPLATE gOutput &operator<<(gOutput &f, const gPolytope<int> &y);
+
+TEMPLATE class gPolytope<gRational>;
+TEMPLATE gOutput &operator<<(gOutput &f, const gPolytope<gRational> &y);
+
+
+
+
