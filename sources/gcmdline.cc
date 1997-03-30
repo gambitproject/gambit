5e88d8176a23a51779d48d844d0aea6baa99b116
gwu <gwu@3fbfd45b-e82e-0410-bb8e-8470640bbd7f>
1997-03-30 13:54:31 +0000

    Initial revision

---
+++ sources/gcmdline.cc
@@ -0,0 +1,511 @@
+
+
+#include <assert.h>
+#include <unistd.h>
+#include <ctype.h>
+#include "gcmdline.h"
+
+
+int gCmdLineInput::s_NumInstances = 0;
+struct termios gCmdLineInput::s_TermAttr;
+
+
+void gCmdLineInput::SaveTermAttr( void )
+{
+#ifdef __GNUG__
+  // save the current terminal attributes
+  tcgetattr( STDIN_FILENO, &s_TermAttr );
+#endif // __GNUG__
+}
+
+void gCmdLineInput::RestoreTermAttr( void )
+{
+#ifdef __GNUG__
+  // restore the terminal attributes to the original
+  tcsetattr( STDIN_FILENO, TCSANOW, &s_TermAttr );
+#endif // __GNUG__
+}
+
+
+
+void gCmdLineInput::SetRawTermAttr( void )
+{
+#ifdef __GNUG__
+  struct termios rawTerm;
+  tcgetattr( STDIN_FILENO, &rawTerm );
+
+  // system( "stty raw -echo opost" );
+  /*
+     stty [-]raw 
+     Same  as  -ignbrk  -brkint  -ignpar  -parmrk -inpck
+     -istrip -inlcr -igncr -icrnl  -ixon  -ixoff  -iuclc
+     -ixany  -imaxbel  -opost -isig -icanon -xcase min 1
+     time 0.  With `-', same as cooked.
+     */
+
+  rawTerm.c_iflag &= ~IGNBRK;
+  rawTerm.c_iflag &= ~BRKINT;
+  rawTerm.c_iflag &= ~IGNBRK;
+  rawTerm.c_iflag &= ~PARMRK;
+  rawTerm.c_iflag &= ~INPCK;
+  rawTerm.c_iflag &= ~ISTRIP;
+  rawTerm.c_iflag &= ~INLCR;
+  rawTerm.c_iflag &= ~IGNCR;
+  rawTerm.c_iflag &= ~ICRNL;
+  rawTerm.c_iflag &= ~IXON;
+  rawTerm.c_iflag &= ~IXOFF;
+  rawTerm.c_iflag &= ~IUCLC;
+  rawTerm.c_iflag &= ~IXANY;
+  rawTerm.c_iflag &= ~IMAXBEL;
+
+  rawTerm.c_oflag |= OPOST;
+
+  rawTerm.c_lflag &= ~ISIG;
+  rawTerm.c_lflag &= ~ICANON;
+  rawTerm.c_lflag &= ~XCASE;
+  rawTerm.c_lflag &= ~ECHO;
+  
+  rawTerm.c_cc[VTIME] = 0;
+  rawTerm.c_cc[VMIN] = 1;
+  tcsetattr( STDIN_FILENO, TCSANOW, &rawTerm );
+
+
+#endif // __GNUG__
+}
+
+
+
+
+
+gCmdLineInput::gCmdLineInput( int historyDepth )
+: m_HistoryDepth( historyDepth ),
+  m_Verbose( true ),
+  m_Prompt( true )
+{
+  if( s_NumInstances == 0 )
+  {
+    SaveTermAttr();
+    SetRawTermAttr();
+  }
+  ++s_NumInstances;
+}
+
+gCmdLineInput::~gCmdLineInput()
+{
+  --s_NumInstances;
+  if( s_NumInstances == 0 )
+    RestoreTermAttr();
+}
+
+
+
+void gCmdLineInput::GetCmdExec( void )
+{
+  // number of invokations thus far
+  static int numInvoke = 0;
+
+  int i = 0;
+
+  if( m_CmdExec.length() > 0 )
+    return;
+
+
+  // set up the editable prompt
+  char buf[16];
+  if( m_Prompt )
+  {
+    if( m_Verbose )
+    {
+      if( numInvoke < m_HistoryDepth )
+	sprintf( buf, "GCL%d:= << ", numInvoke );
+      else
+	sprintf( buf, "GCL%d:=; GCL%d:= << ",
+		numInvoke - m_HistoryDepth, 
+		numInvoke );
+    }
+    else
+    {
+      if( numInvoke < m_HistoryDepth )
+	sprintf( buf, "GCL%d:= ", numInvoke );
+      else
+	sprintf( buf, "GCL%d:=; GCL%d:= ",
+		numInvoke - m_HistoryDepth, 
+		numInvoke );
+    }
+    ++numInvoke;
+  }
+  else
+    sprintf( buf, "" );
+  
+
+  gString cmdBuf = buf;
+  gString cmdBufOld;
+  int curPos = cmdBuf.length();
+  char c = 0;
+
+  int historyPos = m_History.Length() + 1;
+
+  gout << cmdBuf;
+
+  for( ; ; ) // infinite loop
+  {
+    assert( 0 <= curPos );
+    assert( curPos <= cmdBuf.length() );
+    
+    gin.get( c );
+    if( c == EOF || c == '\r' || c == '\n' )
+      break;
+
+    if( c == 27 ) // escape sequences
+    {
+      EscapeCode code = GetEscapeSequence();
+      switch( code )
+      {
+      case ESC_LEFT:
+	if( curPos > 0 )
+	{
+	  --curPos;
+	  gout << '\b';
+	}
+	else // can't move left, beep
+	  gout << '\a';
+	break;
+
+      case ESC_RIGHT:
+	if( curPos < cmdBuf.length() )
+	{
+	  gout << cmdBuf[curPos];
+	  ++curPos;
+	}
+	else // can't move right, beep
+	  gout << '\a';
+	break;
+
+      case ESC_UP:
+	if( historyPos > 1 )
+	{
+	  // clear the current line
+#ifdef USE_CR
+	  gout << '\r';
+#else
+	  for( i = 0; i < curPos; ++i )
+	    gout << '\b';
+#endif
+	  for( i = 0; i < cmdBuf.length(); ++i )
+	    gout << ' ';
+#ifndef USE_CR
+	  for( i = 0; i < cmdBuf.length(); ++i )
+	    gout << '\b';
+#endif
+
+	  // save the latest line
+	  if( historyPos > m_History.Length() )
+	    cmdBufOld = cmdBuf;
+
+	  --historyPos;
+	  cmdBuf = m_History[historyPos];
+	  curPos = cmdBuf.length();
+	  
+	  // display the new line
+#ifdef USE_CR
+	  gout << '\r';
+#endif
+	  gout << cmdBuf;
+	}
+	else // can't go up, beep
+	  gout << '\a';
+	break;
+
+      case ESC_DOWN:
+	if( historyPos <= m_History.Length() )
+	{
+	  // clear the current line
+#ifdef USE_CR
+	  gout << '\r';
+#else
+	  for( i = 0; i < curPos; ++i )
+	    gout << '\b';
+#endif
+	  for( i = 0; i < cmdBuf.length(); ++i )
+	    gout << ' ';
+#ifndef USE_CR
+	  for( i = 0; i < cmdBuf.length(); ++i )
+	    gout << '\b';
+#endif
+
+	  ++historyPos;
+	  if( historyPos <= m_History.Length() )
+	    cmdBuf = m_History[historyPos];
+	  else // restore the latest line
+	    cmdBuf = cmdBufOld;
+	  curPos = cmdBuf.length();
+	  
+	  // display the new line
+#ifdef USE_CR
+	  gout << '\r';
+#endif
+	  gout << cmdBuf;
+	}
+	else // can't go up, beep
+	  gout << '\a';
+	break;
+
+      case ESC_DELETE:
+	if( curPos < cmdBuf.length() )
+	{
+	  cmdBuf.remove( curPos );
+
+	  // print the entire string after the current cursor position
+	  gout << cmdBuf.right( cmdBuf.length() - curPos );
+	  gout << ' '; // this to erase the last character
+
+	  // reposition the cursor
+	  for( i = 0; i < cmdBuf.length() - curPos + 1; ++i )
+	    gout << '\b';
+	}
+	else // nothing to delete, beep
+	  gout << '\a';
+	break;
+
+      default: // bad escape sequence, beep
+	gout << '\a';
+      }
+    }
+    else if( c == 'R' - 'A' + 1 ) // ^R, refreshes the line
+    {
+      gout << "^R\n";
+      gout << cmdBuf;
+      curPos = cmdBuf.length();
+    }
+    else if( c == '\b' || c == 127 ) // backspace
+    {
+      if( curPos > 0 )
+      {
+	cmdBuf.remove( curPos - 1 );
+	--curPos;
+
+	// print the entire string after the current cursor position
+	gout << '\b';
+	gout << cmdBuf.right( cmdBuf.length() - curPos );
+	gout << ' '; // this to erase the last character
+
+	// reposition the cursor
+	for( i = 0; i < cmdBuf.length() - curPos + 1; ++i )
+	  gout << '\b';
+      }
+      else // nothing to delete, beep
+	gout << '\a';
+    }
+    else if( isprint( c ) )// normal characters
+    {
+      cmdBuf.insert( c, curPos );
+      ++curPos;
+      
+      // print the entire string after the current cursor position
+      gout << cmdBuf.right( cmdBuf.length() - curPos + 1 );
+      
+      // reposition the cursor
+      for( i = 0; i < cmdBuf.length() - curPos; ++i )
+	gout << '\b';
+    }
+  }
+
+  if( historyPos < m_History.Length() + 1 )
+    --numInvoke;
+
+  m_History.Append( cmdBuf );
+  m_CmdExec = cmdBuf + '\n';
+  gout << '\n';
+
+  // if the history is too long now, get rid of the first one
+  if( m_History.Length() > m_HistoryDepth )
+    m_History.Remove( 1 );
+  
+}
+
+
+
+
+
+
+
+gCmdLineInput::EscapeCode gCmdLineInput::GetEscapeSequence( void ) const
+{
+  char c1 = 0;
+  char c2 = 0;
+  char c3 = 0;
+  
+  // remember that the first Escape has already been caught
+
+  // the second char must be '[' in an escape sequence
+  if( !gin.eof() )
+    gin.get( c1 );
+  if( c1 != '[' )
+    return ESC_ERROR;
+
+  
+  if( !gin.eof() )
+    gin.get( c2 );
+  switch( c2 )
+  {
+  case 65: // up arrow
+    return ESC_UP;
+  case 66: // down arrow
+    return ESC_DOWN;
+  case 68: // left arrow
+    return ESC_LEFT;
+  case 67: // right arrow
+    return ESC_RIGHT;
+
+  case 51: // delete key, if followed by 126
+    if( !gin.eof() )
+      gin.get( c3 );
+    if( c3 == 126 )
+      return ESC_DELETE;
+    else
+      return ESC_ERROR;
+    break;
+
+  default:
+    return ESC_ERROR;
+  }
+
+  
+}
+
+
+
+void gCmdLineInput::EatSpace( int num )
+{
+  assert( num <= m_CmdExec.length() );
+  int i = 0;
+  for( i = 0; i < num; ++i )
+    m_CmdExec.remove( 0 );
+}
+
+
+
+
+
+gInput& gCmdLineInput::operator >> (int &x)
+{
+  int tokens = 0;
+  while( tokens == 0 )
+  {
+    GetCmdExec();
+    assert( m_CmdExec.length() > 0 );
+    
+    int num = 0;
+    tokens = sscanf( m_CmdExec.stradr(), "%d%n", &x, &num );
+    EatSpace( num );
+  }
+  return *this; 
+}
+
+gInput& gCmdLineInput::operator >> (unsigned int &x)
+{
+  int tokens = 0;
+  while( tokens == 0 )
+  {
+    GetCmdExec();
+    assert( m_CmdExec.length() > 0 );
+    
+    int num = 0;
+    tokens = sscanf( m_CmdExec.stradr(), "%d%n", &x, &num );
+    EatSpace( num );
+  }
+  return *this; 
+}
+
+gInput& gCmdLineInput::operator >> (long &x)
+{
+  int tokens = 0;
+  while( tokens == 0 )
+  {
+    GetCmdExec();
+    assert( m_CmdExec.length() > 0 );
+    
+    int num = 0;
+    tokens = sscanf( m_CmdExec.stradr(), "%ld%n", &x, &num );
+    EatSpace( num );
+  }
+  return *this; 
+}
+
+gInput& gCmdLineInput::operator >> (char &x) 
+{
+  int tokens = 0;
+  while( tokens == 0 )
+  {
+    GetCmdExec();
+    assert( m_CmdExec.length() > 0 );
+    
+    int num = 0;
+    tokens = sscanf( m_CmdExec.stradr(), "%c%n", &x, &num );
+    EatSpace( num );
+  }
+  return *this; 
+}
+
+gInput& gCmdLineInput::operator >> (double &x)
+{
+  int tokens = 0;
+  while( tokens == 0 )
+  {
+    GetCmdExec();
+    assert( m_CmdExec.length() > 0 );
+    
+    int num = 0;
+    tokens = sscanf( m_CmdExec.stradr(), "%lf%n", &x, &num );
+    EatSpace( num );
+  }
+  return *this; 
+}
+
+gInput& gCmdLineInput::operator >> (float &x)
+{
+  int tokens = 0;
+  while( tokens == 0 )
+  {
+    GetCmdExec();
+    assert( m_CmdExec.length() > 0 );
+    
+    int num = 0;
+    tokens = sscanf( m_CmdExec.stradr(), "%f%n", &x, &num );
+    EatSpace( num );
+  }
+  return *this; 
+}
+
+gInput& gCmdLineInput::operator >> (char *x)
+{
+  int tokens = 0;
+  while( tokens == 0 )
+  {
+    GetCmdExec();
+    assert( m_CmdExec.length() > 0 );
+    
+    int num = 0;
+    tokens = sscanf( m_CmdExec.stradr(), "%s%n", x, &num );
+    EatSpace( num );
+  }
+  return *this; 
+}
+
+
+
+gCmdLineInput _gcmdline( 20 );
+gCmdLineInput& gcmdline( _gcmdline );
+
+
+
+#ifdef __GNUG__
+#define TEMPLATE template
+#elif defined __BORLANDC__
+#pragma option -Jgd
+#define TEMPLATE
+#endif   // __GNUG__, __BORLANDC__
+
+#include "gstack.imp"
+
+TEMPLATE class gStack< bool >;
---
+++ sources/gcmdline.h
@@ -0,0 +1,135 @@
+
+
+#ifndef __gcmdline_h__
+#define __gcmdline_h__
+
+
+#include <stdlib.h>
+#include <termios.h>
+
+#include "gambitio.h"
+#include "gstring.h"
+#include "glist.h"
+#include "gstack.h"
+
+
+class gCmdLineInput : public gInput
+{
+private:
+  // the number of instances of thie object
+  static int s_NumInstances;
+
+  // the saved term attributes
+  static struct termios s_TermAttr;
+
+public:
+  static void SaveTermAttr( void );
+  static void RestoreTermAttr( void );
+  static void SetRawTermAttr( void );
+  
+
+private:
+  // the history of commands already executed
+  gList< gString > m_History;
+  
+  // the maximum history size
+  int m_HistoryDepth;
+
+  // the last command being executed
+  gString m_CmdExec;
+
+  // whether EOF has been reached
+  bool m_EOF;
+
+  bool m_Verbose;
+  bool m_Prompt;
+  gStack< bool > m_PromptStack;
+
+private:
+  gCmdLineInput( const gCmdLineInput& );
+  operator = ( const gCmdLineInput& );
+
+  enum EscapeCode
+  {
+    ESC_ERROR,
+
+    ESC_UP,
+    ESC_DOWN,
+    ESC_LEFT,
+    ESC_RIGHT,
+
+    ESC_DELETE
+  };
+
+  EscapeCode GetEscapeSequence( void ) const;
+
+  // if the last executed command had been used up,
+  //   wait until a new line has come in
+  void GetCmdExec( void );
+
+  // this will eat up the given number of characters
+  //   from the start of m_CmdExec
+  void EatSpace( int num );
+
+
+public:
+  gCmdLineInput( int historyDepth );
+  virtual ~gCmdLineInput();
+
+
+  virtual gInput& operator>>(int &x);
+  virtual gInput& operator>>(unsigned int &x);
+  virtual gInput& operator>>(long &x);
+  virtual gInput& operator>>(char &x);
+  virtual gInput& operator>>(double &x);
+  virtual gInput& operator>>(float &x);
+  virtual gInput& operator>>(char *x);
+  
+
+  virtual int get(char &c) 
+  { 
+    operator >> ( c ); 
+    return !eof(); 
+  }
+  virtual void unget(char c) 
+  {
+    m_CmdExec.insert( c, 0 ); 
+  }
+
+  virtual bool eof(void) const { return m_EOF; }
+  virtual void seekp(long /* x */) const { }
+  virtual long getpos(void) const { return 0; }
+  virtual void setpos(long /* x */) const { }
+  
+  virtual bool IsValid(void) const { return !eof(); }
+
+  void SetVerbose( bool verbose ) { m_Verbose = verbose; }
+  bool Verbose( void ) { return m_Verbose; }
+
+  void SetPrompt( bool prompt ) 
+  {    
+    if( prompt == false )
+      m_PromptStack.Push( false );
+    else
+      m_PromptStack.Pop();
+    
+    if( m_PromptStack.Depth() == 0 )
+      m_Prompt = true; 
+    else
+      m_Prompt = false;
+  }
+  bool Prompt( void ) { return m_Prompt; }
+
+  void ResetPrompt( void )
+  {
+    m_PromptStack.Flush();
+    m_Prompt = true;
+  }
+  
+};
+
+
+
+extern gCmdLineInput &gcmdline;
+
+#endif // __gcmdline_h__
