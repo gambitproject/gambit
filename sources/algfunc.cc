c860a54c70d54ae602556a745fc21994d25d9412
geoff <geoff@3fbfd45b-e82e-0410-bb8e-8470640bbd7f>
1997-07-01 12:23:42 +0000

    Added NewVertEnum info

--- sources/algfunc.cc
+++ sources/algfunc.cc
@@ -1,7 +1,7 @@
 //
 // FILE: algfunc.cc -- Solution algorithm functions for GCL
 //
-// $Id$
+// @(#)algfunc.cc	2.16 6/23/97
 //
 
 #include "gsm.h"
@@ -1109,8 +1109,8 @@ Portion* GSM_VertEnum_Float( Portion** param )
   if( start->Length() == 0 )
     vertenum = new VertEnum< double >( *A, *b );
   else 
-    vertenum = new VertEnum< double >( *A, *b, *start );
-
+    /*vertenum = new VertEnum< double >( *A, *b, *start );*/
+    gout << "Please use NewVertEnum for this purpose.\n\n";
   gList< gVector< double > > verts;
   vertenum->Vertices( verts );
 
@@ -1140,7 +1140,8 @@ Portion* GSM_VertEnum_Rational( Portion** param )
   if( start->Length() == 0 )
     vertenum = new VertEnum< gRational >( *A, *b );
   else 
-    vertenum = new VertEnum< gRational >( *A, *b, *start );
+    /*vertenum = new VertEnum< gRational >( *A, *b, *start );*/
+    gout << "Please use NewVertEnum for this purpose.\n\n";
 
   gList< gVector< gRational > > verts;
   vertenum->Vertices( verts );
@@ -1160,6 +1161,71 @@ Portion* GSM_VertEnum_Rational( Portion** param )
   return list;
 }
 
+
+
+
+Portion* GSM_NewVertEnum_Float( Portion** param )
+{
+  gMatrix<double>* A = ListToMatrix_Float((ListPortion*) param[0]);
+  gVector<double>* b = ListToVector_Float((ListPortion*) param[1]);
+  gVector<double>* start = ListToVector_Float((ListPortion*) param[2]);
+
+  NewVertEnum< double >* vertenum = NULL;
+  if( start->Length() == 0 )
+    /*vertenum = new NewVertEnum< double >( *A, *b );*/
+    gout << "Please use VertEnum for this.\n\n";
+  else 
+    vertenum = new NewVertEnum< double >( *A, *b, *start );
+
+  gList< gVector< double > > verts;
+  vertenum->Vertices( verts );
+
+  delete A;
+  delete b;
+  delete start;
+  delete vertenum;
+
+  ListPortion* list = new ListValPortion();
+  int i = 0;
+  for( i = 1; i <= verts.Length(); ++i )
+  {
+    list->Append( ArrayToList( verts[i] ) );
+  }
+
+  return list;
+}
+
+
+Portion* GSM_NewVertEnum_Rational( Portion** param )
+{
+  gMatrix<gRational>* A = ListToMatrix_Rational((ListPortion*) param[0]);
+  gVector<gRational>* b = ListToVector_Rational((ListPortion*) param[1]);
+  gVector<gRational>* start = ListToVector_Rational((ListPortion*) param[2]);
+
+  NewVertEnum< gRational >* vertenum = NULL;
+  if( start->Length() == 0 )
+    /*vertenum = new NewVertEnum< gRational >( *A, *b );*/
+    gout << "Please use VertEnum for this.\n\n";
+  else 
+    vertenum = new NewVertEnum< gRational >( *A, *b, *start );
+
+  gList< gVector< gRational > > verts;
+  vertenum->Vertices( verts );
+
+  delete A;
+  delete b;
+  delete start;
+  delete vertenum;
+
+  ListPortion* list = new ListValPortion();
+  int i = 0;
+  for( i = 1; i <= verts.Length(); ++i )
+  {
+    list->Append( ArrayToList( verts[i] ) );
+  }
+
+  return list;
+}
 
 
 
@@ -1607,8 +1673,29 @@ void Init_algfunc(GSM *gsm)
 					    new ListValPortion(), BYVAL));
   gsm->AddFunction(FuncObj);
 
-}
 
+
+  FuncObj = new FuncDescObj("NewVertEnum", 2);
+  FuncObj->SetFuncInfo(0, FuncInfoType(GSM_NewVertEnum_Float, 
+				       PortionSpec(porFLOAT, 2), 3));
+  FuncObj->SetParamInfo(0, 0, ParamInfoType("A", PortionSpec(porFLOAT,2),
+					    REQUIRED, BYVAL));
+  FuncObj->SetParamInfo(0, 1, ParamInfoType("b", PortionSpec(porFLOAT,1),
+					    REQUIRED, BYVAL));
+  FuncObj->SetParamInfo(0, 2, ParamInfoType("start", PortionSpec(porFLOAT,1),
+					    new ListValPortion(), BYVAL));
+
+  FuncObj->SetFuncInfo(1, FuncInfoType(GSM_NewVertEnum_Rational, 
+				       PortionSpec(porRATIONAL, 2), 3));
+  FuncObj->SetParamInfo(1, 0, ParamInfoType("A", PortionSpec(porRATIONAL,2),
+					    REQUIRED, BYVAL));
+  FuncObj->SetParamInfo(1, 1, ParamInfoType("b", PortionSpec(porRATIONAL,1),
+					    REQUIRED, BYVAL));
+  FuncObj->SetParamInfo(1, 2, ParamInfoType("start", PortionSpec(porRATIONAL,1),
+					    new ListValPortion(), BYVAL));
+  gsm->AddFunction(FuncObj);
+
+}
 
 #ifdef __GNUG__
 #define TEMPLATE template
