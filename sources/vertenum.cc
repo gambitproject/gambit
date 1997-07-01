ee43a4e90975da252e3a82674f8285c73f7bf3be
geoff <geoff@3fbfd45b-e82e-0410-bb8e-8470640bbd7f>
1997-07-01 15:14:52 +0000

    Added NewVertEnum stuff.

--- sources/vertenum.cc
+++ sources/vertenum.cc
@@ -1,13 +1,22 @@
 //#
 //# FILE: vertenum.cc -- Instantiation of vertex enumerator
 //#
-//# $Id$
+//# @(#)vertenum.cc	2.1 3/24/97
 //#
 
 #include "vertenum.imp"
 #include "rational.h"
 
+#ifdef __GNUG__
+#define TEMPLATE template
+#elif defined __BORLANDC__
+#define TEMPLATE
+#pragma option -Jgd
+#endif   // __GNUG__, __BORLANDC__
 
-template class VertEnum<double>;
-template class VertEnum<gRational>;
+TEMPLATE class VertEnum<double>;
+TEMPLATE class VertEnum<gRational>;
+
+TEMPLATE class NewVertEnum<double>;
+TEMPLATE class NewVertEnum<gRational>;
 
