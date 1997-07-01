49b0057ffa08397a27aa720e4ae263758bfeea4b
rdm <rdm@3fbfd45b-e82e-0410-bb8e-8470640bbd7f>
1997-07-01 21:44:05 +0000

    changes for BC

--- sources/gsmatrix.cc
+++ sources/gsmatrix.cc
@@ -1,24 +1,18 @@
 //#
 //# FILE: gsmatrix.cc -- Instantiation of common squarematrix types
 //#
-//# @(#)gsmatrix.cc	1.1 7/19/95
+//# $Id$ 
 //#
 
 #include "gsmatrix.imp"
 #include "rational.h"
 
-#ifdef __GNUG__
-#define TEMPLATE template
-#elif defined __BORLANDC__
-#define TEMPLATE
-#pragma option -Jgd
-#endif   // __GNUG__, __BORLANDC__
 
-// TEMPLATE class gSquareMatrix<float>;
-TEMPLATE class gSquareMatrix<double>;
-TEMPLATE class gSquareMatrix<gRational>;
+// template class gSquareMatrix<float>;
+template class gSquareMatrix<double>;
+template class gSquareMatrix<gRational>;
 
-// TEMPLATE gOutput & operator<< (gOutput&, const gSquareMatrix<float>&);
-TEMPLATE gOutput & operator<< (gOutput&, const gSquareMatrix<double>&);
-TEMPLATE gOutput & operator<< (gOutput&, const gSquareMatrix<gRational>&);
+// template gOutput & operator<< (gOutput&, const gSquareMatrix<float>&);
+template gOutput & operator<< (gOutput&, const gSquareMatrix<double>&);
+template gOutput & operator<< (gOutput&, const gSquareMatrix<gRational>&);
 
--- sources/vertenum.cc
+++ sources/vertenum.cc
@@ -1,22 +1,15 @@
 //#
 //# FILE: vertenum.cc -- Instantiation of vertex enumerator
 //#
-//# @(#)vertenum.cc	2.1 3/24/97
+//#  $Id$
 //#
 
 #include "vertenum.imp"
 #include "rational.h"
 
-#ifdef __GNUG__
-#define TEMPLATE template
-#elif defined __BORLANDC__
-#define TEMPLATE
-#pragma option -Jgd
-#endif   // __GNUG__, __BORLANDC__
+template class VertEnum<double>;
+template class VertEnum<gRational>;
 
-TEMPLATE class VertEnum<double>;
-TEMPLATE class VertEnum<gRational>;
-
-TEMPLATE class NewVertEnum<double>;
-TEMPLATE class NewVertEnum<gRational>;
+template class NewVertEnum<double>;
+template class NewVertEnum<gRational>;
 
