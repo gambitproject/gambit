2ee02655b828b0d2a8980b5eea96edabe09ffeac
rdm <rdm@3fbfd45b-e82e-0410-bb8e-8470640bbd7f>
1998-01-28 11:20:10 +0000

    reinstated class, added operator[] for gArray<int>

--- sources/gnarray.h
+++ sources/gnarray.h
@@ -26,7 +26,7 @@ template <class T> class gNArray   {
     gArray<int> dim;
 
     void DumpFrom(gOutput &, int, gVector<int> &) const;
-    void ReadFrom(gInput &, const gVector<int> &, gVector<int> &, int);
+//    void ReadFrom(gInput &, const gVector<int> &, gVector<int> &, int);
 
   public:
     gNArray(void);
@@ -36,15 +36,20 @@ template <class T> class gNArray   {
 
     gNArray<T> &operator=(const gNArray<T> &);
 
+    /* not used for now
     T operator[](const gVector<int> &) const;
     T &operator[](const gVector<int> &);
+    */
+
+    T operator[](const gArray<int> &) const;
+    T &operator[](const gArray<int> &);
 
     const T &operator[](long l) const;
     T &operator[](long l);
 
     const gArray<int> &Dimensionality(void) const;
 
-    void Input(gInput &, const gVector<int> &, int);
+//    void Input(gInput &, const gVector<int> &, int);
     void Output(gOutput &) const;
 };
 
