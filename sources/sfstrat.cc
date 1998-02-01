e07dbcd0f8016797291da3bd6549714f0755ff91
rdm <rdm@3fbfd45b-e82e-0410-bb8e-8470640bbd7f>
1998-02-01 18:56:39 +0000

    Initial revision

---
+++ sources/sfstrat.cc
@@ -0,0 +1,273 @@
+//
+// FILE: sfstrat.cc -- Implementation of strategy representation for sfg
+//
+// $Id$
+//
+
+#include "sfstrat.h"
+#include "sfg.h"
+
+//--------------------------------------
+// Sequence:  Member functions
+//--------------------------------------
+
+#include "glist.imp"
+#include "gblock.imp"
+#include "garray.imp"
+
+gList<Action *> Sequence::History(void) const 
+{ 
+  gList<Action *> h;
+  Action * a = action;
+  const Sequence * s = (this);
+  while(a) {
+    h.Append(a);
+    s = s->parent;
+    a = s->GetAction();
+  }
+  return h;    
+}
+
+void Sequence::Dump(gOutput &out) const
+{
+  int a = 0, p = 0;
+  if(action) a = action->GetNumber();
+  if(parent) p = parent->GetNumber();
+  out << "\nPl#: " << player->GetNumber() << " Seq# " << number << " act# " << a << " parent: " << p;
+}
+
+gOutput& operator<<(gOutput& s, const Sequence& seq)
+{
+  seq.Dump(s);
+  return s;
+}
+
+
+//--------------------------------------
+// SFSequenceSet:  Member functions
+//--------------------------------------
+
+SFSequenceSet::SFSequenceSet(const EFPlayer *p)
+  : efp(p), sequences()
+{
+  Sequence *empty;
+  empty = new Sequence(p,0,0,1);
+  AddSequence(empty);
+}
+
+SFSequenceSet::SFSequenceSet(const SFSequenceSet &s)
+  : efp(s.efp), sequences(s.sequences)
+{ }
+
+SFSequenceSet::~SFSequenceSet()
+{ 
+
+  // potential problem here?  It is not clear this is where this belongs.  
+  // What if there are multiple SFSequenceSets pointing to 
+  // the same sequences?
+
+  for(int i=1;i<=sequences.Length();i++)
+    delete sequences[i];   
+}
+
+SFSequenceSet &SFSequenceSet::operator=(const SFSequenceSet &s)
+{
+  if (this != &s) {
+    efp = s.efp;
+    sequences = s.sequences;
+  }
+  return *this;
+}
+
+
+bool SFSequenceSet::operator==(const SFSequenceSet &s)
+{
+  if (sequences.Length() != s.sequences.Length()) return (false);
+  int i;
+  for (i = 1; i <= sequences. Length() 
+       && sequences[i] == s.sequences[i]; i++);
+  if (i > sequences.Length()) return (true);
+  else return (false);
+}
+
+//------------------------------------------
+// SFSequenceSet: Member functions 
+//------------------------------------------
+
+// Append a sequences to the SFSequenceSet
+void SFSequenceSet::AddSequence(Sequence *s) 
+{ 
+  assert (efp == s->Player());
+  sequences.Append(s); 
+}
+
+// Removes a sequence pointer. Returns true if the sequence was successfully
+// removed, false otherwise.
+bool SFSequenceSet::RemoveSequence( Sequence *s ) 
+{ 
+  assert (efp == s->Player());
+  int t; 
+  t = sequences.Find(s); 
+  if (t>0) sequences.Remove(t); 
+  return (t>0); 
+} 
+
+// Number of Sequences in a SFSequenceSet
+int SFSequenceSet::NumSequences(void) const
+{
+  return (sequences.Length());
+}
+
+// Return the entire sequence set
+const gBlock<Sequence *> &SFSequenceSet::GetSFSequenceSet(void) const
+{
+  return sequences;
+}
+
+//-----------------------------------------------
+// SFSupport: Ctors, Dtor, Operators
+//-----------------------------------------------
+
+SFSupport::SFSupport(const Sfg &SF) : bsfg(&SF), sups(SF.GetEfg().NumPlayers())
+{ 
+  for (int i = 1; i <= sups.Length(); i++)
+    sups[i] = new SFSequenceSet(SF.GetEfg().Players()[i]);
+}
+
+SFSupport::SFSupport(const SFSupport &s)
+  : bsfg(s.bsfg), sups(s.sups.Length())
+{
+  for (int i = 1; i <= sups.Length(); i++)
+    sups[i] = new SFSequenceSet(*s.sups[i]);
+}
+
+SFSupport::~SFSupport()
+{ 
+  for (int i = 1; i <= sups.Length(); i++)
+    delete sups[i];
+}
+
+SFSupport &SFSupport::operator=(const SFSupport &s)
+{
+  if (this != &s && bsfg == s.bsfg) {
+    for (int i = 1; i <= sups.Length(); i++)  {
+      delete sups[i];
+      sups[i] = new SFSequenceSet(*s.sups[i]);
+    }
+  }
+  return *this;
+}
+
+bool SFSupport::operator==(const SFSupport &s) const
+{
+  assert(sups.Length() == s.sups.Length());
+  int i;
+  for (i = 1; i <= sups.Length() && *sups[i] == *s.sups[i]; i++);
+  if (i > sups.Length()) return (true);
+  else return (false);
+}
+  
+bool SFSupport::operator!=(const SFSupport &s) const
+{
+  return !(*this == s);
+}
+
+//------------------------
+// SFSupport: Members
+//------------------------
+
+const gBlock<Sequence *> &SFSupport::Sequences(int pl) const
+{
+  return (sups[pl]->GetSFSequenceSet());
+}
+
+int SFSupport::NumSequences(int pl) const
+{
+  return sups[pl]->NumSequences();
+}
+
+const gArray<int> SFSupport::NumSequences(void) const
+{
+  gArray<int> a(sups.Length());
+
+  for (int i = 1 ; i <= a.Length(); i++)
+    a[i] = sups[i]->NumSequences();
+  return a;
+}
+
+int SFSupport::TotalNumSequences(void) const
+{
+  int total = 0;
+  for (int i = 1 ; i <= sups.Length(); i++)
+    total += sups[i]->NumSequences();
+  return total;
+}
+
+int SFSupport::Find(Sequence *s) const
+{
+  return sups[s->Player()->GetNumber()]->GetSFSequenceSet().Find(s);
+}
+
+void SFSupport::AddSequence(Sequence *s)
+{
+  sups[s->Player()->GetNumber()]->AddSequence(s);
+}
+
+bool SFSupport::RemoveSequence(Sequence *s)
+{
+  return sups[s->Player()->GetNumber()]->RemoveSequence(s);
+}
+
+
+// Returns true if all sequences in _THIS_ belong to _S_
+bool SFSupport::IsSubset(const SFSupport &s) const
+{
+  assert(sups.Length() == s.sups.Length());
+  for (int i = 1; i <= sups.Length(); i++)
+    if (NumSequences(i) > s.NumSequences(i))
+      return false;
+    else  {
+      const gBlock<Sequence *> &strats =
+        sups[i]->GetSFSequenceSet();
+
+      for (int j = 1; j <= NumSequences(i); j++)
+	if (!s.sups[i]->GetSFSequenceSet().Find(strats[j]))
+	  return false;
+    }
+  return true;
+}
+
+
+void SFSupport::Dump(gOutput&s) const
+{
+  int numplayers;
+  int i;
+  int j;
+  gArray<Sequence *> strat;
+
+  s << "{ ";
+  numplayers = (*bsfg).GetEfg().NumPlayers();
+  for( i = 1; i <= numplayers; i++ )
+  {
+    s << "{ ";
+    strat = Sequences( i );
+    for (j = 1; j <= strat.Length(); j++ )
+    {
+      s << "\"" << strat[ j ]->GetName() << "\" ";
+    }
+    s << "} ";
+  }
+  s << "} ";
+}
+
+gOutput& operator<<(gOutput& s, const SFSupport& n)
+{
+  n.Dump(s);
+  return s;
+}
+
+template class gList<Action *>;
+template class gNode<Action *>;
+template class gBlock<Sequence *>;
+template class gArray<Sequence *>;
+template class gArray<SFSequenceSet *>;
---
+++ sources/sfstrat.h
@@ -0,0 +1,143 @@
+//
+// FILE: sfstrat.h -- Declaration of Sequence Form strategy data types
+//
+// $Id$
+//
+
+#ifndef SFSTRAT_H
+#define SFSTRAT_H
+
+#include "gstring.h"
+#include "gblock.h"
+#include "gambitio.h"
+#include "glist.h"
+#include "efg.h"
+
+/*
+class EFPlayer;
+class Efg;
+class Sfg;
+class Action;
+class Infoset;
+*/
+
+struct Sequence {
+friend class Sfg;
+friend class Efg;
+friend class SFSequenceSet;
+private:
+  int number;
+  gString name;
+  const EFPlayer *player;
+  Action *action;
+  const Sequence *parent;
+  
+  Sequence(const EFPlayer *pl, Action *a, const Sequence *p, int n) 
+    : number(n), player(pl), action(a), parent(p) { }
+  ~Sequence() { }
+public:
+  const gString &GetName(void) const   { return name; }
+  void SetName(const gString &s)       { name = s; }
+  
+  gList<Action *> History(void) const;
+  int GetNumber(void) const        { return number; }
+  Action *GetAction(void) const  {return action; }
+  const Infoset *GetInfoset(void) const   { if(action) return action->BelongsTo();return 0; }
+  const EFPlayer *Player(void) const  { return player; }
+  const Sequence *Parent(void) const   { return parent; }
+  void Dump(gOutput &) const;
+};
+
+class SFSequenceSet {
+protected:
+  const EFPlayer *efp;
+  gBlock <Sequence *> sequences;
+  
+public:
+  SFSequenceSet(const SFSequenceSet &s); 
+  SFSequenceSet(const EFPlayer *p);
+  
+  SFSequenceSet &operator=(const SFSequenceSet &s); 
+  bool operator==(const SFSequenceSet &s);
+
+  virtual ~SFSequenceSet();
+
+  // Append a sequence to the SFSequenceSet
+  void AddSequence(Sequence *s);
+
+  // Removes a sequence pointer. Returns true if the sequence was successfully
+  // removed, false otherwise.
+  bool RemoveSequence( Sequence *s ); 
+
+  // Number of sequences in the SFSequenceSet
+  int NumSequences(void) const;
+
+  //  return the entire sequence set in a const gArray
+  const gBlock<Sequence *> &GetSFSequenceSet(void) const;
+};
+
+
+class SFSupport {
+protected:
+  const Sfg *bsfg;
+  gArray <SFSequenceSet *> sups;
+  
+public:
+  SFSupport(const Sfg &);
+  SFSupport(const SFSupport &s); 
+  virtual ~SFSupport();
+  SFSupport &operator=(const SFSupport &s);
+
+  bool operator==(const SFSupport &s) const;
+  bool operator!=(const SFSupport &s) const;
+
+  const Sfg &Game(void) const   { return *bsfg; }
+  
+  const gBlock<Sequence *> &Sequences(int pl) const;
+
+  int NumSequences(int pl) const;
+  const gArray<int> NumSequences(void) const;
+  int TotalNumSequences(void) const;
+
+  void AddSequence(Sequence *);
+  bool RemoveSequence(Sequence *);
+  
+  bool IsSubset(const SFSupport &s) const;
+
+  // returns the index of the sequence in the support if it exists,
+  // otherwise returns zero
+  int Find(Sequence *) const; 
+
+  void Dump(gOutput &) const;
+};
+
+class SequenceProfile   {
+  friend class Sfg;
+private:
+  long index;
+  gArray<Sequence *> profile;
+  
+public:
+  SequenceProfile(const Sfg &);
+  SequenceProfile(const SequenceProfile &p);
+
+  ~SequenceProfile();
+  
+  SequenceProfile &operator=(const SequenceProfile &);
+  
+  bool IsValid(void) const; 
+  
+  long GetIndex(void) const;
+  
+  Sequence *const operator[](int p) const;
+  Sequence *const Get(int p) const;
+  void Set(int p, Sequence  *const s);
+};
+
+
+gOutput &operator<<(gOutput &f, const Sequence &);
+gOutput &operator<<(gOutput &f, const SFSupport &);
+
+#endif    // SFSTRAT_H
+
+
