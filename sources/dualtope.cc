71c1acaccc85f263e6dbcbe1fe557dbbc33b65c3
mclennan <mclennan@3fbfd45b-e82e-0410-bb8e-8470640bbd7f>
1998-02-07 21:58:24 +0000

    serviceable and tested

--- sources/dualtope.cc
+++ sources/dualtope.cc
@@ -6,22 +6,7 @@
 
 #include "rational.h"
 #include "dualtope.imp"
-
-#ifdef __GNUG__
-#define TEMPLATE template
-#elif defined __BORLANDC__
-#define TEMPLATE
-#pragma option -Jgd
-#endif   // __GNUG__, __BORLANDC__
-
 #include "glist.imp"
-TEMPLATE class gList<ind_pair*>;
-TEMPLATE class gNode<ind_pair*>;
-TEMPLATE class gList<gVector<int> >;
-TEMPLATE class gNode<gVector<int> >;
-//TEMPLATE class gList<gVector<gRational> >;
-//TEMPLATE class gNode<gVector<gRational> >;
-
 
 template class gHalfSpc<gRational>;
 template gOutput &operator<<(gOutput&, const gHalfSpc<gRational>&);
@@ -29,17 +14,10 @@ template gOutput &operator<<(gOutput&, const gHalfSpc<gRational>&);
 template class gList<gHalfSpc<gRational> >;
 template class gNode<gHalfSpc<gRational> >;
 
-template class gDualTpe<gRational>;
-template gOutput &operator<<(gOutput&, const gDualTpe<gRational>&);
+template class gDualTope<gRational>;
+template gOutput &operator<<(gOutput&, const gDualTope<gRational>&);
 
 
-
-
-TEMPLATE class gPolytope<int>;
-TEMPLATE gOutput &operator<<(gOutput &f, const gPolytope<int> &y);
-
-TEMPLATE class gPolytope<gRational>;
-TEMPLATE gOutput &operator<<(gOutput &f, const gPolytope<gRational> &y);
 
 
 
