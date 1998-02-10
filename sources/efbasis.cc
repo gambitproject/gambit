db4116a606904c8171a8850107e9d0c8fb161c0b
rdm <rdm@3fbfd45b-e82e-0410-bb8e-8470640bbd7f>
1998-02-10 15:22:11 +0000

    Initial revision

---
+++ sources/efbasis.cc
@@ -0,0 +1,386 @@
+//
+// FILE: efbasis.cc -- Implementation of EFBasis class
+//
+// $Id$ 
+//
+
+#include "efbasis.h"
+#include "garray.imp"
+
+class EFNodeArrays   {
+  friend class EFNodeSet;
+protected:
+  gBlock<Node *> nodes;
+
+public:
+  EFNodeArrays ( const gArray <Node *> &a);
+  EFNodeArrays ( const EFNodeArrays &a);
+  virtual ~EFNodeArrays();
+  EFNodeArrays &operator=( const EFNodeArrays &a);
+  bool operator==( const EFNodeArrays &a) const;
+};
+
+//----------------------------------------------------
+// EFNodeArray: Constructors, Destructor, operators
+// ---------------------------------------------------
+
+EFNodeArrays::EFNodeArrays(const gArray<Node *> &n)
+  : nodes(n.Length())
+{
+  for (int i = 1; i <= nodes.Length(); i++)
+    nodes[i] = n[i];
+ }
+
+EFNodeArrays::EFNodeArrays(const EFNodeArrays &n)
+  : nodes(n.nodes)
+{ }
+
+EFNodeArrays::~EFNodeArrays ()
+{ }
+
+EFNodeArrays &EFNodeArrays::operator=( const EFNodeArrays &n)
+{
+  nodes = n.nodes; 
+  return *this;
+}
+
+#ifdef __BORLANDC__
+bool operator==(const gArray<Node *> &a, const gArray<Node *> &b)
+{
+  if (a.mindex != b.mindex || a.maxdex != b.maxdex)   return false;
+  for (int i = a.mindex; i <= a.maxdex; i++)
+    if (a[i] != b[i])   return false;
+  return true;
+}
+#endif
+
+bool EFNodeArrays::operator==(const EFNodeArrays &a) const
+{
+  return (nodes == a.nodes);
+}
+
+class EFNodeSet{
+
+protected:
+  EFPlayer *efp;
+  gArray < EFNodeArrays *> infosets;
+public:
+  
+  //----------------------------------------
+  // Constructors, Destructor, operators
+  //----------------------------------------
+
+//  EFNodeSet();
+  EFNodeSet(const EFNodeSet &);
+  EFNodeSet(EFPlayer &);
+  virtual ~EFNodeSet();
+
+  EFNodeSet &operator=(const EFNodeSet &);
+  bool operator==(const EFNodeSet &s) const;
+
+  //--------------------
+  // Member Functions
+  //--------------------
+
+  // Append a Node to an infoset;
+  void AddNode(int iset, Node *);
+
+  // Insert a Node in a particular place in an infoset;
+  void AddNode(int iset, Node *, int index);
+
+
+  // Remove a Node at int i, returns the removed Node pointer
+  Node *RemoveNode(int iset, int i);
+
+  // Remove a Node from an infoset . 
+  // Returns true if the Node was successfully removed, false otherwise.
+  bool RemoveNode(int iset, Node *);
+
+  // Get a garray of the Nodes in an Infoset
+  const gArray<Node *> &NodeList(int iset) const
+     { return infosets[iset]->nodes; }
+  
+  // Get a Node
+  Node *GetNode(int iset, int index);
+
+  // returns the index of the Node if it is in the NodeSet
+  int Find(Node *) const;
+
+  // Number of Nodes in a particular infoset
+  int NumNodes(int iset) const;
+
+  // return the EFPlayer of the EFNodeSet
+  EFPlayer &GetPlayer(void) const;
+
+  // checks for a valid EFNodeSet
+  bool IsValid(void) const;
+
+};
+
+//--------------------------------------------------
+// EFNodeSet: Constructors, Destructor, operators
+//--------------------------------------------------
+
+EFNodeSet::EFNodeSet(EFPlayer &p)
+  : infosets(p.NumInfosets())
+{
+  efp = &p;
+  for (int i = 1; i <= p.NumInfosets(); i++){
+    infosets[i] = new EFNodeArrays(p.Infosets()[i]->Members());
+  }
+}
+
+EFNodeSet::EFNodeSet( const EFNodeSet &s )
+: infosets(s.infosets.Length())
+{
+  efp = s.efp;
+  for (int i = 1; i <= s.infosets.Length(); i++){
+    infosets[i] = new EFNodeArrays(*(s.infosets[i]));
+  }
+}
+
+EFNodeSet::~EFNodeSet()
+{ 
+  for (int i = 1; i <= infosets.Length(); i++)
+    delete infosets[i];
+}
+
+EFNodeSet &EFNodeSet::operator=(const EFNodeSet &s)
+{
+  if (this != &s && efp == s.efp) {
+    for (int i = 1; i<= infosets.Length(); i++)  {
+      delete infosets[i];
+      infosets[i] = new EFNodeArrays(*(s.infosets[i]));
+    }
+  }    
+  return *this;
+}
+
+bool EFNodeSet::operator==(const EFNodeSet &s) const
+{
+  if (infosets.Length() != s.infosets.Length() ||
+      efp != s.efp)
+    return false;
+  
+  int i;
+  for (i = 1; i <= infosets.Length() && 
+       *(infosets[i]) == *(s.infosets[i]);  i++);
+  return (i > infosets.Length());
+}
+
+//------------------------------------------
+// EFNodeSet: Member functions 
+//------------------------------------------
+
+// Append a Node to a particular infoset;
+void EFNodeSet::AddNode(int iset, Node *s) 
+{ 
+  infosets[iset]->nodes.Append(s); 
+}
+
+// Insert a Node  to a particular infoset at a particular place;
+void EFNodeSet::AddNode(int iset, Node *s, int index) 
+{ 
+  infosets[iset]->nodes.Insert(s,index); 
+}
+
+// Remove a Node from infoset iset at int i, 
+// returns the removed Infoset pointer
+Node* EFNodeSet::RemoveNode(int iset, int i) 
+{ 
+  return (infosets[iset]->nodes.Remove(i)); 
+}
+
+// Removes a Node from infoset iset . Returns true if the 
+//Node was successfully removed, false otherwise.
+bool EFNodeSet::RemoveNode(int  iset, Node *s ) 
+{ 
+  int t = infosets[iset]->nodes.Find(s); 
+  if (t>0) infosets[iset]->nodes.Remove(t); 
+  return (t>0); 
+} 
+
+// Get a Node
+Node *EFNodeSet::GetNode(int iset, int index)
+{
+  return (infosets[iset]->nodes)[index];
+}
+
+// Number of Nodes in a particular infoset
+int EFNodeSet::NumNodes(int iset) const
+{
+  return (infosets[iset]->nodes.Length());
+}
+
+// Return the EFPlayer of this EFNodeSet
+EFPlayer &EFNodeSet::GetPlayer(void) const
+{
+  return (*efp);
+}
+
+int EFNodeSet::Find(Node *n) const
+{
+  return (infosets[n->GetInfoset()->GetNumber()]->nodes.Find(n));
+}
+
+// checks for a valid EFNodeSet
+bool EFNodeSet::IsValid(void) const
+{
+  if (infosets.Length() != efp->NumInfosets())   return false;
+
+  for (int i = 1; i <= infosets.Length(); i++)
+    if (infosets[i]->nodes.Length() == 0)   return false;
+
+  return true;
+}
+
+//--------------------------------------------------
+// EFBasis: Constructors, Destructors, Operators
+//--------------------------------------------------
+
+EFBasis::EFBasis(const Efg &E) : EFSupport(E), nodes(E.NumPlayers())
+{
+  for (int i = 1; i <= nodes.Length(); i++)
+    nodes[i] = new EFNodeSet(*(E.Players()[i]));
+}
+
+EFBasis::EFBasis(const EFBasis &b)
+  : EFSupport(b), nodes(b.nodes.Length())
+{
+  for (int i = 1; i <= nodes.Length(); i++)
+    nodes[i] = new EFNodeSet(*(b.nodes[i]));
+}
+
+EFBasis::~EFBasis()
+{
+  for (int i = 1; i <= nodes.Length(); i++)
+    delete nodes[i];
+}
+
+EFBasis &EFBasis::operator=(const EFBasis &b)
+{
+  EFSupport::operator=(b);
+  return *this;
+}
+
+bool EFBasis::operator==(const EFBasis &b) const
+{
+  if( (*this).EFSupport::operator!=(b)) return false;
+
+  if (nodes.Length() != b.nodes.Length()) return false;
+
+  int i;
+  for (i = 1; i <= nodes.Length() && *(nodes[i]) == *(b.nodes[i]); i++);
+  return (i > nodes.Length());
+}
+
+bool EFBasis::operator!=(const EFBasis &b) const
+{
+  return !(*this == b);
+}
+
+//-----------------------------
+// EFBasis: Member Functions 
+//-----------------------------
+
+int EFBasis::NumNodes(int pl, int iset) const
+{
+  return nodes[pl]->NumNodes(iset);
+}
+
+const gArray<Node *> &EFBasis::Nodes(int pl, int iset) const
+{
+  return nodes[pl]->NodeList(iset);
+}
+
+int EFBasis::Find(Node *n) const
+{
+  if (n->GetInfoset()->Game() != befg)   return 0;
+
+  int pl = n->GetInfoset()->GetPlayer()->GetNumber();
+
+  return nodes[pl]->Find(n);
+}
+
+bool EFBasis::IsValid(void) const
+{
+  if(!(*this).EFSupport::IsValid()) return false;
+  if (nodes.Length() != befg->NumPlayers())   return false;
+  for (int i = 1; i <= nodes.Length(); i++)
+    if (!nodes[i]->IsValid())  return false;
+
+  return true;
+}
+
+gPVector<int> EFBasis::NumNodes(void) const
+{
+  gArray<int> foo(befg->NumPlayers());
+  int i;
+  for (i = 1; i <= befg->NumPlayers(); i++)
+    foo[i] = nodes[i]->GetPlayer().NumInfosets();
+
+  gPVector<int> bar(foo);
+  for (i = 1; i <= befg->NumPlayers(); i++)
+    for (int j = 1; j <= nodes[i]->GetPlayer().NumInfosets(); j++)
+      bar(i, j) = NumNodes(i,j);
+
+  return bar;
+}  
+
+bool EFBasis::RemoveNode(Node *n)
+{
+  Infoset *infoset = n->GetInfoset();
+  EFPlayer *player = infoset->GetPlayer();
+
+  return nodes[player->GetNumber()]->RemoveNode(infoset->GetNumber(), n);
+}
+
+void EFBasis::AddNode(Node *n)
+{
+  Infoset *infoset = n->GetInfoset();
+  EFPlayer *player = infoset->GetPlayer();
+
+  nodes[player->GetNumber()]->AddNode(infoset->GetNumber(), n);
+}
+
+bool EFBasis::IsConsistent() const
+{
+
+}
+
+
+void EFBasis::Dump(gOutput& s) const
+{
+  int numplayers;
+  int i;
+  int j;
+  int k;
+
+  (*this).EFSupport::Dump(s);
+  s << "{ ";
+  numplayers = befg->NumPlayers();
+  for (i = 1; i <= numplayers; i++)  {
+    EFPlayer& player = nodes[i]->GetPlayer();
+    s << '"' << player.GetName() << "\" { ";
+    for (j = 1; j <= player.NumInfosets(); j++)  {
+      Infoset* infoset = player.Infosets()[j];
+      s << '"' << infoset->GetName() << "\" { ";
+      for (k = 1; k <= NumNodes(i, j); k++)  {
+	Node* Node = nodes[i]->NodeList(j)[k];
+	s << '"' << Node->GetName() << "\" ";
+      }
+      s << "} ";
+    }
+    s << "} ";
+  }
+  s << "} ";
+}
+
+gOutput& operator<<(gOutput&s, const EFBasis& e)
+{
+  e.Dump(s);
+  return s;
+}
+
+template class gArray<EFNodeSet *>;
+template class gArray<EFNodeArrays *>;
---
+++ sources/efbasis.h
@@ -0,0 +1,49 @@
+//
+// FILE: efbasis.h -- Declaration of EFBasis class
+//
+// $Id$ 
+//
+
+#ifndef EFBASIS_H
+#define EFBASIS_H
+
+#include "gtext.h"
+#include "gblock.h"
+#include "gambitio.h"
+#include "glist.h"
+#include "efg.h"
+
+class EFNodeSet;
+
+class EFBasis : public EFSupport {
+protected:
+  gArray <EFNodeSet *> nodes;
+  
+public:
+  EFBasis(const Efg &);
+  EFBasis(const EFBasis &b); 
+  virtual ~EFBasis();
+  EFBasis &operator=(const EFBasis &b);
+
+  bool operator==(const EFBasis &b) const;
+  bool operator!=(const EFBasis &b) const;
+
+  int NumNodes(int pl, int iset) const;
+  gPVector<int> NumNodes(void) const;
+
+  bool RemoveNode(Node *);
+  void AddNode(Node *);
+
+  // Returns the position of the node in the support.  Returns zero
+  // if it is not there.
+  int Find(Node *) const;
+
+  const gArray<Node *> &Nodes(int pl, int iset) const;
+
+  bool IsValid(void) const;
+  bool IsConsistent() const;
+  void Dump(gOutput &) const;
+};
+
+#endif // EFBASIS_H
+
