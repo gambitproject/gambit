577681e4469fdac9de4da4229e863cbb838a0143
mclennan <mclennan@3fbfd45b-e82e-0410-bb8e-8470640bbd7f>
1998-02-14 13:04:09 +0000

    additional members

--- sources/rectangl.h
+++ sources/rectangl.h
@@ -5,6 +5,7 @@
 //
 
 #include "interval.h"
+#include "odometer.h"
 #include "gvector.h"
 
 /* This file provides the template class
@@ -33,17 +34,24 @@ public:
     bool          operator != (const gRectangle<T>& y) const;
 
     // information
-    const int           Dmnsn()                          const;
-    const gVector<T>    LowerBound()                     const;
-    const gVector<T>    UpperBound()                     const;
-    const gInterval<T>  CartesianFactor(const int&)      const;
-    const gRectangle<T> SameCenterDoubleSideLengths()    const;
-    const gRectangle<T> Orthant(const gArray<int>&)      const;
-    const gVector<T>    SideLengths()                    const;
-    bool                Contains(const gVector<T> &)     const;
-    bool                Contains(const gRectangle<T> &)  const;
-    const T             Volume()                         const;
-    const gVector<T>    Center()                         const;
+    const int                Dmnsn()                          const;
+    const gVector<T>         LowerBound()                     const;
+    const gVector<T>         UpperBound()                     const;
+    const T                  LowerBoundOfCoord(const int&)    const;
+    const T                  UpperBoundOfCoord(const int&)    const;
+    const T                  HeightInCoord(const int&)        const;
+    const gInterval<T>       CartesianFactor(const int&)      const;
+    const gRectangle<T>      SameCenterDoubleSideLengths()    const;
+    const gRectangle<T>      Orthant(const gArray<int>&)      const;
+    const gVector<T>         SideLengths()                    const;
+    bool                     Contains(const gVector<T> &)     const;
+    bool                     Contains(const gRectangle<T> &)  const;
+    const T                  Volume()                         const;
+    const gVector<T>         Center()                         const;
+    const gRectangle<T>      BoundingRectangle()              const;
+    const gList<gVector<T> > VertexList()                     const;
+    const int                NumberOfCellsInSubdivision()     const;
+    const gRectangle<T>      SubdivisionCell(const int&)      const;
 
     // output
 friend gOutput& operator << (gOutput& output, const gRectangle<T>& x);
