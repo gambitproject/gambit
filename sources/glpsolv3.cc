e6888c7e50e7f18919e9c41f976d50587c481840
mclennan <mclennan@3fbfd45b-e82e-0410-bb8e-8470640bbd7f>
1998-02-15 13:02:15 +0000

    add?\127?\127

--- sources/glpsolv3.cc
+++ sources/glpsolv3.cc
@@ -8,15 +8,9 @@
 #include "rational.h"
 #include "glpsolve.imp"
 
-#ifdef __GNUG__
-#define TEMPLATE template
-#elif defined __BORLANDC__
-#define TEMPLATE
-#pragma option -Jgd
-#endif   // __GNUG__, __BORLANDC__
 
-TEMPLATE class gLPTableau3<double>;
-TEMPLATE class gLPTableau3<gDouble>;
-TEMPLATE class gLPTableau3<gRational>;
+template class gLPTableau3<double>;
+template class gLPTableau3<gDouble>;
+template class gLPTableau3<gRational>;
 
 
