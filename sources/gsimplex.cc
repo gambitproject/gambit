e4349ac7baffdd406bfc428016ed0179d6b280e2
mclennan <mclennan@3fbfd45b-e82e-0410-bb8e-8470640bbd7f>
1998-02-17 20:46:14 +0000

    instantiations are now such that everything compiles

--- sources/gsimplex.cc
+++ sources/gsimplex.cc
@@ -13,18 +13,18 @@
 
 template class gSimplex<gRational>;
 template class gList< gSimplex<gRational> >;
-template class gNode< gSimplex<gRational> >;
+//template class gNode< gSimplex<gRational> >;
 template gOutput& operator << (gOutput& output, const gSimplex<gRational>& x);
 template gSimplex<gDouble> TogDouble(const gSimplex<gRational>&);
 
 template class gSimplex<double>;
 template class gList< gSimplex<double> >;
-template class gNode< gSimplex<double> >;
+//template class gNode< gSimplex<double> >;
 template gOutput& operator << (gOutput& output, const gSimplex<double>& x);
 template gSimplex<gDouble> TogDouble(const gSimplex<double>&);
 
 template class gSimplex<gDouble>;
 template class gList< gSimplex<gDouble> >;
-template class gNode< gSimplex<gDouble> >;
+//template class gNode< gSimplex<gDouble> >;
 template gOutput& operator << (gOutput& output, const gSimplex<gDouble>& x);
 template gSimplex<gDouble> TogDouble(const gSimplex<gDouble>&);
