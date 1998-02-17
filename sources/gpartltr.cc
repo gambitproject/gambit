46c799e9a04e109b07812d4fe6179ae7b8085c51
mclennan <mclennan@3fbfd45b-e82e-0410-bb8e-8470640bbd7f>
1998-02-17 22:17:33 +0000

    Initial revision

---
+++ sources/gpartltr.cc
@@ -0,0 +1,33 @@
+//
+// File: gpartltr.cc  
+//        -- Instantiations of classes 
+// 
+// @(#)gpartltr.cc	1.1 1/7/98
+//
+
+#include "gpartltr.imp"
+//#include "double.h"
+#include "glist.imp"
+
+template class TreeOfPartials<gDouble>;
+template gOutput &operator<<(gOutput &f, const TreeOfPartials<gDouble> &y);
+template class gList<TreeOfPartials<gDouble> >;
+template class ListOfPartialTrees<gDouble>;
+template gOutput &operator<<(gOutput &f, 
+			     const ListOfPartialTrees<gDouble> &y);
+
+template class TreeOfPartials<gRational>;
+template gOutput &operator<<(gOutput &f, const TreeOfPartials<gRational> &y);
+template class gList<TreeOfPartials<gRational> >;
+template class ListOfPartialTrees<gRational>;
+template gOutput &operator<<(gOutput &f, 
+			     const ListOfPartialTrees<gRational> &y);
+
+
+template class TreeOfPartials<double>;
+template gOutput &operator<<(gOutput &f, const TreeOfPartials<double> &y);
+template class gList<TreeOfPartials<double> >;
+template class ListOfPartialTrees<double>;
+template gOutput &operator<<(gOutput &f, 
+			     const ListOfPartialTrees<double> &y);
+
---
+++ sources/gpartltr.h
@@ -0,0 +1,104 @@
+//
+// File: gpartltr.h  --  Declaration of TreeOfPartials and ListOfPartialTrees
+// 
+// $Id$
+//
+
+#ifndef GPARTLTR_H
+#define GPARTLTR_H
+
+//#include "gsolver.h"
+//#include "odometer.h"
+//#include "rectangl.h"
+#include "gtree.h"
+#include "gpoly.h"
+#include "gpolylst.h"
+
+// ****************************
+//      class TreeOfPartials
+// ****************************
+
+template <class T> class TreeOfPartials {
+private:
+  gTree<gPoly<T> > PartialTree;
+
+// Recursive Construction
+   void TreeOfPartialsRECURSIVE(gTree<gPoly<T> >&,
+				gTreeNode<gPoly<T> >*)         const;
+
+   T MaximalNonconstantContributionRECURSIVE(
+				const gTreeNode<gPoly<T> >*,
+		      	        const gVector<T>&,
+			        const gVector<T>&,
+		       	              gVector<int>&)           const;
+
+public:
+   TreeOfPartials(const gPoly<T> &);  
+   TreeOfPartials(const TreeOfPartials<T> &);
+   ~TreeOfPartials();
+
+   inline const bool operator ==(const TreeOfPartials<T>& rhs) const 
+     { return (PartialTree == rhs.PartialTree); }
+   inline const bool operator !=(const TreeOfPartials<T>& rhs) const 
+     { return !(*this == rhs); }
+   inline const int Dmnsn()                                    const 
+     { return RootNode()->GetData().Dmnsn(); }
+   inline const T EvaluateRootPoly(const gVector<T>& point)    const 
+     { return RootNode()->GetData().Evaluate(point); }
+
+
+   T MaximalNonconstantContribution(const gVector<T>&, 
+				    const gVector<T>&)         const;
+
+   inline gTreeNode<gPoly<T> >* RootNode()                     const 
+     { return PartialTree.RootNode(); }
+   inline gPoly<T> RootPoly()                                  const 
+     { return RootNode()->GetData(); }
+   inline T ValueOfRootPoly(const gVector<T>& point)           const 
+     { return RootPoly().Evaluate(point); }
+   T ValueOfPartialOfRootPoly(const int&, const gVector<T>&)   const;
+
+friend gOutput& operator << (gOutput& output, const TreeOfPartials<T>& x);
+};
+
+
+// *********************************
+//      class ListOfPartialTrees
+// *********************************
+
+template <class T> class ListOfPartialTrees {
+private:
+  gList<TreeOfPartials<T> >     PartialTreeList;
+
+public:
+   ListOfPartialTrees(const gList<gPoly<T> >&);  
+   ListOfPartialTrees(const gPolyList<T>&);  
+   ListOfPartialTrees(const ListOfPartialTrees<T> &);
+   ~ListOfPartialTrees();
+
+  inline operator =  (const ListOfPartialTrees&) const { 
+    gout << "We have not yet defined operator = in ListOfPartialTrees\n";
+    exit(0);
+  }
+
+  // operators
+  bool operator == (const ListOfPartialTrees&) const;
+  bool operator != (const ListOfPartialTrees&) const;
+
+  inline const TreeOfPartials<T>& operator[](const int& i)        const 
+    { return PartialTreeList[i]; }
+
+  // Information
+  inline int Length()                                             const
+    { return PartialTreeList.Length(); }
+  inline int Dmnsn()                                              const
+    { assert (Length() > 0); return PartialTreeList[1].Dmnsn(); }
+  gMatrix<T> DerivativeMatrix(const gVector<T>&)                  const; 
+  gSquareMatrix<T> SquareDerivativeMatrix(const gVector<T>&)      const; 
+  gVector<T> ValuesOfRootPolys(const gVector<T>&)                 const;
+
+friend gOutput& operator << (gOutput& output, const ListOfPartialTrees<T>& x);
+};
+
+
+#endif // GPARTLTR_H
