7164433d9414cca9548eedabbd5bb494f34f2979
mclennan <mclennan@3fbfd45b-e82e-0410-bb8e-8470640bbd7f>
1998-02-18 13:02:57 +0000

    Initial revision

---
+++ sources/gsmpltpe.cc
@@ -0,0 +1,30 @@
+//
+// FILE: gsmpltpe.cc -- Instantiation of gSimpletope class
+// 
+// @(#)gsmpltpe.cc	1.1 01/07/98 
+//
+
+#include <assert.h>
+#include "glist.imp"
+#include "double.h"
+#include "gsmpltpe.imp"
+#include "glpsolv3.imp"
+
+
+template class gSimpletope<gRational>;
+template class gList< gSimpletope<gRational> >;
+template gOutput& operator << (gOutput& output, 
+			       const gSimpletope<gRational>& x);
+template gSimpletope<gDouble> TogDouble(const gSimpletope<gRational>&);
+
+template class gSimpletope<double>;
+template class gList< gSimpletope<double> >;
+template gOutput& operator << (gOutput& output, 
+			       const gSimpletope<double>& x);
+template gSimpletope<gDouble> TogDouble(const gSimpletope<double>&);
+
+template class gSimpletope<gDouble>;
+template class gList< gSimpletope<gDouble> >;
+template gOutput& operator << (gOutput& output, 
+			       const gSimpletope<gDouble>& x);
+template gSimpletope<gDouble> TogDouble(const gSimpletope<gDouble>&);
