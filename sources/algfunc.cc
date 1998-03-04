e98a366cd29e3b0c784750a12ea48c070e070f10
rdm <rdm@3fbfd45b-e82e-0410-bb8e-8470640bbd7f>
1998-03-04 00:12:20 +0000

    changed VertEnum to get rid of start

--- sources/algfunc.cc
+++ sources/algfunc.cc
@@ -913,31 +913,21 @@ static Portion *GSM_Simpdiv_Efg(Portion **param)
 
 Portion* GSM_VertEnum( Portion** param )
 {
-  if (((PrecisionPortion *) param[3])->Value() == precDOUBLE)  {
+  if (((PrecisionPortion *) param[2])->Value() == precDOUBLE)  {
     gMatrix<double>* A = ListToMatrix_Float((ListPortion*) param[0]);
     gVector<double>* b = ListToVector_Float((ListPortion*) param[1]);
-    gVector<double>* start = ListToVector_Float((ListPortion*) param[2]);
     
     gList< gVector< double > > verts;
     
-    if( start->Length() == 0 ) {
-      VertEnum< double >* vertenum = NULL;
-      vertenum = new VertEnum< double >( *A, *b );
-      vertenum->Vertices( verts );
-      delete vertenum;
-    }
-    else {
-      NewVertEnum< double >* vertenum = NULL;
-      vertenum = new NewVertEnum< double >( *A, *b, *start );
-      vertenum->Vertices( verts );
-      delete vertenum;
-    }
-    
+    VertEnum< double >* vertenum = NULL;
+    vertenum = new VertEnum< double >( *A, *b );
+    vertenum->Vertices( verts );
+    delete vertenum;
+
     delete A;
     delete b;
-    delete start;
     
-  ListPortion* list = new ListPortion();
+    ListPortion* list = new ListPortion();
     for (int i = 1; i <= verts.Length(); i++)  
       list->Append( ArrayToList( verts[i] ) );
     
@@ -946,26 +936,16 @@ Portion* GSM_VertEnum( Portion** param )
   else {
     gMatrix<gRational>* A = ListToMatrix_Rational((ListPortion*) param[0]);
     gVector<gRational>* b = ListToVector_Rational((ListPortion*) param[1]);
-    gVector<gRational>* start = ListToVector_Rational((ListPortion*) param[2]);
     
     gList< gVector< gRational > > verts;
     
-    if( start->Length() == 0 ) {
-      VertEnum< gRational >* vertenum = NULL;
-      vertenum = new VertEnum< gRational >( *A, *b );
-      vertenum->Vertices( verts );
-      delete vertenum;
-    }
-    else {
-      NewVertEnum< gRational >* vertenum = NULL;
-      vertenum = new NewVertEnum< gRational >( *A, *b, *start );
-      vertenum->Vertices( verts );
-      delete vertenum;
-    }
-    
+    VertEnum< gRational >* vertenum = NULL;
+    vertenum = new VertEnum< gRational >( *A, *b );
+    vertenum->Vertices( verts );
+    delete vertenum;
+
     delete A;
     delete b;
-    delete start;
     
     ListPortion* list = new ListPortion();
     for (int i = 1; i <= verts.Length(); i++)  
@@ -1479,14 +1459,12 @@ void Init_algfunc(GSM *gsm)
 
   FuncObj = new FuncDescObj("VertEnum", 1);
   FuncObj->SetFuncInfo(0, FuncInfoType(GSM_VertEnum,
-				       PortionSpec(porNUMBER, 2), 4));
+				       PortionSpec(porNUMBER, 2), 3));
   FuncObj->SetParamInfo(0, 0, ParamInfoType("A", PortionSpec(porNUMBER,2),
 					    REQUIRED, BYVAL));
   FuncObj->SetParamInfo(0, 1, ParamInfoType("b", PortionSpec(porNUMBER,1),
 					    REQUIRED, BYVAL));
-  FuncObj->SetParamInfo(0, 2, ParamInfoType("start", PortionSpec(porNUMBER,1),
-					    new ListPortion(), BYVAL));
-  FuncObj->SetParamInfo(0, 3, ParamInfoType("precision", porPRECISION,
+  FuncObj->SetParamInfo(0, 2, ParamInfoType("precision", porPRECISION,
 					    new PrecisionPortion(precDOUBLE)));
   gsm->AddFunction(FuncObj);
 
