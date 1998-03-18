7e891fc11f1a42a1017ca15c2f59e3d63fb9f007
mclennan <mclennan@3fbfd45b-e82e-0410-bb8e-8470640bbd7f>
1998-03-18 13:57:27 +0000

    removed commented out stuff

--- sources/quiksolv.cc
+++ sources/quiksolv.cc
@@ -17,30 +17,3 @@ template gOutput &operator<<(gOutput &f, const QuikSolv<double> &y);
 template class QuikSolv<gDouble>;
 template gOutput &operator<<(gOutput &f, const QuikSolv<gDouble> &y);
 
-#include "glist.imp"
-
-/*
-// Eventually these classes should be in separate files
-template class TreeOfPartials<gDouble>;
-template gOutput &operator<<(gOutput &f, const TreeOfPartials<gDouble> &y);
-template class gList<TreeOfPartials<gDouble> >;
-template class ListOfPartialTrees<gDouble>;
-template gOutput &operator<<(gOutput &f, 
-			     const ListOfPartialTrees<gDouble> &y);
-
-template class TreeOfPartials<gRational>;
-template gOutput &operator<<(gOutput &f, const TreeOfPartials<gRational> &y);
-template class gList<TreeOfPartials<gRational> >;
-template class ListOfPartialTrees<gRational>;
-template gOutput &operator<<(gOutput &f, 
-			     const ListOfPartialTrees<gRational> &y);
-
-
-template class TreeOfPartials<double>;
-template gOutput &operator<<(gOutput &f, const TreeOfPartials<double> &y);
-template class gList<TreeOfPartials<double> >;
-template class ListOfPartialTrees<double>;
-template gOutput &operator<<(gOutput &f, 
-			     const ListOfPartialTrees<double> &y);
-
-*/
--- sources/quiksolv.h
+++ sources/quiksolv.h
@@ -1,7 +1,7 @@
 //
 // File: quiksolv.h  --  Declaration of gpolylst data type
 // 
-// $Id$
+// @(#)quiksolv.h	1.6 02/17/98
 //
 
 #ifndef QUIKSOLV_H
@@ -30,94 +30,6 @@ each.  This continues until it has been shown that all roots have been
 found, or a predetermined search depth is reached.  The bound on depth
 is necessary because the procedure will not terminate if there are
 singular roots.
-*/
-
-/*
-// ****************************
-//      class TreeOfPartials
-// ****************************
-
-template <class T> class TreeOfPartials {
-private:
-  gTree<gPoly<T> > PartialTree;
-
-// Recursive Construction
-   void TreeOfPartialsRECURSIVE(gTree<gPoly<T> >&,
-				gTreeNode<gPoly<T> >*)         const;
-
-   T MaximalNonconstantContributionRECURSIVE(
-				const gTreeNode<gPoly<T> >*,
-		      	        const gVector<T>&,
-			        const gVector<T>&,
-		       	              gVector<int>&)           const;
-
-public:
-   TreeOfPartials(const gPoly<T> &);  
-   TreeOfPartials(const TreeOfPartials<T> &);
-   ~TreeOfPartials();
-
-   inline const bool operator ==(const TreeOfPartials<T>& rhs) const 
-     { return (PartialTree == rhs.PartialTree); }
-   inline const bool operator !=(const TreeOfPartials<T>& rhs) const 
-     { return !(*this == rhs); }
-   inline const int Dmnsn()                                    const 
-     { return RootNode()->GetData().Dmnsn(); }
-   inline const T EvaluateRootPoly(const gVector<T>& point)    const 
-     { return RootNode()->GetData().Evaluate(point); }
-
-
-   T MaximalNonconstantContribution(const gVector<T>&, 
-				    const gVector<T>&)         const;
-
-   inline gTreeNode<gPoly<T> >* RootNode()                     const 
-     { return PartialTree.RootNode(); }
-   inline gPoly<T> RootPoly()                                  const 
-     { return RootNode()->GetData(); }
-   inline T ValueOfRootPoly(const gVector<T>& point)           const 
-     { return RootPoly().Evaluate(point); }
-   T ValueOfPartialOfRootPoly(const int&, const gVector<T>&)   const;
-
-friend gOutput& operator << (gOutput& output, const TreeOfPartials<T>& x);
-};
-
-
-// *********************************
-//      class ListOfPartialTrees
-// *********************************
-
-template <class T> class ListOfPartialTrees {
-private:
-  gList<TreeOfPartials<T> >     PartialTreeList;
-
-public:
-   ListOfPartialTrees(const gList<gPoly<T> >&);  
-   ListOfPartialTrees(const gPolyList<T>&);  
-   ListOfPartialTrees(const ListOfPartialTrees<T> &);
-   ~ListOfPartialTrees();
-
-  inline operator =  (const ListOfPartialTrees&) const { 
-    gout << "We have not yet defined operator = in ListOfPartialTrees\n";
-    exit(0);
-  }
-
-  // operators
-  bool operator == (const ListOfPartialTrees&) const;
-  bool operator != (const ListOfPartialTrees&) const;
-
-  inline const TreeOfPartials<T>& operator[](const int& i)        const 
-    { return PartialTreeList[i]; }
-
-  // Information
-  inline int Length()                                             const
-    { return PartialTreeList.Length(); }
-  inline int Dmnsn()                                              const
-    { assert (Length() > 0); return PartialTreeList[1].Dmnsn(); }
-  gMatrix<T> DerivativeMatrix(const gVector<T>&)                  const; 
-  gSquareMatrix<T> SquareDerivativeMatrix(const gVector<T>&)      const; 
-  gVector<T> ValuesOfRootPolys(const gVector<T>&)                 const;
-
-friend gOutput& operator << (gOutput& output, const ListOfPartialTrees<T>& x);
-};
 */
 
 /*
