10a2b261b5c97cbfdab1d721e6e253160a7d2c7a
mclennan <mclennan@3fbfd45b-e82e-0410-bb8e-8470640bbd7f>
1998-03-19 14:59:42 +0000

    added CubeContainingCrcmscrbngSphere()

--- sources/rectangl.h
+++ sources/rectangl.h
@@ -37,24 +37,25 @@ public:
     bool          operator != (const gRectangle<T>& y) const;
 
     // information
-    const int                Dmnsn()                          const;
-    const gVector<T>         LowerBound()                     const;
-    const gVector<T>         UpperBound()                     const;
-    const T                  LowerBoundOfCoord(const int&)    const;
-    const T                  UpperBoundOfCoord(const int&)    const;
-    const T                  HeightInCoord(const int&)        const;
-    const gInterval<T>       CartesianFactor(const int&)      const;
-    const gRectangle<T>      SameCenterDoubleSideLengths()    const;
-    const gRectangle<T>      Orthant(const gArray<int>&)      const;
-    const gVector<T>         SideLengths()                    const;
-    bool                     Contains(const gVector<T> &)     const;
-    bool                     Contains(const gRectangle<T> &)  const;
-    const T                  Volume()                         const;
-    const gVector<T>         Center()                         const;
-    const gRectangle<T>      BoundingRectangle()              const;
-    const gList<gVector<T> > VertexList()                     const;
-    const int                NumberOfCellsInSubdivision()     const;
-    const gRectangle<T>      SubdivisionCell(const int&)      const;
+    const int                Dmnsn()                            const;
+    const gVector<T>         LowerBound()                       const;
+    const gVector<T>         UpperBound()                       const;
+    const T                  LowerBoundOfCoord(const int&)      const;
+    const T                  UpperBoundOfCoord(const int&)      const;
+    const T                  HeightInCoord(const int&)          const;
+    const gInterval<T>       CartesianFactor(const int&)        const;
+    const gRectangle<T>      SameCenterDoubleSideLengths()      const;
+    const gRectangle<T>      CubeContainingCrcmscrbngSphere()   const;
+    const gRectangle<T>      Orthant(const gArray<int>&)        const;
+    const gVector<T>         SideLengths()                      const;
+    bool                     Contains(const gVector<T> &)       const;
+    bool                     Contains(const gRectangle<T> &)    const;
+    const T                  Volume()                           const;
+    const gVector<T>         Center()                           const;
+    const gRectangle<T>      BoundingRectangle()                const;
+    const gList<gVector<T> > VertexList()                       const;
+    const int                NumberOfCellsInSubdivision()       const;
+    const gRectangle<T>      SubdivisionCell(const int&)        const;
 
     // output
 friend gOutput& operator << (gOutput& output, const gRectangle<T>& x);
