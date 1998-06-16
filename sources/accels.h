df601b3c5ff2935deefead899e3547626dd66c16
mvanier <mvanier@3fbfd45b-e82e-0410-bb8e-8470640bbd7f>
1998-06-16 01:23:05 +0000

    Reformatted for readability.

--- sources/accels.h
+++ sources/accels.h
@@ -5,51 +5,95 @@
 #include "glist.h"
 #include "gtext.h"
 
-// This structure describes an accelerator--key combination w/ event id to trigger
-typedef enum {aOff=0,aOn,aEither} AccelState;
+// This structure describes an accelerator--key combination 
+// with event id to trigger.
+
+typedef enum { aOff = 0, aOn, aEither } AccelState;
+
 typedef struct accel_struct
 {
-// Data
-	AccelState ctrl;
-	AccelState shift;
-	long key;
-	long id;
-// Constructor
-	accel_struct(void):ctrl(aOff),shift(aOff),key(0),id(0) { }
-	accel_struct(AccelState c,AccelState s,long k,long i):ctrl(c),shift(s),key(k),id(i) { }
-// Operators
-	accel_struct &operator=(const accel_struct &a) {ctrl=a.ctrl;shift=a.shift;key=a.key;id=a.id;return *this;}
-	int operator==(const accel_struct &a) const {return (ctrl==a.ctrl && shift==a.shift && key==a.key);}
-	int operator!=(const accel_struct &a) const {return !(ctrl==a.ctrl && shift==a.shift && key==a.key);}
-	int operator==(wxKeyEvent &a) const 
-	{
-	return ((ctrl==a.ControlDown() || ctrl==aEither) && (shift==a.ShiftDown() || shift==aEither)
-				 && key==a.KeyCode());
-	}
+    // Data
+    AccelState ctrl;
+    AccelState shift;
+    long key;
+    long id;
+
+    // Constructors
+    accel_struct(void):ctrl(aOff), shift(aOff), key(0), id(0) { }
+    accel_struct(AccelState c, AccelState s, long k, long i)
+        : ctrl(c), shift(s), key(k), id(i) { }
+
+    // Operators
+    accel_struct &operator=(const accel_struct &a)
+    {
+        ctrl  = a.ctrl;
+        shift = a.shift;
+        key   = a.key;
+        id    = a.id;
+        return *this;
+    }
+
+    int operator==(const accel_struct &a) const
+    { return (ctrl == a.ctrl && shift == a.shift && key == a.key); }
+
+    int operator!=(const accel_struct &a) const
+    { return !(ctrl == a.ctrl && shift == a.shift && key == a.key); }
+
+    int operator==(wxKeyEvent &a) const 
+    {
+        return ((ctrl == a.ControlDown() || ctrl == aEither) && 
+                (shift == a.ShiftDown() || shift == aEither) && 
+                key == a.KeyCode());
+    }
 } Accel;
-gOutput &operator<<(gOutput &o,const Accel &p);
+
+gOutput &operator<<(gOutput &o, const Accel &p);
+
 // This structure defines a possible accelerator event.
 typedef struct accelevent_struct
 {
-	gText name;
-	long id;
-	// Constructor
-	accelevent_struct(void) : name(""),id(0) { }
-	accelevent_struct(const gText &n,long i) :name(n),id(i) { }
-	// operators
-	accelevent_struct &operator=(const accelevent_struct &a) {name=a.name;id=a.id;return *this;}
-	int operator==(const accelevent_struct &a) {return (name==a.name && id==a.id);}
-	int operator!=(const accelevent_struct &a) {return !(name==a.name && id==a.id);}
-	int operator==(long new_id) {return (id==new_id);}
-	int operator==(const gText new_name) {return (name==new_name);}
+    gText name;
+    long id;
+
+    // Constructor
+    accelevent_struct(void) : name(""), id(0) { }
+    accelevent_struct(const gText &n, long i) : name(n), id(i) { }
+
+    // Operators
+    accelevent_struct &operator=(const accelevent_struct &a)
+    {
+        name = a.name;
+        id   = a.id;
+        return *this;
+    }
+
+    int operator==(const accelevent_struct &a)
+    { return (name == a.name && id == a.id); }
+
+    int operator!=(const accelevent_struct &a)
+    { return !(name == a.name && id == a.id); }
+
+    int operator==(long new_id)          { return (id == new_id);     }
+    int operator==(const gText new_name) { return (name == new_name); }
 } AccelEvent;
-gOutput &operator<<(gOutput &o,const AccelEvent &p);
+
+gOutput &operator<<(gOutput &o, const AccelEvent &p);
+
 // Functions for working with accelerators
-long 	CheckAccelerators(gList<Accel> &list,wxKeyEvent &ev);
-int 	ReadAccelerators(gList<Accel> &list,const char *section="Accelerators",const char *file="gambit.ini");
-int 	WriteAccelerators(const gList<Accel> &list,const char *section="Accelerators",const char *file="gambit.ini");
-void	EditAccelerators(gList<Accel> &list,const gArray<AccelEvent> &events);
+long    CheckAccelerators(gList<Accel> &list, wxKeyEvent &ev);
 
-#define ACCELERATORS_HELP	"Accelerator Keys"
+int     ReadAccelerators(gList<Accel> &list, 
+                         const char *section = "Accelerators", 
+                         const char *file = "gambit.ini");
 
-#endif /* mutual exclusion */
+int     WriteAccelerators(const gList<Accel> &list, 
+                          const char *section = "Accelerators", 
+                          const char *file = "gambit.ini");
+
+void    EditAccelerators(gList<Accel> &list, const gArray<AccelEvent> &events);
+
+#define ACCELERATORS_HELP   "Accelerator Keys"
+
+#endif // ACCELS_H 
+
+
