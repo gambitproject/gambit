21d40b6fd407c6a88ca531709c1649d2e8bfd33a
mvanier <mvanier@3fbfd45b-e82e-0410-bb8e-8470640bbd7f>
1998-06-22 00:39:58 +0000

    Reformatted for readability

--- sources/efgsoln.h
+++ sources/efgsoln.h
@@ -2,8 +2,10 @@
 // form solutions.
 //
 // $Id$
-#ifndef EXTSOLN_H
-#define EXTSOLN_H
+
+#ifndef EFGSOLN_H
+#define EFGSOLN_H
+
 #include "efg.h"
 #include "efgconst.h"
 #include "efgshow.h"
@@ -13,95 +15,100 @@
 //****************************************************************************
 //                       NODE INSPECT WINDOW
 //****************************************************************************
-class NodeSolnShow:public SpreadSheet3D
+
+class NodeSolnShow: public SpreadSheet3D
 {
 private:
-	const EfgShow *parent;
-	const Node *cur_n; // use to refresh data
-	gArray<Bool> features;
-	static char *feature_names[];
-	int num_players;
-	int	Pos(int feature);
-	void SetOptions(void);
-	wxMenuBar *MakeMenuBar(long menus);
+    const EfgShow *parent;
+    const Node *cur_n; // use to refresh data
+    gArray<Bool> features;
+    static char *feature_names[];
+    int num_players;
+    int Pos(int feature);
+    void SetOptions(void);
+    wxMenuBar *MakeMenuBar(long menus);
 public:
-	NodeSolnShow(int num_players,const EfgShow *parent);
-	// Data entry functions
-	void Set(const Node *n);
-	// Take care of some options changes
-	void OnOptionsChanged(unsigned int options=0);
-	// Override menu handling
-	void OnMenuCommand(int id);
-	// Override the help system
-	void OnHelp(int =0);
+    NodeSolnShow(int num_players, const EfgShow *parent);
+    // Data entry functions
+    void Set(const Node *n);
+    // Take care of some options changes
+    void OnOptionsChanged(unsigned int options = 0);
+    // Override menu handling
+    void OnMenuCommand(int id);
+    // Override the help system
+    void OnHelp(int =0);
 };
 
 
 //****************************************************************************
 //                       BEHAV SOLUTION SHOW
 //****************************************************************************
-#define		BSOLN_O_OPTIONS		1
-#define		BSOLN_O_EDIT			2
-#define		BSOLN_O_SORTFILT	4
-#define		BSOLN_O_PICKER		(8|BSOLN_O_OPTIONS|BSOLN_O_SORTFILT)
-#define		BSOLN_O_EFGNFG		16
+#define     BSOLN_O_OPTIONS     1
+#define     BSOLN_O_EDIT        2
+#define     BSOLN_O_SORTFILT    4
+#define     BSOLN_O_PICKER      (8|BSOLN_O_OPTIONS|BSOLN_O_SORTFILT)
+#define     BSOLN_O_EFGNFG      16
 
 class GambitDrawSettings;
+
 class EfgSolnShow: public SpreadSheet3D
 {
 private:
-	const Efg &ef;
-	EfgShow *parent;
-	void	UpdateValues(void);
-	const GambitDrawSettings &gamb_draw_settings;
-	static void normal_button(wxButton &ob,wxEvent &ev);
-	static void add_button(wxButton &ob,wxEvent &ev);
-	static void edit_button(wxButton &ob,wxEvent &ev);
-	static void delete_button(wxButton &ob,wxEvent &ev);
-	static void delete_all_button(wxButton &ob,wxEvent &ev);
-	static void sortfilt_button(wxButton &ob,wxEvent &ev);
-	// Options button
-	static void settings_button(wxButton &ob,wxEvent &ev);
-	void	UpdateSoln(int row,int col);
+    const Efg &ef;
+    EfgShow *parent;
+    void    UpdateValues(void);
+    const GambitDrawSettings &gamb_draw_settings;
+    static void normal_button(wxButton &ob, wxEvent &ev);
+    static void add_button(wxButton &ob, wxEvent &ev);
+    static void edit_button(wxButton &ob, wxEvent &ev);
+    static void delete_button(wxButton &ob, wxEvent &ev);
+    static void delete_all_button(wxButton &ob, wxEvent &ev);
+    static void sortfilt_button(wxButton &ob, wxEvent &ev);
+    // Options button
+    static void settings_button(wxButton &ob, wxEvent &ev);
+    void    UpdateSoln(int row, int col);
+
 protected:
-	BehavSolutionList &solns;
-	const gPVector<int> dim;		// dimensionality dim[player][iset]
-	int 	num_players,num_isets,num_solutions,cur_soln;
-	gBlock<Bool> features;
-	unsigned int opts;
-	static char *feature_names[];
-	static int   feature_width[];
-	BSolnSortFilterOptions	&sf_options;
-	virtual void SetOptions(void);
-	int	 	FeaturePos(int feature);
-	int		SolnNum(int row);
-	int		SolnPos(int soln);
-	virtual void	OnRemove(bool all);
-	virtual void	OnAdd(void);
-	virtual void	OnEdit(void);
-	virtual void  SortFilter(bool inter=true);
+    BehavSolutionList &solns;
+    const gPVector<int> dim;        // dimensionality dim[player][iset]
+    int     num_players, num_isets, num_solutions, cur_soln;
+    gBlock<Bool> features;
+    unsigned int opts;
+    static char *feature_names[];
+    static int   feature_width[];
+    BSolnSortFilterOptions  &sf_options;
+    virtual void SetOptions(void);
+    int     FeaturePos(int feature);
+    int     SolnNum(int row);
+    int     SolnPos(int soln);
+    virtual void    OnRemove(bool all);
+    virtual void    OnAdd(void);
+    virtual void    OnEdit(void);
+    virtual void  SortFilter(bool inter = true);
 
 public:
-	EfgSolnShow(const Efg &ef_,BehavSolutionList &soln,
-						int cur_soln_,const GambitDrawSettings &draw_settings,
-						BSolnSortFilterOptions	&sf_options,
-						EfgShow *parent_=0,
-						unsigned int opts=BSOLN_O_OPTIONS|BSOLN_O_EDIT|BSOLN_O_SORTFILT|BSOLN_O_EFGNFG);
-	// Implement the infoset hilighting behavior.  If iset=0, just return
-	// the option state.
-	Bool HilightInfoset(int pl=0,int iset=0);
-	// Overide help system
-	void OnHelp(int help_type=0);
-	// Take care of some options changes
-	void OnOptionsChanged(unsigned int options=0);
-	// Double clicking on a solution will update the parent
-	void OnDoubleClick(int row,int col,int level,const gText &value);
-	// Moving a cell updates the parent, if the dynamic option is set
-	void OnSelectedMoved(int row,int col,SpreadMoveDir how);
-	// OnOk must be defined to inform parent that I am killed
-	void OnOk(void);
-	// Send one BehavProfile to a MixedProfile NF (Not implemented)
-	void SolutionToNormal(void);
+    EfgSolnShow(const Efg &ef_, BehavSolutionList &soln,
+                int cur_soln_, const GambitDrawSettings &draw_settings,
+                BSolnSortFilterOptions  &sf_options,
+                EfgShow *parent_ = 0,
+                unsigned int opts = 
+				BSOLN_O_OPTIONS|BSOLN_O_EDIT|BSOLN_O_SORTFILT|BSOLN_O_EFGNFG);
+
+    // Implement the infoset hilighting behavior.  If iset = 0, just return
+    // the option state.
+    Bool HilightInfoset(int pl = 0, int iset = 0);
+    // Overide help system
+    void OnHelp(int help_type = 0);
+    // Take care of some options changes
+    void OnOptionsChanged(unsigned int options = 0);
+    // Double clicking on a solution will update the parent
+    void OnDoubleClick(int row, int col, int level, const gText &value);
+    // Moving a cell updates the parent, if the dynamic option is set
+    void OnSelectedMoved(int row, int col, SpreadMoveDir how);
+    // OnOk must be defined to inform parent that I am killed
+    void OnOk(void);
+    // Send one BehavProfile to a MixedProfile NF (Not implemented)
+    void SolutionToNormal(void);
 };
 
 //****************************************************************************
@@ -111,25 +118,26 @@ public:
 class EfgSolnPicker: public EfgSolnShow
 {
 private:
-	gArray<bool> picked;
-	void PickSoln(int row);
-	wxButton *pick_all_button;
-	Bool pick_all;
-	static void pick_all_func(wxButton &ob,wxEvent &);
-	void OnPickAll(void);
+    gArray<bool> picked;
+    void PickSoln(int row);
+    wxButton *pick_all_button;
+    Bool pick_all;
+    static void pick_all_func(wxButton &ob, wxEvent &);
+    void OnPickAll(void);
+
 public:
-	EfgSolnPicker(const Efg &ef_,BehavSolutionList &soln,
-						const GambitDrawSettings &draw_settings,
-						BSolnSortFilterOptions	&sf_options,
-						EfgShow *parent_=0);
-	// Double clicking on a solution will toggle it.
-	void OnDoubleClick(int row,int col,int level,const gText &value);
-	// Override parent class feature
-	void OnSelectedMoved(int row,int col,SpreadMoveDir how);
-	// OnOk must be defined to inform parent that I am killed
-	void OnOk(void);
-	// Override the help system
-	virtual void OnHelp(int help_type=0);
+    EfgSolnPicker(const Efg &ef_, BehavSolutionList &soln,
+                  const GambitDrawSettings &draw_settings,
+                  BSolnSortFilterOptions    &sf_options,
+                  EfgShow *parent_ = 0);
+    // Double clicking on a solution will toggle it.
+    void OnDoubleClick(int row, int col, int level, const gText &value);
+    // Override parent class feature
+    void OnSelectedMoved(int row, int col, SpreadMoveDir how);
+    // OnOk must be defined to inform parent that I am killed
+    void OnOk(void);
+    // Override the help system
+    virtual void OnHelp(int help_type = 0);
 };
 
 //****************************************************************************
@@ -139,39 +147,43 @@ public:
 class Ext1SolnPicker: public EfgSolnShow
 {
 private:
-	int picked;
-	void PickSoln(int row);
+    int picked;
+    void PickSoln(int row);
+
 protected:
-	virtual void	OnRemove(bool all);
+    virtual void OnRemove(bool all);
+
 public:
-	Ext1SolnPicker(const Efg &ef_,BehavSolutionList &soln,
-						const GambitDrawSettings &draw_settings,
-						BSolnSortFilterOptions	&sf_options,
-						EfgShow *parent_=0);
-	// Double clicking on a solution will select it
-	void OnDoubleClick(int row,int col,int level,const gText &value);
-	// Override parent class feature
-	void OnSelectedMoved(int row,int col,SpreadMoveDir how);
-	// OnOk must be defined to inform parent that I am killed
-	void OnOk(void);
-	// Override the help system
-	virtual void OnHelp(int help_type=0);
-	// Return the pick
-	int Picked(void) const;
+    Ext1SolnPicker(const Efg &ef_, BehavSolutionList &soln,
+                   const GambitDrawSettings &draw_settings,
+                   BSolnSortFilterOptions   &sf_options,
+                   EfgShow *parent_ = 0);
+
+    // Double clicking on a solution will select it
+    void OnDoubleClick(int row, int col, int level, const gText &value);
+    // Override parent class feature
+    void OnSelectedMoved(int row, int col, SpreadMoveDir how);
+    // OnOk must be defined to inform parent that I am killed
+    void OnOk(void);
+    // Override the help system
+    virtual void OnHelp(int help_type = 0);
+    // Return the pick
+    int Picked(void) const;
 };
 
 
 class BehavSolnEdit : public SpreadSheet3D
 {
 private:
-	BehavSolution &soln;
-	gPVector<int> dim;
-	int	num_isets;
+    BehavSolution &soln;
+    gPVector<int> dim;
+    int num_isets;
+
 public:
-	// Constructor
-	BehavSolnEdit(BehavSolution &soln,int iset_disp,wxFrame *parent);
-	void OnSelectedMoved(int row,int col,SpreadMoveDir how);
-	void OnOk(void);
+    // Constructor
+    BehavSolnEdit(BehavSolution &soln, int iset_disp, wxFrame *parent);
+    void OnSelectedMoved(int row, int col, SpreadMoveDir how);
+    void OnOk(void);
 };
 
-#endif
+#endif // EFGSOLN_H
