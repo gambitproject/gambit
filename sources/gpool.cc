9903f1f1141d656d3a7ee857322b3db10704ce85
magyar <magyar@3fbfd45b-e82e-0410-bb8e-8470640bbd7f>
1998-08-06 21:23:28 +0000

    Initial revision

---
+++ sources/gpool.cc
@@ -0,0 +1,30 @@
+//
+// FILE: gpool.cc -- Implementation of memory pool
+//
+
+#include "gpool.h"
+
+gPool::gPool(unsigned sz)
+  : esize((sz < sizeof(gLink *)) ? sizeof(gLink *) : sz), head(0)
+{ }
+
+gPool::~gPool()
+{
+// need to go through and delete things really
+}
+
+void gPool::Grow(void)
+{
+  const int overhead = 12;
+  const int chunk_size = 8 * 1024 - overhead;
+  const int nelem = (chunk_size - esize) / esize;
+
+  char *start = new char[chunk_size];
+  char *last = &start[(nelem - 1) * esize];
+  
+  for (char *p = start; p < last; p += esize)
+    ((gLink *) p)->next = (gLink *) (p + esize);
+  ((gLink *) last)->next = 0;
+  head = (gLink *) start;
+}
+
---
+++ sources/gpool.h
@@ -0,0 +1,41 @@
+//
+// FILE: gpool.h -- Memory pool
+//
+
+class gPool   {
+private:
+  struct gLink  { gLink *next; };
+
+  const unsigned esize;
+  gLink *head;
+
+  // these are here to protect against copying -- shouldn't be done with this
+  // class!
+  gPool(const gPool &);
+  void operator=(const gPool &);
+  
+  void Grow(void);
+
+public:
+  gPool(unsigned n);
+  ~gPool();
+
+  void *Alloc(void);
+  void Free(void *);
+};
+
+inline void *gPool::Alloc(void)
+{
+  if (head == 0)  Grow();
+  gLink *p = head;
+  head = p->next;
+  return p;
+}
+
+inline void gPool::Free(void *b)
+{
+  gLink *p = (gLink *) b;
+  p->next = head;
+  head = p;
+}
+
