882fb3af35910d20dc4f6fd0cf2ea92052d28d58
magyar <magyar@3fbfd45b-e82e-0410-bb8e-8470640bbd7f>
1998-08-19 23:30:12 +0000

    Cleanup of code; now prints warning if Include file not found

--- sources/gpreproc.cc
+++ sources/gpreproc.cc
@@ -8,6 +8,155 @@
 #include "gpreproc.h"
 #include "system.h"
 
+
+//-------------------------------------------------------------------------
+//            gPreprocessor: private auxiliary member functions
+//-------------------------------------------------------------------------
+
+bool gPreprocessor::ExpectText(const char *p_text)
+{
+  int length = strlen(p_text);
+  char c;
+
+  if (m_InputStack.Peek()->eof())
+    return false;
+
+  for (int i = 0; i < length; i++) {
+    GetChar(c);
+    if (m_InputStack.Peek()->eof() || c != p_text[i])
+      return false;
+  }
+  return true;
+}
+
+//
+// Note: filename might be changed after this call
+//
+gInput *gPreprocessor::LoadInput(gText &p_name)
+{
+  gInput *infile = NULL;
+
+  extern char *_SourceDir;
+  const char *SOURCE = _SourceDir; 
+  const char SLASH = System::Slash();
+  
+  bool search = (strchr((char *) p_name, SLASH) == NULL);
+  gText IniFileName = p_name;
+  
+  try   {
+    return new gFileInput(IniFileName);
+  }
+  catch (gFileInput::OpenFailed &) {
+    // I'm thinking there's a more elegant way than this.
+    // But I don't know what it is.
+    if (search)   {
+      if (System::GetEnv("HOME") != NULL)   {
+	IniFileName = ((gText) System::GetEnv("HOME") + 
+		       (gText) SLASH + (gText) p_name);
+	try   {
+	  infile = new gFileInput(IniFileName);
+	  p_name = IniFileName;
+	  return infile;
+	}
+	catch (gFileInput::OpenFailed &) {
+	  infile = 0;
+	  // didn't find in home directory... fall on through
+	}
+      }
+      if (System::GetEnv("GCLLIB") != NULL)  {
+	IniFileName = ((gText) System::GetEnv("GCLLIB") + 
+		       (gText) SLASH + (gText) p_name);
+	try  {
+	  infile = new gFileInput(IniFileName);
+	  p_name = IniFileName;
+	  return infile;
+	}
+	catch (gFileInput::OpenFailed &) {
+	  infile = 0;
+	  // didn't find it in GCLLIB directory either... keep going
+	}
+      }
+      if (SOURCE != NULL)  {
+	IniFileName = (gText) SOURCE + (gText) SLASH + (gText) p_name;
+	try  {
+	  infile = new gFileInput(IniFileName);
+	  p_name = IniFileName;
+	  return infile;
+	}
+	catch (gFileInput::OpenFailed &) {
+	  // give up!
+	  return 0;
+	}
+      }
+      return 0;
+    }
+    else {  // don't search, just give up
+      return 0;
+    }
+  }
+}
+
+void gPreprocessor::SetPrompt(bool p_prompt)
+{
+  if (m_InputStack.Peek() == m_CmdLine)
+    m_CmdLine->SetPrompt(p_prompt);	  
+}
+
+void gPreprocessor::GetChar(char &p_char)
+{
+  if (m_StartupString.Length() > 0)  {
+    p_char = m_StartupString[0];
+    m_StartupString = m_StartupString.Right(m_StartupString.Length() - 1);
+  }
+  else {
+    m_InputStack.Peek()->get(p_char);
+    if (EOL(p_char))
+      ++m_LineNumberStack.Peek();
+  }
+
+  m_RawLine += p_char;
+}
+
+bool gPreprocessor::IsQuoteEscapeSequence(const gText &p_line) const
+{
+  bool backslash = false;
+  char c = p_line[p_line.Length() - 1];
+  if (c == '\"') {
+    // if there's an odd number of consecutive backslashes,
+    //   then it's an escape sequence 
+    int i = 2;
+    while (p_line.Length() >= i && p_line[p_line.Length() - i] == '\\') {
+      backslash = !backslash;
+      i++;
+    }
+  }
+  return backslash;
+}
+
+
+
+gPreprocessor::gPreprocessor(gCmdLineInput *p_cmdline,
+			     const char *p_cmd /* = NULL */) 
+  : m_CmdLine(p_cmdline), m_PrevFileName("console"), m_PrevLineNumber(1),
+    m_StartupString(p_cmd)
+{
+  m_InputStack.Push(m_CmdLine);
+  m_FileNameStack.Push(m_PrevFileName);
+  m_LineNumberStack.Push(m_PrevLineNumber);
+
+  if (p_cmd && !EOL(m_StartupString[m_StartupString.Length() - 1]))
+    m_StartupString += '\n';
+}
+
+gPreprocessor::~gPreprocessor()
+{
+  while (m_InputStack.Depth() > 1)
+    delete m_InputStack.Pop();
+}
+
+
+
+
 //-------------------------------------------------------------------
 // GetLine
 //   Does the main pre-processing job.
@@ -30,7 +179,7 @@
 //    replaced with a space.
 //---------------------------------------------------------------------    
 
-gText gPreprocessor::GetLine( void )
+gText gPreprocessor::GetLine(void)
 {
   m_RawLine = "";
 
@@ -54,124 +203,101 @@ gText gPreprocessor::GetLine( void )
   int bracket = 0;
   bool continuation = false;
 
-  while( line.Right( 1 ) == '\\' && !error )
-  {
+  while (line.Right(1) == '\\' && !error) {
     // Strip the trailing backslash
-    line = line.Left( line.Length() - 1 );
+    line = line.Left(line.Length() - 1);
 
-    while( !m_InputStack.Peek()->eof() )
-    {
-      GetChar( c );
+    while (!m_InputStack.Peek()->eof()) {
+      GetChar(c);
       
-      if( !m_InputStack.Peek()->eof() )
-      {
+      if (!m_InputStack.Peek()->eof()) {
 	// Ignore CR or replace with space as appropriate.
-	if( !EOL( c ) )
+	if (!EOL(c))
 	  line += c;
-	else
-	{ 
-	  if( bracket > 0 )
-	  {
-	    if( line.Right( 1 ) == '\\' )
-	      line = line.Left( line.Length() - 1 );
+	else {
+	  if (bracket > 0) {
+	    if (line.Right(1) == '\\')
+	      line = line.Left(line.Length() - 1);
 	    line += '\n';
 	  }
-	  else
-	  {
+	  else {
 	    line += '\n';
 	    break;
 	  }
 	}
 
-
-	if( c == '[' )
-	{
+	if (c == '[') {
 	  // Turn off prompts until the bracket is matched.
-	  if( bracket == 0 )
-	    SetPrompt( false );	  
+	  if (bracket == 0)
+	    SetPrompt(false);	  
 	  ++bracket;
-
 	}
-	else if( c == ']' )
-	{
+	else if (c == ']') {
 	  // If brackets are matched, turn prompt back on.
 	  --bracket;
-	  if( bracket == 0 )
-	    SetPrompt( true );	  
+	  if (bracket == 0)
+	    SetPrompt(true);	  
 
-	  if( bracket < 0 )
+	  if (bracket < 0)
 	    bracket = 0;
-
 	}
-	else if( c == '\"' )
-	{
+	else if (c == '\"') {
 	  // In the middle of a string; accept everything
 	  //   until the closing quote mark is found.
-	  SetPrompt( false );	  
+	  SetPrompt(false);	  
 	  quote = !quote;
-	  while( !m_InputStack.Peek()->eof() && quote )
-	  {
-	    GetChar( c );
+	  while (!m_InputStack.Peek()->eof() && quote) {
+	    GetChar(c);
 	    line += c;
 
-	    if( c == '\"' && !IsQuoteEscapeSequence( line ) )
+	    if (c == '\"' && !IsQuoteEscapeSequence(line))
 	      quote = !quote;
 	  }
-	  SetPrompt( true );	  
-
+	  SetPrompt(true);	  
 	}
 
-
-	assert( !quote );
-
-	if( line.Right( 2 ) == "//" )
-	{
+	if (line.Right(2) == "//") {
 	  // In a line comment.  Ignore everything until
 	  //   end of line.
-	  line = line.Left( line.Length() - 2 );
+	  line = line.Left(line.Length() - 2);
 
-	  while( !m_InputStack.Peek()->eof() && !EOL( c ) )
-	    GetChar( c );
+	  while (!m_InputStack.Peek()->eof() && !EOL(c))
+	    GetChar(c);
 	}
-	else if( line.Right( 2 ) == "/*" )
-	{
+	else if (line.Right(2) == "/*") {
 	  // In a C style comment.  Ignore everything until
 	  //   comment closing is found.
-	  line = line.Left( line.Length() - 2 );
+	  line = line.Left(line.Length() - 2);
 
-	  SetPrompt( false );
+	  SetPrompt(false);
 	  gText comment = "  ";
-	  while( !m_InputStack.Peek()->eof() && 
-		( comment.Right( 2 ) != "*/" || quote ) )
-	  {
-	    GetChar( c );
+	  while (!m_InputStack.Peek()->eof() && 
+		 (comment.Right(2) != "*/" || quote)) {
+	    GetChar(c);
 	    comment += c;
 
-	    if( c == '\"' )
-	    {
+	    if (c == '\"') {
 	      bool escapeSeq = false;
-	      if( quote )
-		escapeSeq = IsQuoteEscapeSequence( comment );
-	      if( !escapeSeq )
+	      if (quote)
+		escapeSeq = IsQuoteEscapeSequence(comment);
+	      if (!escapeSeq)
 		quote = !quote;
 	    }
 	  }
-	  SetPrompt( true );
-
+	  SetPrompt(true);
 	}
 
-	//------------------- Include -------------------------------
-	else if( line.Right( 7 ) == "Include" )
-	{
-	  // Process Include[] calls.
-
-	  line = line.Left( line.Length() - 7 );
-
+	//
+	// Handle Include[] directives
+	//
+	else if (line.Right(7) == "Include") {
+	  line = line.Left(line.Length() - 7);
 	  c = ' ';
-	  while( !m_InputStack.Peek()->eof() && c == ' ' )
-	    GetChar( c );
-	  if( c != '[' )
-	  {
+	  
+	  while (!m_InputStack.Peek()->eof() && c == ' ')
+	    GetChar(c);
+
+	  if (c != '[') {
 	    line += "False";
 	    errorMsg = "Include[] syntax error; opening '[' not found";
 	    error = true;
@@ -179,10 +305,9 @@ gText gPreprocessor::GetLine( void )
 	  }
 
 	  c = ' ';
-	  while( !m_InputStack.Peek()->eof() && c == ' ' )
-	    GetChar( c );
-	  if( c != '\"' )
-	  {
+	  while (!m_InputStack.Peek()->eof() && c == ' ')
+	    GetChar(c);
+	  if (c != '\"') {
 	    line += "False";
 	    errorMsg = "Include[] syntax error; opening '\"' not found";
 	    error = true;
@@ -191,14 +316,14 @@ gText gPreprocessor::GetLine( void )
 
 	  gText filename;
 	  c = ' ';
-	  while( !m_InputStack.Peek()->eof() && c != '\"' && !EOL( c ) )
-	  {
-	    GetChar( c );
-	    if( c != '\"' )
+
+	  while (!m_InputStack.Peek()->eof() && c != '\"' && !EOL(c)) {
+	    GetChar(c);
+	    if (c != '\"')
 	      filename += c;
 	  }
-	  if( EOL( c ) )
-	  {
+
+	  if (EOL(c)) {
 	    line += "False";
 	    errorMsg = "Include[] syntax error; closing '\"' not found";
 	    error = true;
@@ -206,10 +331,9 @@ gText gPreprocessor::GetLine( void )
 	  }
 
 	  c = ' ';
-	  while( !m_InputStack.Peek()->eof() && c == ' ' )
-	    GetChar( c );
-	  if( c != ']' )
-	  {
+	  while (!m_InputStack.Peek()->eof() && c == ' ')
+	    GetChar(c);
+	  if (c != ']') {
 	    line += "False";
 	    errorMsg = "Include[] syntax error; closing ']' not found";
 	    error = true;
@@ -219,30 +343,24 @@ gText gPreprocessor::GetLine( void )
 	  // bring in the rest of this line
 	  gText restOfLine;
 	  c = ' ';
-	  while( !m_InputStack.Peek()->eof() && !EOL( c ) )
-	  {
-	    GetChar( c );
+	  while(!m_InputStack.Peek()->eof() && !EOL(c)) {
+	    GetChar(c);
 	    restOfLine += c;
 	  }
 
 	  // note: filename might be changed after this call
-	  gInput * input = 0;
-	  try {
-	    input = LoadInput( filename );
-	  }
-	  catch (gFileInput::OpenFailed &)  {
-	    line += "False";
-	    errorMsg = "Include file \"" + filename + "\" not found";
-	    error = true;
-	  }
+	  gInput *input = LoadInput(filename);
 	  
-	  if( input )
-	  {
-	    //	    assert( input->IsValid() );
+	  if (input) {
 	    line += "True";
-	    m_InputStack.Push( input );
-	    m_FileNameStack.Push( filename );
-	    m_LineNumberStack.Push( 1 );
+	    m_InputStack.Push(input);
+	    m_FileNameStack.Push(filename);
+	    m_LineNumberStack.Push(1);
+	  }
+	  else {
+	    line += "False";
+	    gerr << "GCL Warning: Include file \"" << filename;
+	    gerr << "\" not found.\n";
 	  }
 
 	  line += restOfLine;
@@ -250,23 +368,22 @@ gText gPreprocessor::GetLine( void )
 	  
 	} // "Include"
 
-	//------------------- GetPath -------------------------------
-	else if( line.Right( 7 ) == "GetPath" )
-	{
-	  // Process GetPath[] calls.
-
+	//
+	// Handle GetPath directive
+	//
+	else if (line.Right(7) == "GetPath") {
 	  bool text_found = false;
 	  bool closed_bracket = false;
 	  bool file = true;
 	  bool path = true;
 
-	  line = line.Left( line.Length() - 7 );
+	  line = line.Left(line.Length() - 7);
 
 	  c = ' ';
-	  while( !m_InputStack.Peek()->eof() && c == ' ' )
-	    GetChar( c );
-	  if( c != '[' )
-	  {
+	  while (!m_InputStack.Peek()->eof() && c == ' ')
+	    GetChar(c);
+
+	  if (c != '[') {
 	    line += "\"\"";
 	    errorMsg = "GetPath[] syntax error; opening '[' not found";
 	    error = true;
@@ -274,51 +391,44 @@ gText gPreprocessor::GetLine( void )
 	  }
 
 	  c = ' ';
-	  while( !m_InputStack.Peek()->eof() && c == ' ' )
-	    GetChar( c );
+	  while (!m_InputStack.Peek()->eof() && c == ' ')
+	    GetChar(c);
 	  text_found = false;
-	  if( c == 'T' ) // True
-	  {
-	    text_found = ExpectText( "rue" );
-	    if( text_found )
+	  if (c == 'T') { // True
+	    text_found = ExpectText("rue");
+	    if (text_found)
 	      file = true;
 	  }
-	  else if( c == 'F' )
-	  {
-	    text_found = ExpectText( "alse" );
-	    if( text_found )
+	  else if (c == 'F') {
+	    text_found = ExpectText("alse");
+	    if (text_found)
 	      file = false;
 	  }
-	  else if ( c == ']' )
-	  {
+	  else if (c == ']') {
 	    text_found = true;
 	    closed_bracket = true;
 	  }
-	  if( !text_found )
-	  {
+
+	  if (!text_found) {
 	    line += "\"\"";
 	    errorMsg = "GetPath[] syntax error (1)";
 	    error = true;
 	    break;
 	  }
 
-	  if( !closed_bracket )
-	  {
+	  if (!closed_bracket) {
 	    c = ' ';
-	    while( !m_InputStack.Peek()->eof() && c == ' ' )
-	      GetChar( c );
+	    while (!m_InputStack.Peek()->eof() && c == ' ')
+	      GetChar(c);
 	    text_found = false;
-	    if( c == ',' )
-	    {
+	    if (c == ',') {
 	      text_found = true;
 	    }
-	    else if( c == ']' )
-	    {
+	    else if (c == ']') {
 	      text_found = true;
 	      closed_bracket = true;
 	    }
-	    if( !text_found )
-	    {
+	    if (!text_found) {
 	      line += "\"\"";
 	      errorMsg = "GetPath[] syntax error (2)";
 	      error = true;
@@ -326,26 +436,22 @@ gText gPreprocessor::GetLine( void )
 	    }
 	  }
 
-	  if( !closed_bracket )
-	  {
+	  if (!closed_bracket) {
 	    c = ' ';
-	    while( !m_InputStack.Peek()->eof() && c == ' ' )
-	      GetChar( c );
+	    while (!m_InputStack.Peek()->eof() && c == ' ')
+	      GetChar(c);
 	    text_found = false;
-	    if( c == 'T' ) // True
-	    {
-	      text_found = ExpectText( "rue" );
-	      if( text_found )
+	    if (c == 'T') { // True
+	      text_found = ExpectText("rue");
+	      if (text_found)
 		path = true;
 	    }
-	    else if( c == 'F' )
-	    {
-	      text_found = ExpectText( "alse" );
-	      if( text_found )
+	    else if (c == 'F') {
+	      text_found = ExpectText("alse");
+	      if (text_found)
 		path = false;
 	    }
-	    if( !text_found )
-	    {
+	    if (!text_found) {
 	      line += "\"\"";
 	      errorMsg = "GetPath[] syntax error (3)";
 	      error = true;
@@ -353,13 +459,11 @@ gText gPreprocessor::GetLine( void )
 	    }
 	  }
 
-	  if( !closed_bracket )
-	  {
+	  if (!closed_bracket) {
 	    c = ' ';
-	    while( !m_InputStack.Peek()->eof() && c == ' ' )
-	      GetChar( c );
-	    if( c != ']' )
-	    {
+	    while (!m_InputStack.Peek()->eof() && c == ' ')
+	      GetChar(c);
+	    if (c != ']') {
 	      line += "\"\"";
 	      errorMsg = "GetPath[] syntax error; closing ']' not found";
 	      error = true;
@@ -372,18 +476,15 @@ gText gPreprocessor::GetLine( void )
 	  gText txt = GetFileName();
 	  const char SLASH = System::Slash();
 
-	  if( file && path )
-	  {
+	  if (file && path) {
 	  }
-	  else if( file )
-	  {
-	    if( txt.LastOccur( SLASH ) > 0 )
-	      txt = txt.Right( txt.Length() - txt.LastOccur( SLASH ) );
+	  else if (file) {
+	    if (txt.LastOccur(SLASH) > 0) 
+	      txt = txt.Right(txt.Length() - txt.LastOccur(SLASH));
 	  }
-	  else if( path )
-	  {
-	    if( txt.LastOccur( SLASH ) > 0 )
-	      txt = txt.Left( txt.LastOccur( SLASH ) );
+	  else if (path) {
+	    if (txt.LastOccur(SLASH) > 0)
+	      txt = txt.Left(txt.LastOccur(SLASH));
 	    else
 	      txt = "";
 	  }
@@ -393,34 +494,30 @@ gText gPreprocessor::GetLine( void )
 	  line += (gText) '\"' + (gText) txt + (gText) '\"';
 
 	} // "GetPath"
-
       }
     }
 
     // This outer loop deals with explicit line continuation characters
-    if( line.Right( 1 ) == '\\' )
-    {
-      if( !continuation )
-	SetPrompt( false );
+    if (line.Right( 1 ) == '\\') {
+      if (!continuation)
+	SetPrompt(false);
       continuation = true;
     }
   }
 
-  if( continuation )
-    SetPrompt( true );
+  if (continuation)
+    SetPrompt(true);
 
-  
-  if( error )
+  if (error)
     gerr << "GCL Error: " << errorMsg << '\n';
 
-
-  if( m_InputStack.Peek()->eof() ) {
+  if (m_InputStack.Peek()->eof()) {
     if (m_InputStack.Depth() > 1)
       delete m_InputStack.Pop();
     else
       m_InputStack.Pop();
-    (void) m_FileNameStack.Pop();
-    (void) m_LineNumberStack.Pop();
+    m_FileNameStack.Pop();
+    m_LineNumberStack.Pop();
   }
 
   return line;
@@ -429,79 +526,3 @@ gText gPreprocessor::GetLine( void )
 
 
 
-
-
-// note: filename might be changed after this call
-gInput* gPreprocessor::LoadInput( gText& name )
-{
-  gInput* _Input = NULL;
-
-  extern char* _SourceDir;
-  const char* SOURCE = _SourceDir; 
-  assert( SOURCE );
-  
-  const char SLASH = System::Slash();
-  
-  bool search = false;
-  if (strchr((char *) name, SLASH) == NULL)
-    search = true;
-  gText IniFileName;
-  
-  IniFileName = name;
-  _Input = 0;
-  try   {
-    _Input = new gFileInput(IniFileName);
-  }
-  catch (gFileInput::OpenFailed &)   {
-    if (search)   {
-      if (System::GetEnv("HOME") != NULL)   {
-	IniFileName = (gText) System::GetEnv("HOME") + 
-	  (gText) SLASH + (gText) name;
-	try   {
-	  _Input = new gFileInput(IniFileName);
-	}
-	catch (gFileInput::OpenFailed &) {_Input = 0;}
-	if(_Input) {name = IniFileName;return _Input;}
-      }
-      if (System::GetEnv("GCLLIB") != NULL)  {
-	IniFileName = (gText) System::GetEnv("GCLLIB") + 
-	  (gText) SLASH + (gText) name;
-	try  {
-	  _Input = new gFileInput(IniFileName);
-	}
-	catch (gFileInput::OpenFailed &){_Input = 0;}
-	if(_Input)  {name = IniFileName;return _Input;}
-      }
-      if (SOURCE != NULL)  {
-	IniFileName = (gText) SOURCE + (gText) SLASH + (gText) name;
-	try  {
-	  _Input = new gFileInput(IniFileName);
-	}
-	catch (gFileInput::OpenFailed &){_Input = 0;}
-	if(_Input)  {name = IniFileName;return _Input;}
-      }
-    }
-  }
-  return _Input;
-}
-
-
-
-
-bool gPreprocessor::ExpectText( const char* text )
-{
-  int length = strlen( text );
-  int i = 0;
-  char c = 0;
-
-  if( m_InputStack.Peek()->eof() )
-    return false;
-
-  for( i = 0; i < length; ++i )
-  {
-    GetChar( c );
-    if( m_InputStack.Peek()->eof() || c != text[i] )
-      return false;
-  }
-  return true;
-}
--- sources/gpreproc.h
+++ sources/gpreproc.h
@@ -10,109 +10,43 @@
 #ifndef GPREPROC_H
 #define GPREPROC_H
 
-
-#include <assert.h>
-#include <string.h>
-
 #include "gcmdline.h"
 #include "gtext.h"
 #include "gstack.h"
 
 
-class gPreprocessor
-{
+class gPreprocessor  {
 private:
-  gCmdLineInput* m_CmdLine;
+  gCmdLineInput *m_CmdLine;
 
-  gStack< gInput* > m_InputStack;
-  gStack< int >     m_LineNumberStack;
-  gStack< gText > m_FileNameStack;
+  gStack<gInput *> m_InputStack;
+  gStack<int> m_LineNumberStack;
+  gStack<gText> m_FileNameStack;
 
-  gText           m_RawLine;
-  gText           m_PrevFileName;
-  int               m_PrevLineNumber;
+  gText m_RawLine, m_PrevFileName;
+  int m_PrevLineNumber;
+  gText m_StartupString;
 
-  gText           m_StartupString;
+  // PRIVATE AUXILIARY MEMBER FUNCTIONS
+  gInput *LoadInput(gText &p_name);
+  bool ExpectText(const char *p_text);
   
+  bool EOL(char p_char) const { return (p_char == '\n' || p_char == '\r'); }
 
-
-  gInput* LoadInput( gText& name );
-  bool ExpectText( const char* text );
-  
-  bool EOL( char c ) const { return ( c == '\n' || c == '\r' ); }
-
-  void SetPrompt( bool prompt )
-  {
-    if( m_InputStack.Peek() == m_CmdLine )
-      m_CmdLine->SetPrompt( prompt );	  
-  }
-
-  void GetChar( char& c )
-  {
-    if( m_StartupString.Length() > 0 )
-    {
-      c = m_StartupString[0];
-      m_StartupString = m_StartupString.Right( m_StartupString.Length() - 1 );
-    }
-    else
-    {
-      assert( !eof() );
-      m_InputStack.Peek()->get( c );
-      if( EOL( c ) )
-	++m_LineNumberStack.Peek();
-    }
-
-    m_RawLine += c;
-  }
-
-  bool IsQuoteEscapeSequence( const gText& line ) const
-  {
-    bool backslash = false;
-    assert( line.Length() > 0 );
-    char c = line[ line.Length() - 1 ];
-    if( c == '\"' )
-    {
-      // if there's an odd number of consecutive backslashes,
-      //   then it's an escape sequence 
-      int i = 2;
-      while( line.Length() >= i && line[ line.Length() - i ] == '\\' )
-      {
-	backslash = !backslash;
-	++i;
-      }
-    }
-    return backslash;
-  }
-
+  void SetPrompt(bool p_prompt);
+  void GetChar(char &p_char);
+  bool IsQuoteEscapeSequence(const gText &p_line) const;
 
 public:
-
-  gPreprocessor( gCmdLineInput* cmdline, const char* cmd = NULL ) 
-    : m_CmdLine( cmdline ), m_PrevFileName( "console" ), m_PrevLineNumber( 1 ),
-      m_StartupString( cmd )
-  {
-    assert( m_CmdLine );
-    m_InputStack.Push( m_CmdLine );
-    m_FileNameStack.Push( m_PrevFileName );
-    m_LineNumberStack.Push( m_PrevLineNumber );
-
-    if(cmd && !EOL( m_StartupString[ m_StartupString.Length() - 1 ] ) )
-      m_StartupString += '\n';
-  }
-
-  ~gPreprocessor()
-  {
-    while( m_InputStack.Depth() > 1 )
-      delete m_InputStack.Pop();
-  }
-
+  gPreprocessor(gCmdLineInput *p_cmdline, const char *p_cmd = NULL); 
+  ~gPreprocessor();
 
   //----------------------------------------------
   // eof
   //   If true, then there is no more input from
   //   the preprocessor.
   //-----------------------------------------------
-  bool eof( void ) const { return m_InputStack.Depth() == 0; }
+  bool eof(void) const { return m_InputStack.Depth() == 0; }
 
 
   //---------------------------------------------
@@ -120,7 +54,7 @@ public:
   //   Returns the preprocessed lines.  See 
   //   gpreproc.cc for details.
   //----------------------------------------------
-  gText GetLine( void );
+  gText GetLine(void);
 
 
   //---------------------------------------------
@@ -128,9 +62,7 @@ public:
   //   Returns the un-preprocessed line corresponding to
   //   the previous GetLine() call.
   //----------------------------------------------
-  const gText& GetRawLine( void ) const
-  { return m_RawLine; }
-
+  const gText& GetRawLine(void) const  { return m_RawLine; }
 
 
   //---------------------------------------------
@@ -138,8 +70,7 @@ public:
   //   Returns the file name corresponding to
   //   the previous GetLine() call.
   //---------------------------------------------
-  const gText& GetFileName( void ) const 
-  { return m_PrevFileName; }
+  const gText& GetFileName(void) const { return m_PrevFileName; }
   
 
   //---------------------------------------------
@@ -147,14 +78,8 @@ public:
   //   Returns the line number corresponding to
   //   the previous GetLine() call.
   //---------------------------------------------
-  int GetLineNumber( void ) const 
-  { return m_PrevLineNumber; }
-
-
-
-
+  int GetLineNumber(void) const  { return m_PrevLineNumber; }
 };
 
+#endif // GPREPROC_H
 
-
-#endif // __gpreproc_h__
