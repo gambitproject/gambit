3675482ec36bd6a7b43539c8c95a485d594aa1c5
magyar <magyar@3fbfd45b-e82e-0410-bb8e-8470640bbd7f>
1998-08-22 23:07:36 +0000

    Converted assertions to exceptions

--- sources/gstack.h
+++ sources/gstack.h
@@ -1,8 +1,8 @@
-//#
-//# FILE: gstack.h -- Definition of gStack container type
-//#
-//# $Id$
-//#
+//
+// FILE: gstack.h -- Definition of gStack container type
+//
+// $Id$
+//
 
 #ifndef GSTACK_H
 #define GSTACK_H
@@ -15,27 +15,29 @@
 // allocation, and deallocation; the array currently cannot be expanded
 // or contracted at all.
 
-template <class T> class gStack   
-{
- protected:
+template <class T> class gStack  {
+protected:
   int depth;
   int max_depth;
   T*  contents;
 
- public:
-          // CONSTRUCTORS AND DESTRUCTOR
+public:
+  class StackUnderflow : public gException {
+  public:
+    virtual ~StackUnderflow() { }
+    gText Description(void) const { return "gStack underflow error"; }
+  };
+
+  // CONSTRUCTORS AND DESTRUCTOR
   gStack(int md = 10);       // initialize stack of maximum depth 'md'
   virtual ~gStack();    // destruct gStack
 
-          // GENERAL DATA ACCESS
+  // GENERAL DATA ACCESS
   int Depth(void) const;
   int MaxDepth(void) const;
     
-          // STACK OPERATIONS
-          //       Peek() and Pop() assume that depth > 0
-          // Failure to maintain these assertions on entry will
-          // cause a failed assert() call...
-
+  // STACK OPERATIONS
+  //       Peek() and Pop() assume that depth > 0
   void Push (T new_el);    // push an element onto top of stack
   T    Pop  (void);        // pop the top element off the stack
   T    Peek (void) const;  // get the top element without popping
@@ -45,7 +47,7 @@ template <class T> class gStack
                             //  version allows the TOS to be modified
                             //  without the overhead of a push+pop --
                             //  use this with care!)
-  void Resize( int new_max_depth );
+  void Resize(int new_max_depth);
   void Flush (void);       // empty the stack in one fell swoop
 };
 
