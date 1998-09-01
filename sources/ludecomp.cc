b8de053dab5231412d1ea19384bd716f3191c18d
rdm <rdm@3fbfd45b-e82e-0410-bb8e-8470640bbd7f>
1998-09-01 07:02:27 +0000

    Splitting out basis from ludecomp -- adding basis.cc to make.filelist

--- sources/ludecomp.cc
+++ sources/ludecomp.cc
@@ -10,14 +10,12 @@
 
 template class EtaMatrix< double >;
 template class gList< EtaMatrix< double > >;
-template class Basis< double >;
 template class LUdecomp< double >;
 
 template gOutput& operator<<( gOutput&, const EtaMatrix< double > &); 
 
 template class EtaMatrix< gRational >;
 template class gList< EtaMatrix< gRational > >;
-template class Basis< gRational >;
 template class LUdecomp< gRational >;
 
 template gOutput& operator<<( gOutput&, const EtaMatrix< gRational > &); 
