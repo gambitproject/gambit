dacdd90b23f02658817e8d93d8aff329dfbaf0c6
magyar <magyar@3fbfd45b-e82e-0410-bb8e-8470640bbd7f>
1998-09-03 15:48:48 +0000

    Moved non-type-dependent algorithm code from .imp files

--- sources/efgpure.cc
+++ sources/efgpure.cc
@@ -4,16 +4,70 @@
 // $Id$
 //
 
-#include "efgpure.imp"
-#include "rational.h"
+#include "efgpure.h"
 
-int EnumPure(const EFSupport &support,
-	     gList<BehavSolution> &solutions, double &time)
+#include "glist.h"
+#include "efg.h"
+#include "efgiter.h"
+#include "efgciter.h"
+#include "behavsol.h"
+
+int FindPureNash(const EFSupport &p_support,
+		 gList<BehavSolution> &p_solutions)
 {
-  EfgPSNEBySubgame module(support);
+  int index;
+  EfgContIter citer(p_support);
+  gPVector<gNumber> probs(p_support.Game().NumInfosets());
+
+  do  {
+    int flag = 1;
+    citer.GetProfile().InfosetProbs(probs);
+
+    EfgIter eiter(citer);
+
+    for (int pl = 1; flag && pl <= p_support.Game().NumPlayers(); pl++)  {
+      gNumber current = citer.Payoff(pl);
+      for (int iset = 1; flag && iset <= p_support.Game().Players()[pl]->NumInfosets();
+	         iset++)  {
+      	if (probs(pl, iset) == gNumber(0))   continue;
+	      Infoset *s = p_support.Game().Players()[pl]->Infosets()[iset];
+       	for (int act = 1; act <= s->NumActions(); act++)  {
+	  eiter.Next(pl, iset);
+	  if (eiter.Payoff(pl) > current)  {
+	    flag = 0;
+	    break;
+	  }
+      	}
+      }
+    }
+
+    if (flag)  {
+      BehavProfile<double> temp(p_support);
+      // zero out all the entries, since any equilibria are pure
+      ((gVector<double> &) temp).operator=(0);
+      const PureBehavProfile<gNumber> &profile = citer.GetProfile();
+      for (int pl = 1; pl <= p_support.Game().NumPlayers(); pl++)  {
+	for (int iset = 1; iset <= p_support.Game().Players()[pl]->NumInfosets();
+	     iset++)
+	  temp(pl, iset, profile.GetAction(p_support.Game().Players()[pl]->Infosets()[iset])->GetNumber()) = 1;
+      }
+
+      index = p_solutions.Append(BehavSolution(temp, EfgAlg_PURENASH));
+      p_solutions[index].SetIsNash(triTRUE);
+    }
+  }  while (citer.NextContingency());
+
+  return p_solutions.Length();
+}
+
+
+int EnumPure(const EFSupport &p_support,
+	     gList<BehavSolution> &p_solutions, double &p_time)
+{
+  EfgPSNEBySubgame module(p_support);
   module.Solve();
-  time = module.Time();
-  solutions = module.GetSolutions();
+  p_time = module.Time();
+  p_solutions = module.GetSolutions();
   return 1;
 }
 
--- sources/nfgpure.cc
+++ sources/nfgpure.cc
@@ -4,6 +4,52 @@
 // $Id$
 //
 
-#include "nfgpure.imp"
-#include "rational.h"
+#include "nfgpure.h"
 
+#include "gstream.h"
+#include "nfg.h"
+#include "nfgiter.h"
+#include "nfgciter.h"
+#include "glist.h"
+#include "mixed.h"
+
+void FindPureNash(const NFSupport &p_support, int p_max,
+		  gStatus &p_status, gList<MixedSolution> &p_solutions)
+{
+  const Nfg &nfg = p_support.Game();
+  NfgContIter citer(p_support);
+
+  do  {
+    p_status.Get();
+
+    bool flag = true;
+    NfgIter niter(citer);
+    
+    for (int pl = 1; flag && pl <= nfg.NumPlayers(); pl++)  {
+      gNumber current = nfg.Payoff(citer.GetOutcome(), pl);
+      for (int i = 1; i <= p_support.NumStrats(pl); i++)  {
+	niter.Next(pl);
+	if (nfg.Payoff(niter.GetOutcome(), pl) > current)  {
+	  flag = false;
+	  break;
+	}
+      }
+    }
+    
+    if (flag)  {
+      MixedProfile<gNumber> temp(p_support);
+      // zero out all the entries, since any equlibria are pure
+      ((gVector<gNumber> &) temp).operator=(gNumber(0));
+      gArray<int> profile = citer.Get();
+      for (int pl = 1; pl <= profile.Length(); pl++)
+	temp(pl, profile[pl]) = 1;
+      
+      int index = p_solutions.Append(MixedSolution(temp, NfgAlg_PURENASH));
+      if (p_solutions[index].LiapValue() < p_solutions[index].Epsilon())
+	p_solutions[index].SetIsNash(triTRUE);
+      else
+	p_solutions[index].SetIsNash(triFALSE);
+      if (p_max > 0 && index == p_max)  break;
+    }
+  }  while (citer.NextContingency());
+}
