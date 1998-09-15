df52df1f340d359415a0741e4bf01db04a18b9c1
magyar <magyar@3fbfd45b-e82e-0410-bb8e-8470640bbd7f>
1998-09-15 16:15:04 +0000

    Silenced (harmless) BC compiler warning

--- sources/system.cc
+++ sources/system.cc
@@ -9,98 +9,86 @@
 #include <string.h>
 
 #ifdef __GNUG__
-  #include <unistd.h>
+#include <unistd.h>
 #elif defined __BORLANDC__
-  #include <windows.h>
+#include <windows.h>
 #endif   // __GNUG__, __BORLANDC__
 
 #include "gstream.h"
 #include "system.h"
 
-System::System( void )
+const char *System::GetEnv(const char *name)
 {
-  assert( 0 );
+  assert(name != NULL);
+  assert(strlen(name) > 0);
+  return getenv(name);
 }
 
-const char* System::GetEnv( const char* name )
+int System::SetEnv(const char *name, const char *value)
 {
-  assert( name != NULL );
-  assert( strlen( name ) > 0 );
-  return getenv( name );
+  assert(name != NULL);
+  assert(strlen(name) > 0);
+  assert(value != NULL);
+  char *envstr = new char[strlen(name) + strlen(value) + 2];
+  strcpy(envstr, name);
+  strcat(envstr, "=");
+  strcat(envstr, value);
+  return putenv(envstr);
 }
 
-int System::SetEnv( const char* name, const char* value )
+int System::UnSetEnv(const char *name)
 {
-  assert( name != NULL );
-  assert( strlen( name ) > 0 );
-  assert( value != NULL );
-  char* envstr = new char[ strlen(name) + strlen(value) + 2 ];
-  strcpy( envstr, name );
-  strcat( envstr, "=" );
-  strcat( envstr, value );
-  return putenv( envstr );
-}
-
-int System::UnSetEnv( const char* name )
-{
-  assert( name != NULL );
-  assert( strlen( name ) > 0 );  
-  char* envstr = new char[ strlen(name) + 1 ];
-  strcpy( envstr, name );
-  return putenv( envstr );
+  assert(name != NULL);
+  assert(strlen(name) > 0);  
+  char *envstr = new char[strlen(name) + 1];
+  strcpy(envstr, name);
+  return putenv(envstr);
 }
 
 
-const char* System::GetCmdInterpreter( void )
+const char *System::GetCmdInterpreter(void)
 {
-  const char* command = NULL;
 #ifdef __GNUG__
-  command = System::GetEnv( "SHELL" );
+  return System::GetEnv("SHELL");
 #elif defined __BORLANDC__
-  command = System::GetEnv( "COMSPEC" );
+  return System::GetEnv("COMSPEC");
 #endif   // __GNUG__, __BORLANDC__
-  return command;
 }
 
-int System::Shell( const char* command )
+int System::Shell(const char *command)
 {
-  
-  if( command == NULL )
+  if (command == NULL)
     command = System::GetCmdInterpreter();
   
   int result = -1;
-  if( command == NULL )
-  {
+  if (command == NULL) {
     gerr << "System::Shell: Command interpreter not found or\n";
     gerr << "               feature not implemented for this compiler.\n";
   }
-  else
-  {
-    assert( command != NULL );
-    result = system( command );
+  else {
+    assert(command != NULL);
+    result = system(command);
   }
   return result;
 }
 
-
-int System::Spawn( const char* command )
+int System::Spawn(const char *command)
 {    
-  if( command == 0 )
+  if (command == 0)
     command = System::GetCmdInterpreter();
   
 #ifdef __GNUG__
   pid_t pid = fork();
-  if( pid == 0 )
-  {
-    System::Shell( command );
-    exit( 0 );
+  if (pid == 0) {
+    System::Shell(command);
+    exit(0);
   }
   return 0;
+
 #elif defined __BORLANDC__
 
-  int result = WinExec( command, SW_SHOW );  
-  if( 0 <= result && result < 32 )
-  {
+  int result = WinExec(command, SW_SHOW);  
+  if (0 <= result && result < 32) {
     gerr << "System::Spawn: Error executing \"" << command << "\"\n";
     return -1;
   }
@@ -112,7 +100,7 @@ int System::Spawn( const char* command )
 
 // This returns the slash character for the system;
 //   forward '/' for UNIX, backward '\' for DOS/Windows
-char System::Slash( void )
+char System::Slash(void)
 {
 #ifdef __GNUG__
   return '/';
@@ -120,3 +108,6 @@ char System::Slash( void )
   return '\\';
 #endif   // __GNUG__, __BORLANDC__  
 }
+
+
+
--- sources/system.h
+++ sources/system.h
@@ -1,36 +1,29 @@
 //
-// FILE: system.h -- Imeplements operating system specofic functions
+// FILE: system.h -- Imeplements operating system specific functions
 //
 // $Id$
 //
 
+#ifndef SYSTEM_H
+#define SYSTEM_H
 
-
-
-#ifndef __system_h__
-#define __system_h__
-
-
-class System
-{
+class System {
 private:
-  System( void );
+  System(void);
 
 public:
-  static const char* GetEnv( const char* name );
-  static int SetEnv( const char* name, const char* value );
-  static int UnSetEnv( const char* name );
+  static const char *GetEnv(const char *name);
+  static int SetEnv(const char *name, const char *value);
+  static int UnSetEnv(const char *name);
 
-  static const char* GetCmdInterpreter( void );
-  static int Shell( const char* command );
-  static int Spawn( const char* command );
+  static const char *GetCmdInterpreter(void);
+  static int Shell(const char *command);
+  static int Spawn(const char *command);
 
   // This returns the slash character for the system;
   //   forward '/' for UNIX, backward '\' for DOS/Windows
-  static char Slash( void );
-  
+  static char Slash(void);
 };
 
-
-#endif // __system_h__
+#endif  // SYSTEM_H
 
