04280c0b7666caf21921bd0970867e37f087f9e6
magyar <magyar@3fbfd45b-e82e-0410-bb8e-8470640bbd7f>
1998-09-28 17:41:13 +0000

    Changes to compile under BC

--- sources/gmatrix.cc
+++ sources/gmatrix.cc
@@ -7,23 +7,21 @@
 #include "gmatrix.imp"
 #include "rational.h"
 
-
 template class gMatrix<double>;
 template class gMatrix<gRational>;
 template class gMatrix<gInteger>;
 template class gMatrix<int>;
 
-template gVector<double> operator* (const gVector<double>&,
-				    const gMatrix<double>&);
-template gVector<gRational> operator* (const gVector<gRational>&,
-				    const gMatrix<gRational>&);
-template gVector<gRational> operator* (const gVector<gInteger>&,
-				    const gMatrix<gInteger>&);
-template gVector<int> operator* (const gVector<int>&,
-				    const gMatrix<int>&);
+template gVector<double> operator*(const gVector<double> &,
+				   const gMatrix<double> &);
+template gVector<gRational> operator*(const gVector<gRational> &,
+				      const gMatrix<gRational> &);
+template gVector<gInteger> operator*(const gVector<gInteger> &,
+				     const gMatrix<gInteger> &);
+template gVector<int> operator*(const gVector<int> &,
+				const gMatrix<int> &);
 
-
-template gOutput & operator<< (gOutput&, const gMatrix<double>&);
-template gOutput & operator<< (gOutput&, const gMatrix<gRational>&);
-template gOutput & operator<< (gOutput&, const gMatrix<gInteger>&);
-template gOutput & operator<< (gOutput&, const gMatrix<int>&);
+template gOutput &operator<<(gOutput &, const gMatrix<double> &);
+template gOutput &operator<<(gOutput &, const gMatrix<gRational> &);
+template gOutput &operator<<(gOutput &, const gMatrix<gInteger> &);
+template gOutput &operator<<(gOutput &, const gMatrix<int> &);
