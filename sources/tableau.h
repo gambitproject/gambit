981b6cd49e82304dfb07c55eb0ed3d9a37626ad2
rdm <rdm@3fbfd45b-e82e-0410-bb8e-8470640bbd7f>
1998-11-16 01:11:53 +0000

    Splitting LPTableau out into its own set of files

--- sources/tableau.h
+++ sources/tableau.h
@@ -1,6 +1,5 @@
 //
 // FILE: tableau1.h:  new Tableau classes  
-//                    To use, rename tableau1.h,cc,imp to tableau.h,cc,imp
 //
 // $Id$
 //
@@ -167,61 +166,7 @@ public:
 // Tableau<gRational>  currently in tableau2.h
 //  
 
-// ---------------------------------------------------------------------------
-//  LPTableau Stuff (For Linear Programming code)
-// ---------------------------------------------------------------------------
-
 #include "tableau2.h"
-
-template <class T> class LPTableau : public Tableau<T> {
-private:
-  gVector<T> dual;
-  gVector<T> unitcost;
-  gVector<T> cost;
-  gBlock<bool> UB,LB;  // does col have upper/lower bound?
-  gBlock<T> ub,lb;   // upper/lower bound
-  
-  void SolveDual();
-public:
-  LPTableau(const gMatrix<T> &A, const gVector<T> &b); 
-  LPTableau(const LPTableau<T>&);
-  virtual ~LPTableau();
-  
-  LPTableau<T>& operator=(const LPTableau<T>&);
-  
-      // cost information
-  void SetCost(const gVector<T>& ); // unit column cost := 0
-  void SetCost(const gVector<T>&, const gVector<T>& );
-  gVector<T> GetCost() const;
-  gVector<T> GetUnitCost() const;
-  T TotalCost(); // cost of current solution
-  T RelativeCost(int) const; // negative index convention
-  void RelativeCostVector(gVector<T> &, gVector<T> &); 
-  void DualVector(gVector<T> &) const; // column vector
-      // Redefined functions
-  void Refactor();
-  void Pivot(int outrow,int col);
-  void ReversePivots(gList<gArray<int> > &);
-  bool IsReversePivot(int i, int j);
-  void DualReversePivots(gList<gArray<int> > &);
-  bool IsDualReversePivot(int i, int j);
-  BFS<T> DualBFS(void) const;
-
-  // Inserts an artificial variable in column col
-  void InsertArtificial( int art, int col );
-
-  // Appends an artificial variable
-  int AppendArtificial( int art );
-
-  // Removes an artificial variable located at col.
-  void RemoveArtificial( int col );
-
-  // returns the index of the last artificial variable
-  int LastArtificial( void );
-
-  // Removes all artificial variables
-  void FlushArtificial( void );
-};
 
 #ifdef __GNUG__
 #include "rational.h"
@@ -235,12 +180,3 @@ template <class T> gOutput &operator<<(gOutput &, const Basis<T> &);
 #endif   // __GNUG__, __BORLANDC__
 
 #endif     // TABLEAU_H
-
-
-
-
-
-
-
-
-
