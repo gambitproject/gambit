7149ba10531b56c3dc349ccb72fa7c4436b0fd5f
rdm <rdm@3fbfd45b-e82e-0410-bb8e-8470640bbd7f>
1998-11-16 01:07:45 +0000

    Splitting LPTableau code out of tableau.* into its own files

---
+++ sources/lptab.cc
@@ -0,0 +1,9 @@
+//#
+//# FILE: lptab.cc -- LPTableau and basis class instantiations
+//#
+//#
+
+#include "lptab.imp"
+
+template class LPTableau<double>;
+template class LPTableau<gRational>;
---
+++ sources/lptab.h
@@ -0,0 +1,63 @@
+//
+// FILE: lptab.h:  LPTableau classes  
+//
+// ---------------------------------------------------------------------------
+//  LPTableau Stuff (For Linear Programming code)
+// ---------------------------------------------------------------------------
+
+#ifndef LPTAB_H  
+#define LPTAB_H
+
+#include "tableau.h"
+
+template <class T> class LPTableau : public Tableau<T> {
+private:
+  gVector<T> dual;
+  gVector<T> unitcost;
+  gVector<T> cost;
+  gBlock<bool> UB,LB;  // does col have upper/lower bound?
+  gBlock<T> ub,lb;   // upper/lower bound
+  
+  void SolveDual();
+public:
+  LPTableau(const gMatrix<T> &A, const gVector<T> &b); 
+  LPTableau(const LPTableau<T>&);
+  virtual ~LPTableau();
+  
+  LPTableau<T>& operator=(const LPTableau<T>&);
+  
+      // cost information
+  void SetCost(const gVector<T>& ); // unit column cost := 0
+  void SetCost(const gVector<T>&, const gVector<T>& );
+  gVector<T> GetCost() const;
+  gVector<T> GetUnitCost() const;
+  T TotalCost(); // cost of current solution
+  T RelativeCost(int) const; // negative index convention
+  void RelativeCostVector(gVector<T> &, gVector<T> &); 
+  void DualVector(gVector<T> &) const; // column vector
+      // Redefined functions
+  void Refactor();
+  void Pivot(int outrow,int col);
+  void ReversePivots(gList<gArray<int> > &);
+  bool IsReversePivot(int i, int j);
+  void DualReversePivots(gList<gArray<int> > &);
+  bool IsDualReversePivot(int i, int j);
+  BFS<T> DualBFS(void) const;
+
+  // Inserts an artificial variable in column col
+  void InsertArtificial( int art, int col );
+
+  // Appends an artificial variable
+  int AppendArtificial( int art );
+
+  // Removes an artificial variable located at col.
+  void RemoveArtificial( int col );
+
+  // returns the index of the last artificial variable
+  int LastArtificial( void );
+
+  // Removes all artificial variables
+  void FlushArtificial( void );
+};
+
+#endif     // LPTAB_H
