9c874ce94e294d198a2a89b197b61277c82662d4
magyar <magyar@3fbfd45b-e82e-0410-bb8e-8470640bbd7f>
1998-11-18 00:23:00 +0000

    Changes to remove obsolete polensub.imp, and to set up PolEnum for use with GUI

--- sources/polensub.cc
+++ sources/polensub.cc
@@ -1,11 +1,47 @@
 //
-// FILE: polensub.cc -- Instantiation of PolEnumBySubgame
+// FILE: polensub.cc -- Implementation of PolEnumBySubgame
 //
 // $Id$ 
 //
 
-#include "rational.h"
-#include "polensub.imp"
+#include "polensub.h"
+
+int PolEnumBySubgame::SolveSubgame(const Efg &E, const EFSupport &sup,
+				   gList<BehavSolution> &solns)
+{
+  Nfg *N = MakeReducedNfg(sup);
+
+  NFSupport *S = new NFSupport(*N);
+  
+  ViewNormal(*N, S);
+  
+  gList<MixedSolution> solutions;
+
+  long neval;
+  double time;
+
+  PolEnum(*S, params, solutions, neval, time);
+
+  nevals += neval;
+  
+  for (int i = 1; i <= solutions.Length(); i++)  {
+    BehavProfile<gNumber> bp(sup);
+    MixedToBehav(*N, MixedProfile<gNumber>(solutions[i]), E, bp);
+    solns.Append(bp);
+  }
+
+  delete S;
+  delete N;
+
+  return 0;
+}
+
+PolEnumBySubgame::PolEnumBySubgame(const EFSupport &S,
+				   const PolEnumParams &p, int max)
+  : SubgameSolver(S, max), nevals(0), params(p)
+{ }
+
+PolEnumBySubgame::~PolEnumBySubgame()   { }
 
 int PolEnum(const EFSupport &support, const PolEnumParams &params,
 	    gList<BehavSolution> &solutions, long &nevals, double &time)
--- sources/polensub.h
+++ sources/polensub.h
@@ -9,6 +9,23 @@
 
 #include "polenum.h"
 #include "behavsol.h"
+#include "subsolve.h"
+
+class PolEnumBySubgame : public SubgameSolver  {
+  private:
+    int nevals;
+    PolEnumParams params;
+
+    int SolveSubgame(const Efg &, const EFSupport &,
+		     gList<BehavSolution> &);
+    EfgAlgType AlgorithmID() const { return EfgAlg_POLENSUB; }    
+
+  public:
+    PolEnumBySubgame(const EFSupport &, const PolEnumParams &, int max = 0);
+    virtual ~PolEnumBySubgame();
+
+    int NumEvals(void) const   { return nevals; }
+};
 
 int PolEnum(const EFSupport &, const PolEnumParams &,
 	    gList<BehavSolution> &, long &nevals, double &time);
