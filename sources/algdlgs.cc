65f6ce370cb559427f2286fbea5ed3ad54e5bb63
rdm <rdm@3fbfd45b-e82e-0410-bb8e-8470640bbd7f>
1998-12-19 20:39:56 +0000

    Changes to get the pxi shell commands to work correctly -- got rid code appending path to default files.  Changed so that pxifile is only written to gambit.ini file when using save option

--- sources/algdlgs.cc
+++ sources/algdlgs.cc
@@ -76,7 +76,7 @@ OutputParamsSettings::~OutputParamsSettings(void)
   delete [] m_precisionStr;
   delete m_precisionList;
 
-  if (outfile && outfile != wout && outfile != &gnull) 
+  if (outfile && outfile != wout && outfile != &gnull)
     delete outfile;
 
   if (errfile && errfile != wout && errfile != &gnull) 
@@ -157,9 +157,9 @@ gPrecision OutputParamsSettings::Precision(void) const
 //            OutputParamsDialog: Member function definitions
 //========================================================================
 
-OutputParamsDialog::OutputParamsDialog(const char *label, 
+OutputParamsDialog::OutputParamsDialog(const char *label,
                                        wxWindow *parent, 
-                                       const char *help_str) 
+                                       const char *help_str)
   : MyDialogBox(parent, (char *)label, help_str)
 { }
 
@@ -195,7 +195,7 @@ void OutputParamsDialog::MakeCommonFields(bool p_dominance, bool p_subgames,
     Add(wxMakeFormString("Method", &dom_method_str, wxFORM_RADIOBOX,
              new wxList(wxMakeConstraintStrings(dom_method_list),
                     0), 0, wxVERTICAL));
-  
+
     Add(wxMakeFormNewLine());
   }
   
@@ -208,7 +208,7 @@ void OutputParamsDialog::MakeCommonFields(bool p_dominance, bool p_subgames,
 
   Add(wxMakeFormMessage("Algorithm behavior:"));
   Add(wxMakeFormNewLine());
-}  
+}
 
 void OutputParamsDialog::MakeOutputFields(unsigned int fields)
 {
@@ -222,7 +222,7 @@ void OutputParamsDialog::MakeOutputFields(unsigned int fields)
   }
 
   if ((fields & MAXSOLN_FIELD) && !(fields & SPS_FIELD)) {
-    Add(wxMakeFormShort("Max Ttl Solns", &m_stopAfter, wxFORM_DEFAULT, 
+    Add(wxMakeFormShort("Max Ttl Solns", &m_stopAfter, wxFORM_DEFAULT,
             NULL, NULL, wxVERTICAL, 100));
     Add(wxMakeFormNewLine());
   }
@@ -250,7 +250,7 @@ void OutputParamsDialog::MakeOutputFields(unsigned int fields)
   }
 
   Add(wxMakeFormString("Trace Level", &trace_str, wxFORM_CHOICE,
-               new wxList(wxMakeConstraintStrings(trace_list), 0), 
+               new wxList(wxMakeConstraintStrings(trace_list), 0),
                0, wxVERTICAL));
   Add(wxMakeFormNewLine());
 }
@@ -261,7 +261,7 @@ void OutputParamsDialog::MakeOutputFields(unsigned int fields)
 #define SAVED_NAME      1
 #define PROMPT_NAME     2
 
-int PxiParamsSettings::naming_option = 0;
+int PxiParamsSettings::naming_option = DEFAULT_NAME;
 
 PxiParamsSettings::PxiParamsSettings(const char *alg, const char *fn)
 {
@@ -274,37 +274,36 @@ PxiParamsSettings::PxiParamsSettings(const char *alg, const char *fn)
   sprintf(tmp_str, "%s-Plot-Type", algname);
   wxGetResourceStr(PARAMS_SECTION, tmp_str, type_str, defaults_file);
   pxiname = new char[250];
-  
+
   if (naming_option == SAVED_NAME)
     wxGetResourceStr(PARAMS_SECTION, "Pxi-Saved-Name", pxiname, defaults_file);
   
   if (naming_option == DEFAULT_NAME)
-    strcpy(pxiname, wxOutputFile(filename));
-
+    strcpy(pxiname, FileNameFromPath(wxOutputFile(filename)));
+  
   if (naming_option == PROMPT_NAME) {
     if (FromDialog()) { // will be set later.  this is just the dialog settings
       strcpy(pxiname, "PROMPT");
     }
     else {
-      char *s = wxGetTextFromUser("Pxi data output file", 
-				  "Enter File Name", filename);
+      char *s = wxGetTextFromUser("Pxi data output file",
+				  "Enter File Name", wxOutputFile(filename));
+      if (!s)
+	s = "junk.pxi";
       
-      if (!s) 
-	s = "pxi.out";
-
       strcpy(pxiname, s);
     }
   }
-
+  
   name_option_str = new char[20];
   pxi_command = new char[250];
   wxGetResourceStr(PARAMS_SECTION, "Pxi-Command", pxi_command, defaults_file);
   wxGetResource(PARAMS_SECTION, "Run-Pxi", &run_pxi, defaults_file);
-  
+
   type_list = new wxStringList("Lin", "Log", 0);
   name_option_list = new wxStringList("Default", "Saved", "Prompt", 0);
   strcpy(name_option_str, (char *)name_option_list->Nth(naming_option)->Data());
-  
+
   pxifile = 0;
 }
 
@@ -326,47 +325,28 @@ int PxiParamsSettings::PxiType(void)
 // Run Pxi
 int PxiParamsSettings::RunPxi(void)
 {
-    if (pxifile && run_pxi && pxi_command && pxiname)
+  if (pxifile && run_pxi && pxi_command && pxiname)
     {
-        // first of all, close the pxifile
-        delete pxifile;
-        pxifile = 0;
-
-        gText pxi_run(pxi_command);
-        pxi_run += " ";
-        // if the filename includes path, use it, if not, append current dir
-	//        char *fname = copystring(FileNameFromPath(pxiname));
-	char *fname = copystring(pxiname);
-        gText pxifilename;
-
-        if (strcmp(fname, pxiname) == 0)
-        {
-            pxifilename = wxGetWorkingDirectory();
-#ifdef wx_x
-            pxifilename += '/';
-#else
-            pxifilename += '\\';
-#endif
-            pxifilename += pxiname;
-        }
-        else
-        {
-            pxifilename = pxiname;
-        }
-
-        pxi_run += pxifilename;
-        delete [] fname;
-        Bool ok = wxExecute((char *)pxi_run);
-
-        if (!ok) wxMessageBox("PXI could not run!\nIt was either not found or\n"
-                              " there was insufficient memory/resources.\n"
-                              "Please check the path and free memory.", 
-                              "PXI Error");
-        return ok;
+      // first of all, close the pxifile
+      delete pxifile;
+      pxifile = 0;
+      
+      // shell out the pxi command
+      gText pxi_run(pxi_command);
+      pxi_run += " ";
+      gText pxifilename(pxiname);
+      pxi_run += pxifilename;
+      Bool ok = wxExecute((char *)pxi_run);
+      
+      if (!ok) wxMessageBox("PXI could not run!\nIt was either not found or\n"
+			    " there was insufficient memory/resources.\n"
+			    "Please check the path and free memory.",
+			    "PXI Error");
+      return ok;
     }
-    else
+  else
     {
-        return 0;
+      return 0;
     }
 }
 
@@ -374,31 +354,32 @@ int PxiParamsSettings::RunPxi(void)
 // Save Defaults
 void PxiParamsSettings::SaveDefaults(void)
 {
-    wxWriteResource(PARAMS_SECTION, "Pxi-Command", pxi_command, defaults_file);
+  wxWriteResource(PARAMS_SECTION, "Pxi-Command", pxi_command, defaults_file);
+  if(naming_option==SAVED_NAME)
     wxWriteResource(PARAMS_SECTION, "Pxi-Saved-Name", pxiname, defaults_file);
-    naming_option = wxListFindString(name_option_list, name_option_str);
-    char tmp_str[100];
-    sprintf(tmp_str, "%s-Plot-Type", algname);
-    wxWriteResource(PARAMS_SECTION, tmp_str, type_str, defaults_file);
-    wxWriteResource(PARAMS_SECTION, "Run-Pxi", run_pxi, defaults_file);
+  naming_option = wxListFindString(name_option_list, name_option_str);
+  char tmp_str[100];
+  sprintf(tmp_str, "%s-Plot-Type", algname);
+  wxWriteResource(PARAMS_SECTION, tmp_str, type_str, defaults_file);
+  wxWriteResource(PARAMS_SECTION, "Run-Pxi", run_pxi, defaults_file);
 }
 
 // Pxi Params Settings Destructor
 PxiParamsSettings::~PxiParamsSettings(void)
 {
-    SaveDefaults();
-    delete [] algname;
-    delete [] filename;
-    delete [] pxiname;
-    delete [] type_str;
-    delete [] pxi_command;
-    delete [] name_option_str;
-    delete type_list;
-    delete name_option_list;
-
-    if (pxifile) 
-        delete pxifile;
-    // could have been deleted in RunPxi
+  SaveDefaults();
+  delete [] algname;
+  delete [] filename;
+  delete [] pxiname;
+  delete [] type_str;
+  delete [] pxi_command;
+  delete [] name_option_str;
+  delete type_list;
+  delete name_option_list;
+  
+  if (pxifile)
+    delete pxifile;
+  // could have been deleted in RunPxi
 };
 
 
@@ -407,30 +388,30 @@ PxiParamsSettings::~PxiParamsSettings(void)
 PxiParamsDialog::PxiParamsDialog(const char *alg, const char *label, 
                                  const char *fn, wxWindow *parent,
                                  const char *help_str)
-    : OutputParamsDialog(label, parent, help_str), PxiParamsSettings(alg, fn)
+  : OutputParamsDialog(label, parent, help_str), PxiParamsSettings(alg, fn)
 { }
 
 // Make Pxi Fields
 void PxiParamsDialog::MakePxiFields(void)
 {
-    Form()->Add(wxMakeFormNewLine());
-
-    Form()->Add(wxMakeFormString("Plot Type", &type_str, wxFORM_RADIOBOX,
-                                 new wxList(wxMakeConstraintStrings(type_list), 0)));
-
-    Form()->Add(wxMakeFormString("PxiFile", &pxiname, 
-                                 wxFORM_DEFAULT, NULL, NULL, 0,
-                                 300));
-
-    Add(wxMakeFormString("Next", &name_option_str, wxFORM_RADIOBOX,
-                         new wxList(wxMakeConstraintStrings(name_option_list), 0)));
-    Form()->Add(wxMakeFormNewLine());
-
-    Form()->Add(wxMakeFormBool("Run PXI", &run_pxi));
-
-    Form()->Add(wxMakeFormString("PXI Command", &pxi_command,
-                                 wxFORM_DEFAULT, NULL, NULL, 0,
-                                 300));
+  Form()->Add(wxMakeFormNewLine());
+  
+  Form()->Add(wxMakeFormString("Plot Type", &type_str, wxFORM_RADIOBOX,
+			       new wxList(wxMakeConstraintStrings(type_list), 0)));
+  
+  Form()->Add(wxMakeFormString("PxiFile", &pxiname, 
+			       wxFORM_DEFAULT, NULL, NULL, 0,
+			       300));
+  
+  Add(wxMakeFormString("Next", &name_option_str, wxFORM_RADIOBOX,
+		       new wxList(wxMakeConstraintStrings(name_option_list), 0)));
+  Form()->Add(wxMakeFormNewLine());
+  
+  Form()->Add(wxMakeFormBool("Run PXI", &run_pxi));
+  
+  Form()->Add(wxMakeFormString("PXI Command", &pxi_command,
+			       wxFORM_DEFAULT, NULL, NULL, 0,
+			       300));
 }
 
 // Destructor
