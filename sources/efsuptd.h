0bdbb60d88fe0df94da807327fefda29a3ad3b9a
magyar <magyar@3fbfd45b-e82e-0410-bb8e-8470640bbd7f>
1999-01-06 20:43:34 +0000

    Implemented OnClose for support selection dialogs (also cleaned up and organized dialog code a bit)

--- sources/efsuptd.h
+++ sources/efsuptd.h
@@ -1,161 +1,63 @@
-// File: efsuptd.h -- Declarations of dialogs for dealing with EF
-// supports.
+//
+// FILE: efsuptd.h -- Declarations of dialogs for selecting supports on
+//                    the extensive form
+//
 // $Id$
+//
+
 #ifndef EFSUPTD_H
 #define EFSUPTD_H
-#include "elimdomd.h"
-class EFSupportInspectDialog:public wxDialogBox
-{
-private:
-	EfgShow *es;
-	gList<EFSupport *> &sups;
-	int init_disp,init_cur;
-	wxText *cur_dim,*disp_dim;
-	wxChoice *disp_item,*cur_item;
-	wxCheckBox *root_item;
-	char *cur_str,*disp_str;
-	wxStringList *support_list;
-	Bool root_reachable;
-// Low level event handlers
-	static void cur_func(wxChoice &ob,wxEvent &)
-	{((EFSupportInspectDialog *)ob.GetClientData())->OnCur(ob.GetSelection()+1);}
-	static void disp_func(wxChoice &ob,wxEvent &)
-	{((EFSupportInspectDialog *)ob.GetClientData())->OnDisp(ob.GetSelection()+1);}
-	static void new_sup_func(wxButton &ob,wxEvent &)
-	{((EFSupportInspectDialog *)ob.GetClientData())->OnNewSupport();}
-	static void change_sup_func(wxButton &ob,wxEvent &)
-	{((EFSupportInspectDialog *)ob.GetClientData())->OnChangeSupport();}
-	static void help_func(wxButton &,wxEvent &)
-	{wxHelpContents(EFG_SUPPORTS_HELP);}
-	static void close_func(wxButton &ob,wxEvent &)
-	{((EfgShow *)ob.GetClientData())->ChangeSupport(DESTROY_DIALOG);}
-	static void remove_sup_func(wxButton &ob,wxEvent &)
-	{((EFSupportInspectDialog *)ob.GetClientData())->OnRemoveSupport();}
 
-// High level event handlers
-	void OnNewSupport(void)
-	{
-	if (es->MakeSupport())
-	{
-		disp_item->Append(ToText(sups.Length()));
-		disp_item->SetSize(-1,-1,-1,-1);
-		cur_item->Append(ToText(sups.Length()));
-		cur_item->SetSize(-1,-1,-1,-1);
-	}
-	}
-	void OnRemoveSupport(void);
-	void OnChangeSupport(void)
-	{
-	es->ChangeSupport(UPDATE_DIALOG);init_disp=DispSup();init_cur=CurSup();
-	}
-	void OnCur(int cur_sup)
-	{
-	cur_dim->SetValue(gpvect_to_string(sups[cur_sup]->NumActions()));
-	disp_dim->SetValue(gpvect_to_string(sups[cur_sup]->NumActions()));
-	disp_item->SetSelection(cur_sup-1);
-	}
-	void OnDisp(int disp_sup)
-	{disp_dim->SetValue(gpvect_to_string(sups[disp_sup]->NumActions()));}
+#include "elimdomd.h"
+
+class EFSupportInspectDialog : public wxDialogBox {
+private:
+  EfgShow *es;
+  gList<EFSupport *> &sups;
+  int init_disp,init_cur;
+  wxText *cur_dim,*disp_dim;
+  wxChoice *disp_item,*cur_item;
+  wxCheckBox *root_item;
+  char *cur_str,*disp_str;
+  wxStringList *support_list;
+  Bool root_reachable;
+
+  // Low level event handlers
+  static void cur_func(wxChoice &ob,wxEvent &)
+    {((EFSupportInspectDialog *)ob.GetClientData())->OnCur(ob.GetSelection()+1);}
+  static void disp_func(wxChoice &ob,wxEvent &)
+    {((EFSupportInspectDialog *)ob.GetClientData())->OnDisp(ob.GetSelection()+1);}
+  static void new_sup_func(wxButton &ob,wxEvent &)
+    {((EFSupportInspectDialog *)ob.GetClientData())->OnNewSupport();}
+  static void change_sup_func(wxButton &ob,wxEvent &)
+    {((EFSupportInspectDialog *)ob.GetClientData())->OnChangeSupport();}
+  static void help_func(wxButton &,wxEvent &)
+    {wxHelpContents(EFG_SUPPORTS_HELP);}
+  static void close_func(wxButton &ob,wxEvent &)
+    {((EfgShow *)ob.GetClientData())->ChangeSupport(DESTROY_DIALOG);}
+  static void remove_sup_func(wxButton &ob,wxEvent &)
+    {((EFSupportInspectDialog *)ob.GetClientData())->OnRemoveSupport();}
+
+  // High level event handlers
+  void OnNewSupport(void);
+  void OnRemoveSupport(void);
+  void OnChangeSupport(void);
+  void OnCur(int cur_sup);
+  void OnDisp(int disp_sup);
+
 // Utility funcs
-	static gText gpvect_to_string(const gPVector<int> &a)
-	{
-	int sum;
-	gText tmp='(';
-	for (int i=1;i<=a.Lengths().Length();i++)
-	{
-	  sum=0;
-		for (int j=1;j<=a.Lengths()[i];j++) sum+=a(i,j);
-		tmp+=ToText(sum)+((i==a.Lengths().Length()) ? ")" : ",");
-	}
-	return tmp;
-	}
+  static gText gpvect_to_string(const gPVector<int> &);
+
 public:
-	EFSupportInspectDialog(gList<EFSupport *> &sups_,int cur_sup,
-											int disp_sup,EfgShow *es_,wxWindow *parent=0)
-		: wxDialogBox(parent,"Supports"),es(es_),sups(sups_)
-	{
-  init_disp=disp_sup;init_cur=cur_sup;
-	wxForm *f=new wxForm(0);
-	SetLabelPosition(wxVERTICAL);
-	cur_dim=new wxText(this,0,"Current",
-											gpvect_to_string(sups[cur_sup]->NumActions()),
-											-1,-1,80,-1,wxREADONLY);
-	disp_dim=new wxText(this,0,"Display",
-											gpvect_to_string(sups[disp_sup]->NumActions()),
-											-1,-1,80,-1,wxREADONLY);
-	support_list=wxStringListInts(sups.Length());
-	cur_str=new char[10];strcpy(cur_str,ToText(cur_sup));
-	disp_str=new char[10];strcpy(disp_str,ToText(disp_sup));
-	wxFormItem *cur_fitem=wxMakeFormString("",&cur_str,wxFORM_CHOICE,
-					new wxList(wxMakeConstraintStrings(support_list),0));
-	f->Add(cur_fitem);
-	f->Add(wxMakeFormMessage("      ")); // fix the spacing... not neat but..
-	wxFormItem *disp_fitem=wxMakeFormString("",&disp_str,wxFORM_CHOICE,
-					new wxList(wxMakeConstraintStrings(support_list),0));
-	f->Add(disp_fitem);
-	f->Add(wxMakeFormNewLine());
-	wxFormItem *root_fitem=wxMakeFormBool("Root reachable only",&root_reachable);
-	f->Add(root_fitem);
-	f->Add(wxMakeFormNewLine());
-	wxFormItem *newsup_fitem=wxMakeFormButton("New",(wxFunction)new_sup_func);
-	f->Add(newsup_fitem);
-	wxFormItem *rmvsup_fitem=wxMakeFormButton("Remove",(wxFunction)remove_sup_func);
-	f->Add(rmvsup_fitem);
-	f->Add(wxMakeFormNewLine());
-	wxFormItem *close_fitem=wxMakeFormButton("Close",(wxFunction)close_func);
-	f->Add(close_fitem);
-	wxFormItem *cngsup_fitem=wxMakeFormButton("Apply",(wxFunction)change_sup_func);
-	f->Add(cngsup_fitem);
-	wxFormItem *help_fitem=wxMakeFormButton("?",(wxFunction)help_func);
-	f->Add(help_fitem);
-	f->AssociatePanel(this);
-	cur_item=(wxChoice *)cur_fitem->GetPanelItem();
-	cur_item->Callback((wxFunction)cur_func);
-	cur_item->SetClientData((char *)this);
-	disp_item=(wxChoice *)disp_fitem->GetPanelItem();
-	disp_item->SetClientData((char *)this);
-	disp_item->Callback((wxFunction)disp_func);
-	((wxButton *)newsup_fitem->GetPanelItem())->SetClientData((char *)this);
-	((wxButton *)rmvsup_fitem->GetPanelItem())->SetClientData((char *)this);
-	((wxButton *)cngsup_fitem->GetPanelItem())->SetClientData((char *)this);
-	((wxButton *)help_fitem->GetPanelItem())->SetClientData((char *)this);
-	((wxButton *)close_fitem->GetPanelItem())->SetClientData((char *)es);
-	root_item=(wxCheckBox *)root_fitem->GetPanelItem();
-	Fit();
-	Show(TRUE);
-	}
-	// Data Access members
-	int 	CurSup(void) {return cur_item->GetSelection()+1;}
-	int 	DispSup(void) {return disp_item->GetSelection()+1;}
-	Bool	RootReachable(void) {return root_item->GetValue();}
+  EFSupportInspectDialog(gList<EFSupport *> &sups_, int cur_sup,
+			 int disp_sup, EfgShow *es_, wxWindow *parent = 0);
+
+  Bool OnClose(void);
+
+  // Data Access members
+  int CurSup(void) { return (cur_item->GetSelection() + 1); }
+  int DispSup(void) { return (disp_item->GetSelection() + 1); }
+  Bool RootReachable(void) { return root_item->GetValue(); }
 };
 
-
-// Note can not delete the first support
-void EFSupportInspectDialog::OnRemoveSupport(void)
-{
-SupportRemoveDialog SRD(this,sups.Length());
-if (SRD.Completed()==wxOK)
-{
-gArray<bool> selected(SRD.Selected());
-bool revert=false;
-int i;
-for (i=sups.Length();i>=2;i--)
-	if (selected[i])
-	{
-		delete sups.Remove(i);
-		if (i==init_cur || i==init_disp && revert==false)
-		{
-			wxMessageBox("Display/Current support deleted.\nReverting to full support");
-			revert=true;
-		}
-	}
-disp_item->Clear();cur_item->Clear();
-for (i=1;i<=sups.Length();i++)
-	{disp_item->Append(ToText(i));cur_item->Append(ToText(i));}
-disp_item->SetSize(-1,-1,-1,-1);cur_item->SetSize(-1,-1,-1,-1);
-disp_item->SetSelection(0);cur_item->SetSelection(0);
-if (revert) es->ChangeSupport(UPDATE_DIALOG);
-}
-}
-#endif
+#endif  // EFSUPTD_H
--- sources/nfsuptd.h
+++ sources/nfsuptd.h
@@ -1,149 +1,58 @@
 //
-// FILE: nfsuptd.h -- Declarations of dialogs for dealing with NF
-// supports.
+// FILE: nfsuptd.h -- Declarations of dialogs for selecting supports on
+//                    the normal form
 //
 // $Id$
 //
 
 #ifndef NFSUPTD_H
 #define NFSUPTD_H
-class NfgShow;
-class NFSupportInspectDialog:public wxDialogBox
-{
-private:
-	NfgShow *bns;
-	gList<NFSupport *> &sups;
-	int init_disp,init_cur;
-	wxText *cur_dim,*disp_dim;
-	wxChoice *disp_item,*cur_item;
-	char *cur_str,*disp_str;
-	wxStringList *support_list;
-// Low level event handlers
-	static void cur_func(wxChoice &ob,wxEvent &)
-	{((NFSupportInspectDialog *)ob.GetClientData())->OnCur(ob.GetSelection()+1);}
-	static void disp_func(wxChoice &ob,wxEvent &)
-	{((NFSupportInspectDialog *)ob.GetClientData())->OnDisp(ob.GetSelection()+1);}
-	static void new_sup_func(wxButton &ob,wxEvent &)
-	{((NFSupportInspectDialog *)ob.GetClientData())->OnNewSupport();}
-	static void change_sup_func(wxButton &ob,wxEvent &)
-	{((NFSupportInspectDialog *)ob.GetClientData())->OnChangeSupport();}
-	static void help_func(wxButton &,wxEvent &)
-	{wxHelpContents(NFG_SUPPORTS_HELP);}
-	static void close_func(wxButton &ob,wxEvent &)
-	{((NfgShow *)ob.GetClientData())->ChangeSupport(DESTROY_DIALOG);}
-	static void remove_sup_func(wxButton &ob,wxEvent &)
-	{((NFSupportInspectDialog *)ob.GetClientData())->OnRemoveSupport();}
-// High level event handlers
-	void OnNewSupport(void)
-	{
-	if (bns->MakeSupport())
-	{
-		disp_item->Append(ToText(sups.Length()));
-		disp_item->SetSize(-1,-1,-1,-1);
-		cur_item->Append(ToText(sups.Length()));
-		cur_item->SetSize(-1,-1,-1,-1);
-	}
-	}
-	void OnRemoveSupport(void);
-	void OnChangeSupport(void)
-	{
-	bns->ChangeSupport(UPDATE_DIALOG);init_disp=DispSup();init_cur=CurSup();
-	}
-	void OnCur(int cur_sup)
-	{
-	cur_dim->SetValue(array_to_string(sups[cur_sup]->NumStrats()));
-	disp_dim->SetValue(array_to_string(sups[cur_sup]->NumStrats()));
-	disp_item->SetSelection(cur_sup-1);
-	}
 
-	void OnDisp(int disp_sup)
-	{disp_dim->SetValue(array_to_string(sups[disp_sup]->NumStrats()));}
+class NFSupportInspectDialog : public wxDialogBox {
+private:
+  NfgShow *bns;
+  gList<NFSupport *> &sups;
+  int init_disp,init_cur;
+  wxText *cur_dim,*disp_dim;
+  wxChoice *disp_item,*cur_item;
+  char *cur_str,*disp_str;
+  wxStringList *support_list;
+
+// Low level event handlers
+  static void cur_func(wxChoice &ob,wxEvent &)
+    {((NFSupportInspectDialog *)ob.GetClientData())->OnCur(ob.GetSelection()+1);}
+  static void disp_func(wxChoice &ob,wxEvent &)
+    {((NFSupportInspectDialog *)ob.GetClientData())->OnDisp(ob.GetSelection()+1);}
+  static void new_sup_func(wxButton &ob,wxEvent &)
+    {((NFSupportInspectDialog *)ob.GetClientData())->OnNewSupport();}
+  static void change_sup_func(wxButton &ob,wxEvent &)
+    {((NFSupportInspectDialog *)ob.GetClientData())->OnChangeSupport();}
+  static void help_func(wxButton &,wxEvent &)
+    {wxHelpContents(NFG_SUPPORTS_HELP);}
+  static void close_func(wxButton &ob,wxEvent &)
+    {((NfgShow *)ob.GetClientData())->ChangeSupport(DESTROY_DIALOG);}
+  static void remove_sup_func(wxButton &ob,wxEvent &)
+    {((NFSupportInspectDialog *)ob.GetClientData())->OnRemoveSupport();}
+
+// High level event handlers
+  void OnNewSupport(void);
+  void OnRemoveSupport(void);
+  void OnChangeSupport(void);
+  void OnCur(int cur_sup);
+  void OnDisp(int disp_sup);
 
 // Utility funcs
-	static gText array_to_string(const gArray<int> &a)
-	{
-	gText tmp='(';
-	for (int i=1;i<=a.Length();i++) tmp+=ToText(a[i])+((i==a.Length()) ? ")" : ",");
-	return tmp;
-	}
+  static gText array_to_string(const gArray<int> &);
+
 public:
-	NFSupportInspectDialog(gList<NFSupport *> &sups_,int cur_sup,int disp_sup,NfgShow *bns_,wxWindow *parent=0)
-		: wxDialogBox(parent,"Supports"),bns(bns_),sups(sups_)
-	{
-  init_cur=cur_sup;init_disp=disp_sup;
-	wxForm *f=new wxForm(0);
-	SetLabelPosition(wxVERTICAL);
-	cur_dim=new wxText(this,0,"Current",
-											array_to_string(sups[cur_sup]->NumStrats()),
-											-1,-1,80,-1,wxREADONLY);
-	disp_dim=new wxText(this,0,"Display",
-											array_to_string(sups[disp_sup]->NumStrats()),
-											-1,-1,80,-1,wxREADONLY);
-	support_list=wxStringListInts(sups.Length());
-	cur_str=new char[10];strcpy(cur_str,ToText(cur_sup));
-	disp_str=new char[10];strcpy(disp_str,ToText(disp_sup));
-	wxFormItem *cur_fitem=wxMakeFormString("",&cur_str,wxFORM_CHOICE,
-					new wxList(wxMakeConstraintStrings(support_list),0));
-	f->Add(cur_fitem);
-	f->Add(wxMakeFormMessage("      ")); // fix the spacing... not neat but..
-	wxFormItem *disp_fitem=wxMakeFormString("",&disp_str,wxFORM_CHOICE,
-					new wxList(wxMakeConstraintStrings(support_list),0));
-	f->Add(disp_fitem);
-	f->Add(wxMakeFormNewLine());
-	wxFormItem *newsup_fitem=wxMakeFormButton("New",(wxFunction)new_sup_func);
-	f->Add(newsup_fitem);
-	wxFormItem *rmvsup_fitem=wxMakeFormButton("Remove",(wxFunction)remove_sup_func);
-	f->Add(rmvsup_fitem);
-	f->Add(wxMakeFormNewLine());
-	wxFormItem *close_fitem=wxMakeFormButton("Close",(wxFunction)close_func);
-	f->Add(close_fitem);
-	wxFormItem *cngsup_fitem=wxMakeFormButton("Apply",(wxFunction)change_sup_func);
-	f->Add(cngsup_fitem);
-	wxFormItem *help_fitem=wxMakeFormButton("?",(wxFunction)help_func);
-	f->Add(help_fitem);
-	f->AssociatePanel(this);
-	cur_item=(wxChoice *)cur_fitem->GetPanelItem();
-	cur_item->Callback((wxFunction)cur_func);
-	cur_item->SetClientData((char *)this);
-	disp_item=(wxChoice *)disp_fitem->GetPanelItem();
-	disp_item->SetClientData((char *)this);
-	disp_item->Callback((wxFunction)disp_func);
-	((wxButton *)newsup_fitem->GetPanelItem())->SetClientData((char *)this);
-	((wxButton *)rmvsup_fitem->GetPanelItem())->SetClientData((char *)this);
-	((wxButton *)cngsup_fitem->GetPanelItem())->SetClientData((char *)this);
-	((wxButton *)help_fitem->GetPanelItem())->SetClientData((char *)this);
-	((wxButton *)close_fitem->GetPanelItem())->SetClientData((char *)bns);
-	Fit();
-	Show(TRUE);
-	}
-	// Data Access members
-	int 	CurSup(void) {return cur_item->GetSelection()+1;}
-	int 	DispSup(void) {return disp_item->GetSelection()+1;}
+  NFSupportInspectDialog(gList<NFSupport *> &sups_, int cur_sup, int disp_sup,
+			 NfgShow *bns_, wxWindow *parent = 0);
+
+  Bool OnClose(void);
+
+  // Data Access members
+  int CurSup(void) { return (cur_item->GetSelection() + 1); }
+  int DispSup(void) { return (disp_item->GetSelection() + 1); }
 };
 
-// Note can not delete the first support
-void NFSupportInspectDialog::OnRemoveSupport(void)
-{
-  int i;
-  SupportRemoveDialog SRD(this,sups.Length());
-  if (SRD.Completed()==wxOK)  {
-	 gArray<bool> selected(SRD.Selected());
-	 bool revert=false;
-	 for (i=sups.Length();i>=2;i--)
-		if (selected[i])  {
-			delete sups.Remove(i);
-			if (i==init_cur || i==init_disp && revert==false)  {
-			  wxMessageBox("Display/Current support deleted.\nReverting to full support");
-			  revert=true;
-			}
-		}
-	 disp_item->Clear();cur_item->Clear();
-	 for (i=1;i<=sups.Length();i++)
-		{disp_item->Append(ToText(i));cur_item->Append(ToText(i));}
-	 disp_item->SetSize(-1,-1,-1,-1);cur_item->SetSize(-1,-1,-1,-1);
-	 disp_item->SetSelection(0);cur_item->SetSelection(0);
-	 if (revert) bns->ChangeSupport(UPDATE_DIALOG);
-  }
-}
-
-#endif
+#endif  // NFSUPTD_H
