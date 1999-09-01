713a58737eaecbdb55d2f2390389a2eae04ff0e9
rdm <rdm@3fbfd45b-e82e-0410-bb8e-8470640bbd7f>
1999-09-01 00:03:29 +0000

    Templated ComputeMixedDominated (prior to making changes)

--- sources/nfdommix.cc
+++ sources/nfdommix.cc
@@ -12,8 +12,11 @@
 #include "lpsolve.h"
 #include "gstatus.h"
 
+// Note: junk is dummy arg so the function can be templated. 
+// There is probably a cleaner way to do this.  
+template <class T>
 bool ComputeMixedDominated(const Nfg &nfg, const NFSupport &S, NFSupport &R,
-			   int pl, bool strong, gPrecision precision,
+			   int pl, bool strong,T junk,
 			   gOutput &tracefile, gStatus &status)
 {
   NfgContIter s(S);
@@ -21,353 +24,178 @@ bool ComputeMixedDominated(const Nfg &nfg, const NFSupport &S, NFSupport &R,
   double d1,d2;
   d1 = (double)(pl-1)/(double)S.Game().NumPlayers();
   d2 = (double)pl/(double)S.Game().NumPlayers();
-
+  
   gArray<bool> dom(S.NumStrats(pl));
-
-  // This is a quick and dirty implementation of multiple precisions;
-  // a template-based method is more appropriate for long-term maintainability
-  if (precision == precRATIONAL) {
-    gRational eps;
-    gEpsilon(eps);
-
-    gVector<gRational> dominator(S.NumStrats(pl));
-
-    int st,i,k,n;
-
-    if (strong)   {
-      gRational COpt;
-      bool ret = false;
-      int strats = S.NumStrats(pl);
-      int contingencies = 1;
-      for(k=1;k<=nfg.NumPlayers();k++)
-	if(k!=pl) contingencies*=S.NumStrats(k);
-      
-      gMatrix<gRational> A(1,contingencies+1,1,strats);
-      gVector<gRational> B(1,contingencies+1);
-      gVector<gRational> C(1,strats);
-
-      n = contingencies + 1;
-      for (k = 1; k < strats; k++) {
-	C[k] = (gRational) 0;
-	A(n, k) = (gRational) 1;
-      }
-      A(n, k) = (gRational) 0;
-      B[n] = (gRational) 1;
-      C[k] = (gRational) 1;
-
-      s.First();
-      for (n = 1; n <= contingencies; n++) {
-	s.Set(pl, 1);
-	B[n] = -nfg.Payoff(s.GetOutcome(), pl);
-	for (k = 2; k <= strats; k++) {
-	  s.Set(pl, k);
-	  A(n, k - 1) = -nfg.Payoff(s.GetOutcome(), pl);
-	}
-	A(n, strats) = (gRational) 1;
-	s.NextContingency();
-      }
-
-      for (k = 1; k <= strats; k++) {
-	status.Get();
-	double s1 = (double)k/(double)(strats);
-	status.SetProgress((1.0-s1)*d1 + s1*d2);
-	// tracefile << '\n' << (gRectArray<gRational> &)A << '\n';
-	// tracefile << B << '\n';
-	// tracefile << C << '\n';
-	LPSolve<gRational> Tab(A, B, C, 1);
-
-	COpt = Tab.OptimumCost();
-	tracefile << "\nPlayer = " << pl << " Strat = "<< k;
-	// tracefile << " F = " << Tab.IsFeasible();
-	// tracefile << " x = " << Tab.OptimumVector();
-	// tracefile << " Obj = " << COpt;
-
-	dom[k] = false;
-
-	if (Tab.IsFeasible() && COpt > eps) {
-	  tracefile << " Strongly Dominated by ";
-	  gVector<gRational> xx(Tab.OptimumVector());
-	  for(i=1,st=1;st<=strats;st++) {
-	    if(st==k) dominator[st] = (gRational)0;
-	    else {
-	      dominator[st] = xx[i];
-	      i++;
-	    }
-	  }
-	  tracefile << dominator;
-	  ret = true;
-	  dom[k] = true;
-	}
-	if (k<strats)
-	  A.SwitchColumn(k,B);
-      }
-      // tracefile << "\n";
-      
-      if (!ret) 
-	return false;
-      
-      for (k = 1; k <= strats; k++)
-	if (dom[k])
-	  R.RemoveStrategy(S.Strategies(pl)[k]);
-
-      return true;
+  
+  T eps;
+  gEpsilon(eps);
+  
+  gVector<T> dominator(S.NumStrats(pl));
+  
+  int st,i,k,n;
+  
+  if (strong)   {
+    T COpt;
+    bool ret = false;
+    int strats = S.NumStrats(pl);
+    int contingencies = 1;
+    for(k=1;k<=nfg.NumPlayers();k++)
+      if(k!=pl) contingencies*=S.NumStrats(k);
+    
+    gMatrix<T> A(1,contingencies+1,1,strats);
+    gVector<T> B(1,contingencies+1);
+    gVector<T> C(1,strats);
+    
+    n = contingencies + 1;
+    for (k = 1; k < strats; k++) {
+      C[k] = (T) 0;
+      A(n, k) = (T) 1;
     }
-    else  {    // look for weak domination
-      gRational C0 = (gRational) 0, COpt, TmpC;
-      bool ret = false;
-      int strats = S.NumStrats(pl);
-      int contingencies = 1;
-      for(k=1;k<=nfg.NumPlayers();k++)
-	if(k!=pl) contingencies*=S.NumStrats(k);
-      
-      gMatrix<gRational> A(1,contingencies+1,1,strats-1);
-      gVector<gRational> B(1,contingencies+1);
-      gVector<gRational> C(1,strats-1);
-      
-      n=contingencies+1;
-      for(k=1;k<strats;k++) {
-	C[k] = (gRational) 0;
-	A(n,k)=(gRational) 1;
+    A(n, k) = (T) 0;
+    B[n] = (T) 1;
+    C[k] = (T) 1;
+    
+    s.First();
+    for (n = 1; n <= contingencies; n++) {
+      s.Set(pl, 1);
+      B[n] = -nfg.Payoff(s.GetOutcome(), pl);
+      for (k = 2; k <= strats; k++) {
+	s.Set(pl, k);
+	A(n, k - 1) = -nfg.Payoff(s.GetOutcome(), pl);
       }
-      B[n]=(gRational)1;
-
-      s.First();
-      for(n=1;n<=contingencies;n++) {
-	s.Set(pl, 1);
-	B[n]=-nfg.Payoff(s.GetOutcome(), pl);
-	C0 -= B[n];
-	for(k=2;k<=strats;k++) {
-	  s.Set(pl,k);
-	  A(n,k-1)=-nfg.Payoff(s.GetOutcome(), pl);
-	  C[k-1]-=A(n,k-1);
-	}
-	s.NextContingency();
-      }
-
-      for (k = 1; k <= strats; k++) {
-	status.Get();
-	double s1 = (double)k/(double)(strats);
-	status.SetProgress((1.0-s1)*d1 + s1*d2);
-	// tracefile << '\n' << (gRectArray<gRational> &)A << '\n';
-	// tracefile << B << '\n';
-	// tracefile << C << '\n';
-	LPSolve<gRational> Tab(A, B, C, 1);
-
-	COpt = Tab.OptimumCost();
-	tracefile << "\nPlayer = " << pl << " Strat = "<< k;
-	// tracefile << " F = " << Tab.IsFeasible();
-	// tracefile << " x = " << Tab.OptimumVector();
-	// tracefile << " Obj = " << COpt;
-	
-	dom[k] = false;
-
-	if (Tab.IsFeasible() && (COpt >= C0-eps && COpt <=C0+eps))
-	  tracefile << " Duplicated strategy? ";
-	else if (Tab.IsFeasible() && COpt > C0+eps) {
-	  tracefile << " Weakly Dominated by ";
-	  gVector<gRational> xx(Tab.OptimumVector());
-	  for(i=1,st=1;st<=strats;st++) {
-	    if(st==k) dominator[st] = (gRational)0;
-	    else {
-	      dominator[st] = xx[i];
-	      i++;
-	    }
-	  }
-	  tracefile << dominator;
-	  ret = true;
-	  dom[k] = true;
-	}
-	// else  tracefile << "\n\n";
-	if(k<strats) {
-	  A.SwitchColumn(k,B);
-	  TmpC=C0; C0=C[k]; C[k]=TmpC;
-	}
-	
-      }
-      // tracefile << "\n";
-
-      if (!ret) 
-	return false;
-
-      for (k = 1; k <= strats; k++)
-	if (dom[k])
-	  R.RemoveStrategy(S.Strategies(pl)[k]);
-      
-      return true;
+      A(n, strats) = (T) 1;
+      s.NextContingency();
     }
+    
+    for (k = 1; k <= strats; k++) {
+      status.Get();
+      double s1 = (double)k/(double)(strats);
+      status.SetProgress((1.0-s1)*d1 + s1*d2);
+      // tracefile << '\n' << (gRectArray<T> &)A << '\n';
+      // tracefile << B << '\n';
+      // tracefile << C << '\n';
+      LPSolve<T> Tab(A, B, C, 1);
+      
+      COpt = Tab.OptimumCost();
+      tracefile << "\nPlayer = " << pl << " Strat = "<< k;
+      // tracefile << " F = " << Tab.IsFeasible();
+      // tracefile << " x = " << Tab.OptimumVector();
+      // tracefile << " Obj = " << COpt;
+      
+      dom[k] = false;
+      
+      if (Tab.IsFeasible() && COpt > eps) {
+	tracefile << " Strongly Dominated by ";
+	gVector<T> xx(Tab.OptimumVector());
+	for(i=1,st=1;st<=strats;st++) {
+	  if(st==k) dominator[st] = (T)0;
+	  else {
+	    dominator[st] = xx[i];
+	    i++;
+	  }
+	}
+	tracefile << dominator;
+	ret = true;
+	dom[k] = true;
+      }
+      if (k<strats)
+	A.SwitchColumn(k,B);
+    }
+    // tracefile << "\n";
+    
+    if (!ret) 
+      return false;
+    
+    for (k = 1; k <= strats; k++)
+      if (dom[k])
+	R.RemoveStrategy(S.Strategies(pl)[k]);
+    
+    return true;
   }
-  else {  // precDOUBLE
-    double eps;
-    gEpsilon(eps);
-    gVector<double> dominator(S.NumStrats(pl));
-
-    int st,i,k,n;
-
-    if (strong)   {
-      double COpt;
-      bool ret = false;
-      int strats = S.NumStrats(pl);
-      int contingencies = 1;
-      for(k=1;k<=nfg.NumPlayers();k++)
-	if(k!=pl) contingencies*=S.NumStrats(k);
-      
-      gMatrix<double> A(1,contingencies+1,1,strats);
-      gVector<double> B(1,contingencies+1);
-      gVector<double> C(1,strats);
-
-      n = contingencies + 1;
-      for (k = 1; k < strats; k++) {
-	C[k] = (double) 0;
-	A(n, k) = (double) 1;
-      }
-      A(n, k) = (double) 0;
-      B[n] = (double) 1;
-      C[k] = (double) 1;
-
-      s.First();
-      for (n = 1; n <= contingencies; n++) {
-	s.Set(pl, 1);
-	B[n] = -nfg.Payoff(s.GetOutcome(), pl);
-	for (k = 2; k <= strats; k++) {
-	  s.Set(pl, k);
-	  A(n, k - 1) = -nfg.Payoff(s.GetOutcome(), pl);
-	}
-	A(n, strats) = (double) 1;
-	s.NextContingency();
-      }
-
-      for (k = 1; k <= strats; k++) {
-	status.Get();
-	double s1 = (double)k/(double)(strats);
-	status.SetProgress((1.0-s1)*d1 + s1*d2);
-	// tracefile << '\n' << (gRectArray<double> &)A << '\n';
-	// tracefile << B << '\n';
-	// tracefile << C << '\n';
-	LPSolve<double> Tab(A, B, C, 1);
-
-	COpt = Tab.OptimumCost();
-	tracefile << "\nPlayer = " << pl << " Strat = "<< k;
-	// tracefile << " F = " << Tab.IsFeasible();
-	// tracefile << " x = " << Tab.OptimumVector();
-	// tracefile << " Obj = " << COpt;
-
-	dom[k] = false;
-
-	if (Tab.IsFeasible() && COpt > eps) {
-	  tracefile << " Strongly Dominated by ";
-	  gVector<double> xx(Tab.OptimumVector());
-	  for(i=1,st=1;st<=strats;st++) {
-	    if(st==k) dominator[st] = (double)0;
-	    else {
-	      dominator[st] = xx[i];
-	      i++;
-	    }
-	  }
-	  tracefile << dominator;
-	  ret = true;
-	  dom[k] = true;
-	}
-	if (k<strats)
-	  A.SwitchColumn(k,B);
-      }
-      // tracefile << "\n";
-      
-      if (!ret) 
-	return false;
-      
-      for (k = 1; k <= strats; k++)
-	if (dom[k])
-	  R.RemoveStrategy(S.Strategies(pl)[k]);
-
-      return true;
+  else  {    // look for weak domination
+    T C0 = (T) 0, COpt, TmpC;
+    bool ret = false;
+    int strats = S.NumStrats(pl);
+    int contingencies = 1;
+    for(k=1;k<=nfg.NumPlayers();k++)
+      if(k!=pl) contingencies*=S.NumStrats(k);
+    
+    gMatrix<T> A(1,contingencies+1,1,strats-1);
+    gVector<T> B(1,contingencies+1);
+    gVector<T> C(1,strats-1);
+    
+    n=contingencies+1;
+    for(k=1;k<strats;k++) {
+      C[k] = (T) 0;
+      A(n,k)=(T) 1;
     }
-    else  {    // look for weak domination
-      double C0 = (double) 0, COpt, TmpC;
-      bool ret = false;
-      int strats = S.NumStrats(pl);
-      int contingencies = 1;
-      for(k=1;k<=nfg.NumPlayers();k++)
-	if(k!=pl) contingencies*=S.NumStrats(k);
-      
-      gMatrix<double> A(1,contingencies+1,1,strats-1);
-      gVector<double> B(1,contingencies+1);
-      gVector<double> C(1,strats-1);
-      
-      n=contingencies+1;
-      for(k=1;k<strats;k++) {
-	C[k] = (double) 0;
-	A(n,k)=(double) 1;
+    B[n]=(T)1;
+    
+    s.First();
+    for(n=1;n<=contingencies;n++) {
+      s.Set(pl, 1);
+      B[n]=-nfg.Payoff(s.GetOutcome(), pl);
+      C0 -= B[n];
+      for(k=2;k<=strats;k++) {
+	s.Set(pl,k);
+	A(n,k-1)=-nfg.Payoff(s.GetOutcome(), pl);
+	C[k-1]-=A(n,k-1);
       }
-      B[n]=(double)1;
-
-      s.First();
-      for(n=1;n<=contingencies;n++) {
-	s.Set(pl, 1);
-	B[n]=-nfg.Payoff(s.GetOutcome(), pl);
-	C0 -= B[n];
-	for(k=2;k<=strats;k++) {
-	  s.Set(pl,k);
-	  A(n,k-1)=-nfg.Payoff(s.GetOutcome(), pl);
-	  C[k-1]-=A(n,k-1);
-	}
-	s.NextContingency();
-      }
-
-      for (k = 1; k <= strats; k++) {
-	status.Get();
-	double s1 = (double)k/(double)(strats);
-	status.SetProgress((1.0-s1)*d1 + s1*d2);
-	// tracefile << '\n' << (gRectArray<double> &)A << '\n';
-	// tracefile << B << '\n';
-	// tracefile << C << '\n';
-	LPSolve<double> Tab(A, B, C, 1);
-
-	COpt = Tab.OptimumCost();
-	tracefile << "\nPlayer = " << pl << " Strat = "<< k;
-	// tracefile << " F = " << Tab.IsFeasible();
-	// tracefile << " x = " << Tab.OptimumVector();
-	// tracefile << " Obj = " << COpt;
-	
-	dom[k] = false;
-
-	if (Tab.IsFeasible() && (COpt >= C0-eps && COpt <=C0+eps))
-	  tracefile << " Duplicated strategy? ";
-	else if (Tab.IsFeasible() && COpt > C0+eps) {
-	  tracefile << " Weakly Dominated by ";
-	  gVector<double> xx(Tab.OptimumVector());
-	  for(i=1,st=1;st<=strats;st++) {
-	    if(st==k) dominator[st] = (double)0;
-	    else {
-	      dominator[st] = xx[i];
-	      i++;
-	    }
-	  }
-	  tracefile << dominator;
-	  ret = true;
-	  dom[k] = true;
-	}
-	// else  tracefile << "\n\n";
-	if(k<strats) {
-	  A.SwitchColumn(k,B);
-	  TmpC=C0; C0=C[k]; C[k]=TmpC;
-	}
-	
-      }
-      // tracefile << "\n";
-
-      if (!ret) 
-	return false;
-
-      for (k = 1; k <= strats; k++)
-	if (dom[k])
-	  R.RemoveStrategy(S.Strategies(pl)[k]);
-      
-      return true;
+      s.NextContingency();
     }
-
-
-
+    
+    for (k = 1; k <= strats; k++) {
+      status.Get();
+      double s1 = (double)k/(double)(strats);
+      status.SetProgress((1.0-s1)*d1 + s1*d2);
+      // tracefile << '\n' << (gRectArray<T> &)A << '\n';
+      // tracefile << B << '\n';
+      // tracefile << C << '\n';
+      LPSolve<T> Tab(A, B, C, 1);
+      
+      COpt = Tab.OptimumCost();
+      tracefile << "\nPlayer = " << pl << " Strat = "<< k;
+      // tracefile << " F = " << Tab.IsFeasible();
+      // tracefile << " x = " << Tab.OptimumVector();
+      // tracefile << " Obj = " << COpt;
+      
+      dom[k] = false;
+      
+      if (Tab.IsFeasible() && (COpt >= C0-eps && COpt <=C0+eps))
+	tracefile << " Duplicated strategy? ";
+      else if (Tab.IsFeasible() && COpt > C0+eps) {
+	tracefile << " Weakly Dominated by ";
+	gVector<T> xx(Tab.OptimumVector());
+	for(i=1,st=1;st<=strats;st++) {
+	  if(st==k) dominator[st] = (T)0;
+	  else {
+	    dominator[st] = xx[i];
+	    i++;
+	  }
+	}
+	tracefile << dominator;
+	ret = true;
+	dom[k] = true;
+      }
+      // else  tracefile << "\n\n";
+      if(k<strats) {
+	A.SwitchColumn(k,B);
+	TmpC=C0; C0=C[k]; C[k]=TmpC;
+      }
+      
+    }
+    // tracefile << "\n";
+    
+    if (!ret) 
+      return false;
+    
+    for (k = 1; k <= strats; k++)
+      if (dom[k])
+	R.RemoveStrategy(S.Strategies(pl)[k]);
+    
+    return true;
   }
+  
 }
 
 NFSupport *ComputeMixedDominated(NFSupport &S,
@@ -379,13 +207,27 @@ NFSupport *ComputeMixedDominated(NFSupport &S,
   NFSupport *newS = new NFSupport(S);
   bool any = false;
   
-  for (int i = 1; i <= players.Length(); i++)   {
-    status.Get();
-    int pl = players[i];
-
-    any |= ComputeMixedDominated(N, S, *newS, pl, strong, precision,
-				 tracefile, status);
+  if(precision == precRATIONAL) {
+    
+    for (int i = 1; i <= players.Length(); i++)   {
+      status.Get();
+      int pl = players[i];
+      
+      any |= ComputeMixedDominated<gRational>(N, S, *newS, pl, strong,
+				  (gRational)0, tracefile, status);
+    }
   }
+  else if (precision == precDOUBLE) {
+    for (int i = 1; i <= players.Length(); i++)   {
+      status.Get();
+      int pl = players[i];
+      
+      any |= ComputeMixedDominated<double>(N, S, *newS, pl, strong,
+				  (double)0, tracefile, status);
+    }
+  }
+
+
   tracefile << "\n";
   if (!any)  {
     delete newS;
@@ -395,7 +237,14 @@ NFSupport *ComputeMixedDominated(NFSupport &S,
   return newS;
 }
 
-
+template bool 
+ComputeMixedDominated(const Nfg &nfg, const NFSupport &S, 
+		      NFSupport &R,int pl, bool strong, gRational junk,
+		      gOutput &tracefile, gStatus &status);
+template bool 
+ComputeMixedDominated(const Nfg &nfg, const NFSupport &S, 
+		      NFSupport &R,int pl, bool strong, double junk,
+		      gOutput &tracefile, gStatus &status);
 
 
 
