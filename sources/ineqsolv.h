cf8883704bee06cdbaf80835e2e02d7af56bdeed
mclennan <mclennan@3fbfd45b-e82e-0410-bb8e-8470640bbd7f>
1999-09-15 18:57:42 +0000

    First version of module for determining whether a system of inequalities can be simultaneously satisfied at a point in a rectangle.

---
+++ sources/ineqsolv.h
@@ -0,0 +1,90 @@
+//
+// File: ineqsolv.h  --  Declaration of gpolylst data type
+// 
+// $Id$
+//
+
+#ifndef INEQSOLV_H
+#define INEQSOLV_H
+
+#include "gstatus.h"
+
+#include "gsolver.h"
+#include "odometer.h"
+#include "rectangl.h"
+#include "gtree.h"
+#include "gpoly.h"
+#include "gpolylst.h"
+#include "gpartltr.h"
+
+/*
+    The class described in this file is a method of determining whether a
+system of weak inequalities has a solution (a point where all are satisfied)
+in a given rectangle.  Ir is modeled on QuikSolv, but simpler.  There is
+no Newton search, only repeated subdivision, queries at the center, and
+tests against whether one of the inequalities is provably everywhere 
+negative in the rectangle.
+*/
+
+/*
+   The main constructor for this takes a gPolyList<T>, interpreted as 
+inequalities in the sense that, at a solution, all the polynomials
+are required to be nonnegative.
+*/
+
+
+// ***********************
+//      class IneqSolv
+// ***********************
+
+template <class T> class IneqSolv {
+ private:
+  const gPolyList<T>                 System;
+  const ListOfPartialTrees<T>        TreesOfPartials;
+        T                            Epsilon;
+        bool                         HasBeenSolved;
+        gTriState                    HasASolution;
+        gVector<T>                   Sample;
+  gStatus &m_status;
+
+  // Routines Doing the Actual Work
+
+  bool IsASolution(const gVector<T>&)                              const;
+
+  bool SystemHasNoSolutionIn(const gRectangle<T>& r, gArray<int>&) const;
+
+  const bool ASolutionExistsRecursion(const gRectangle<T>&, 
+				      gVector<T>&,
+				      gArray<int>&)                const;
+
+ public:
+   IneqSolv(const gPolyList<T> &, gStatus &);  
+   IneqSolv(const IneqSolv<T> &);
+   ~IneqSolv();
+
+   // Operators
+   IneqSolv<T>& operator= (const IneqSolv<T> &);
+   bool         operator==(const IneqSolv<T> &) const;
+   bool         operator!=(const IneqSolv<T> &) const;
+
+   // Information
+   inline const gSpace*                  AmbientSpace()              const 
+     { return System.AmbientSpace(); }
+   inline const term_order*              TermOrder()                 const 
+     { return System.TermOrder(); }
+   inline const int                      Dmnsn()                     const 
+     { return System.Dmnsn(); }
+   inline const gPolyList<T>             UnderlyingEquations()       const 
+     { return System; }
+   inline const bool                     WasSolved()                 const
+     { return HasBeenSolved; }
+   inline const gVector<T>               SampleSolution()            const
+     { return Sample; }
+
+  // The function that does everything
+  const bool ASolutionExists(const gRectangle<T>&, gVector<T>& sample);
+
+friend gOutput& operator << (gOutput& output, const IneqSolv<T>& x);
+};  
+
+#endif INEQSOLV_H
