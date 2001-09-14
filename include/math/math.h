7b2f765325a14c27ac8e829a323b22968df816f7
magyar <magyar@3fbfd45b-e82e-0410-bb8e-8470640bbd7f>
2001-09-14 17:07:45 +0000

    (1) Made sure to #include math.h for abs(double) function for inverse of square matrix (2) Instantiation of operator<< for gMatrix<long double>

--- include/math/math.h
+++ include/math/math.h
@@ -16,6 +16,7 @@ template <class T> T gmax(const T &a, const T &b);
 template <class T> T gmax(const gArray<T> &);
 
 double abs(double a);
+inline double abs(long double a) { return abs((double) a); }
 gNumber abs(const gNumber &);
 
 int sign(const double &a);
