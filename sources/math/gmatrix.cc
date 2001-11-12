bd3a209e376f0a35df1b7338508a7d4cf0007ecf
magyar <magyar@3fbfd45b-e82e-0410-bb8e-8470640bbd7f>
2001-11-12 15:56:18 +0000

    Added instantiation for operator<< for gMatrix<gDouble>

--- sources/math/gmatrix.cc
+++ sources/math/gmatrix.cc
@@ -26,4 +26,5 @@ template gOutput &operator<<(gOutput &, const gMatrix<double> &);
 template gOutput &operator<<(gOutput &, const gMatrix<long double> &);
 template gOutput &operator<<(gOutput &, const gMatrix<gRational> &);
 template gOutput &operator<<(gOutput &, const gMatrix<gInteger> &);
+template gOutput &operator<<(gOutput &, const gMatrix<gDouble> &);
 template gOutput &operator<<(gOutput &, const gMatrix<int> &);
