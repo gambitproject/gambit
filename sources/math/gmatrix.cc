21ed3d046fac5c2a0e925ca2b0388ce3dc2c2ccb
magyar <magyar@3fbfd45b-e82e-0410-bb8e-8470640bbd7f>
2001-11-15 14:25:01 +0000

    Added instantiation of gMatrix<gDouble>

--- sources/math/gmatrix.cc
+++ sources/math/gmatrix.cc
@@ -6,12 +6,14 @@
 
 #include "math/gmatrix.imp"
 #include "math/rational.h"
+#include "math/gnumber.h"
 
 template class gMatrix<double>;
 template class gMatrix<long double>;
 template class gMatrix<gRational>;
 template class gMatrix<gInteger>;
 template class gMatrix<int>;
+template class gMatrix<gNumber>;
 
 template gVector<double> operator*(const gVector<double> &,
 				   const gMatrix<double> &);
@@ -21,6 +23,8 @@ template gVector<gInteger> operator*(const gVector<gInteger> &,
 				     const gMatrix<gInteger> &);
 template gVector<int> operator*(const gVector<int> &,
 				const gMatrix<int> &);
+template gVector<gNumber> operator*(const gVector<gNumber> &,
+				    const gMatrix<gNumber> &);
 
 template gOutput &operator<<(gOutput &, const gMatrix<double> &);
 template gOutput &operator<<(gOutput &, const gMatrix<long double> &);
@@ -28,3 +32,4 @@ template gOutput &operator<<(gOutput &, const gMatrix<gRational> &);
 template gOutput &operator<<(gOutput &, const gMatrix<gInteger> &);
 template gOutput &operator<<(gOutput &, const gMatrix<gDouble> &);
 template gOutput &operator<<(gOutput &, const gMatrix<int> &);
+template gOutput &operator<<(gOutput &, const gMatrix<gNumber> &);
