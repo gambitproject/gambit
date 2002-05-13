17709d4b33e8c31dc1b6eb11398d9e340da3861d
magyar <magyar@3fbfd45b-e82e-0410-bb8e-8470640bbd7f>
2002-05-13 22:23:09 +0000

    Added dialogEditMove

---
+++ sources/gui/dleditmove.cc
@@ -0,0 +1,245 @@
+//
+// $Source$
+// $Date$
+// $Revision$
+//
+// DESCRIPTION:
+// Dialog for viewing and editing properties of a move
+//
+
+#include "wx/wxprec.h"
+#ifndef WX_PRECOMP
+#include "wx/wx.h"
+#endif  // WX_PRECOMP
+#include "guishare/valnumber.h"
+#include "game/efg.h"
+#include "dleditmove.h"
+
+//======================================================================
+//                      class dialogEditMove
+//======================================================================
+
+const int idLISTBOX_ACTIONS = 1000;
+const int idBUTTON_ACTION_ADDBEFORE = 1001;
+const int idBUTTON_ACTION_ADDAFTER = 1002;
+const int idBUTTON_ACTION_DELETE = 1003;
+
+BEGIN_EVENT_TABLE(dialogEditMove, wxDialog)
+  EVT_LISTBOX(idLISTBOX_ACTIONS, dialogEditMove::OnActionChanged)
+  EVT_BUTTON(idBUTTON_ACTION_ADDBEFORE, dialogEditMove::OnAddActionBefore)
+  EVT_BUTTON(idBUTTON_ACTION_ADDAFTER, dialogEditMove::OnAddActionAfter)
+  EVT_BUTTON(idBUTTON_ACTION_DELETE, dialogEditMove::OnDeleteAction)
+  EVT_BUTTON(wxID_OK, dialogEditMove::OnOK)
+END_EVENT_TABLE()
+
+dialogEditMove::dialogEditMove(wxWindow *p_parent, Infoset *p_infoset)
+  : wxDialog(p_parent, -1, "Move properties"), m_infoset(p_infoset)
+{
+  SetAutoLayout(true);
+
+  wxBoxSizer *topSizer = new wxBoxSizer(wxVERTICAL);
+
+  wxBoxSizer *labelSizer = new wxBoxSizer(wxHORIZONTAL);
+  labelSizer->Add(new wxStaticText(this, wxID_STATIC, "Information set label"),
+		  0, wxALL | wxCENTER, 5);
+  m_infosetName = new wxTextCtrl(this, -1,
+				  (const char *) p_infoset->GetName());
+  labelSizer->Add(m_infosetName, 1, wxALL | wxCENTER | wxEXPAND, 5);
+  topSizer->Add(labelSizer, 0, wxALL | wxEXPAND, 0);
+
+  topSizer->Add(new wxStaticText(this, wxID_STATIC,
+				 wxString::Format("Number of members: %d",
+						  p_infoset->NumMembers())),
+		0, wxALL, 5);
+  topSizer->Add(new wxStaticText(this, wxID_STATIC,
+				 wxString::Format("Belongs to player %d: "
+						  "'%s'",
+				 p_infoset->GetPlayer()->GetNumber(),
+				 (char *) p_infoset->GetPlayer()->GetName())),
+		0, wxALL, 5);
+  
+
+  wxStaticBoxSizer *actionBoxSizer =
+    new wxStaticBoxSizer(new wxStaticBox(this, -1, "Actions"), wxHORIZONTAL);
+ 
+  m_actionList = new wxListBox(this, idLISTBOX_ACTIONS);
+  for (int act = 1; act <= p_infoset->NumActions(); act++) {
+    m_actionList->Append((const char *)
+			 (ToText(act) + ": " + 
+			  p_infoset->GetAction(act)->GetName()));
+    m_actionNames.Append(p_infoset->GetAction(act)->GetName());
+    if (p_infoset->IsChanceInfoset()) {
+      m_actionProbs.Append(p_infoset->Game()->GetChanceProbs(p_infoset)[act]);
+    }
+    m_actions.Append(p_infoset->GetAction(act));
+  }
+  m_actionList->SetSelection(0);
+  m_lastSelection = 0;
+  actionBoxSizer->Add(m_actionList, 1, wxALL | wxEXPAND, 5);
+
+  wxBoxSizer *rhsSizer = new wxBoxSizer(wxVERTICAL);
+
+  wxBoxSizer *editSizer = new wxBoxSizer(wxHORIZONTAL);
+
+  wxBoxSizer *editNameSizer = new wxBoxSizer(wxVERTICAL);
+  editNameSizer->Add(new wxStaticText(this, wxID_STATIC, "Action name"),
+		     0, wxTOP | wxCENTER, 5);
+  m_actionName = new wxTextCtrl(this, -1, "");
+  editNameSizer->Add(m_actionName, 0, wxALL | wxEXPAND, 5);
+  m_actionName->SetValue((const char *) m_actionNames[1]);
+  editSizer->Add(editNameSizer, 0, wxALL, 5);
+
+  if (p_infoset->IsChanceInfoset()) {
+    wxBoxSizer *editProbSizer = new wxBoxSizer(wxVERTICAL);
+    editProbSizer->Add(new wxStaticText(this, wxID_STATIC, "Probability"),
+		       0, wxTOP | wxCENTER, 5);
+    m_actionProbValue = ToText(p_infoset->Game()->GetChanceProbs(p_infoset)[1]);
+    m_actionProb = new wxTextCtrl(this, -1, "",
+				  wxDefaultPosition, wxDefaultSize,
+				  0, gNumberValidator(&m_actionProbValue,
+						      0, 1),
+				  "action probability");
+    editProbSizer->Add(m_actionProb, 0, wxALL | wxEXPAND, 5);
+    editSizer->Add(editProbSizer, 0, wxALL, 5);
+  }
+  rhsSizer->Add(editSizer, 0, wxALL, 5);
+
+  wxBoxSizer *addRemoveSizer = new wxBoxSizer(wxVERTICAL);
+  m_addBeforeButton = new wxButton(this, idBUTTON_ACTION_ADDBEFORE, 
+				   "Add action before");
+  addRemoveSizer->Add(m_addBeforeButton, 0, wxALL | wxEXPAND, 5);
+  m_addAfterButton = new wxButton(this, idBUTTON_ACTION_ADDAFTER,
+				  "Add action after");
+  addRemoveSizer->Add(m_addAfterButton, 0, wxALL | wxEXPAND, 5);
+  m_deleteButton = new wxButton(this, idBUTTON_ACTION_DELETE,
+				"Delete action");
+  addRemoveSizer->Add(m_deleteButton, 0, wxALL | wxEXPAND, 5);
+  m_deleteButton->Enable(m_actionList->Number() > 1);
+  rhsSizer->Add(addRemoveSizer, 0, wxALL | wxCENTER, 5);
+
+  actionBoxSizer->Add(rhsSizer, 0, wxALL, 5);
+  topSizer->Add(actionBoxSizer, 0, wxALL | wxEXPAND, 5);
+
+  wxBoxSizer *buttonSizer = new wxBoxSizer(wxHORIZONTAL);
+  wxButton *okButton = new wxButton(this, wxID_OK, "OK");
+  okButton->SetDefault();
+  buttonSizer->Add(okButton, 0, wxALL, 5);
+  buttonSizer->Add(new wxButton(this, wxID_CANCEL, "Cancel"), 0, wxALL, 5);
+  buttonSizer->Add(new wxButton(this, wxID_HELP, "Help"), 0, wxALL, 5);
+  topSizer->Add(buttonSizer, 0, wxALL | wxCENTER, 5);
+
+  SetSizer(topSizer);
+  topSizer->Fit(this);
+  topSizer->SetSizeHints(this);
+  Layout();
+}
+
+void dialogEditMove::OnActionChanged(wxCommandEvent &)
+{ 
+  m_actionNames[m_lastSelection+1] = m_actionName->GetValue().c_str();
+  if (m_infoset->IsChanceInfoset()) {
+    m_actionProbs[m_lastSelection+1] = ToNumber(m_actionProb->GetValue().c_str());
+  }
+  Action *action = m_actions[m_lastSelection+1];
+  if (action) {
+    m_actionList->SetString(m_lastSelection,
+			    (const char *) 
+			    ((ToText(action->GetNumber()) + ": " +
+			      m_actionName->GetValue().c_str())));
+  }
+  else {
+    m_actionList->SetString(m_lastSelection, m_actionName->GetValue());
+  }
+  m_actionName->SetValue((const char *)
+			 m_actionNames[m_actionList->GetSelection() + 1]);
+  if (m_infoset->IsChanceInfoset()) {
+    m_actionProb->SetValue((const char *)
+			   ToText(m_actionProbs[m_actionList->GetSelection()+1]));
+  }
+  m_lastSelection = m_actionList->GetSelection();
+}
+
+void dialogEditMove::OnAddActionBefore(wxCommandEvent &)
+{
+  m_actionNames[m_lastSelection+1] = m_actionName->GetValue().c_str();
+  if (m_infoset->IsChanceInfoset()) {
+    m_actionProbs[m_lastSelection+1] = 
+      ToNumber(m_actionProb->GetValue().c_str());
+  }
+  m_actionList->SetString(m_lastSelection,
+			  (const char *) ((ToText(m_lastSelection+1) + ": " +
+					   m_actionName->GetValue().c_str())));
+  
+  m_actionNames.Insert("NewAction", m_actionList->GetSelection() + 1);
+  if (m_infoset->IsChanceInfoset()) {
+    m_actionProbs.Insert(0, m_actionList->GetSelection() + 1);
+  }
+  m_actions.Insert(0, m_actionList->GetSelection() + 1);
+  wxArrayString labels;
+  labels.Add("NewAction");
+  m_actionList->InsertItems(labels, m_actionList->GetSelection());
+  m_actionList->SetSelection(m_lastSelection);
+  m_actionName->SetValue((const char *)
+			 m_actionNames[m_actionList->GetSelection() + 1]);
+  m_deleteButton->Enable(true);
+}
+
+void dialogEditMove::OnAddActionAfter(wxCommandEvent &)
+{
+  m_actionNames[m_lastSelection+1] = m_actionName->GetValue().c_str();
+  if (m_infoset->IsChanceInfoset()) {
+    m_actionProbs[m_lastSelection+1] = 
+      ToNumber(m_actionProb->GetValue().c_str());
+  }
+  m_actionList->SetString(m_lastSelection,
+			  (const char *) ((ToText(m_lastSelection+1) + ": " +
+					   m_actionName->GetValue().c_str())));
+  
+  m_actionNames.Insert("NewAction", m_actionList->GetSelection() + 2);
+  if (m_infoset->IsChanceInfoset()) {
+    m_actionProbs.Insert(0, m_actionList->GetSelection() + 2);
+  }
+  m_actions.Insert(0, m_actionList->GetSelection() + 2);
+  wxArrayString labels;
+  labels.Add("NewAction");
+  m_actionList->InsertItems(labels, m_actionList->GetSelection() + 1);
+  m_lastSelection++;
+  m_actionList->SetSelection(m_lastSelection);
+  m_actionName->SetValue((const char *)
+			 m_actionNames[m_actionList->GetSelection() + 1]);
+  m_deleteButton->Enable(true);
+}
+
+void dialogEditMove::OnDeleteAction(wxCommandEvent &)
+{
+  m_actionNames.Remove(m_actionList->GetSelection() + 1);
+  if (m_infoset->IsChanceInfoset()) {
+    m_actionProbs.Remove(m_actionList->GetSelection() + 1);
+  }
+  m_actions.Remove(m_actionList->GetSelection() + 1);
+  m_actionList->Delete(m_actionList->GetSelection());
+  if (m_lastSelection >= m_actionList->Number()) {
+    m_lastSelection = m_actionList->Number() - 1;
+  }
+  m_actionList->SetSelection(m_lastSelection);
+  m_actionName->SetValue((const char *)
+			 m_actionNames[m_actionList->GetSelection() + 1]);
+  m_deleteButton->Enable(m_actionList->Number() > 1);
+}
+
+void dialogEditMove::OnOK(wxCommandEvent &p_event)
+{
+  // Copy any edited data into the blocks
+  m_actionNames[m_lastSelection+1] = m_actionName->GetValue().c_str();
+  if (m_infoset->IsChanceInfoset()) {
+    m_actionProbs[m_lastSelection+1] = 
+      ToNumber(m_actionProb->GetValue().c_str());
+  }
+  // Go on with usual processing
+  p_event.Skip();
+}
+
+
+
+
+
---
+++ sources/gui/dleditmove.h
@@ -0,0 +1,46 @@
+//
+// $Source$
+// $Date$
+// $Revision$
+//
+// DESCRIPTION:
+// Dialog for viewing and editing properties of a move
+//
+
+#ifndef DLEDITMOVE_H
+#define DLEDITMOVE_H
+
+class dialogEditMove : public wxDialog {
+private:
+  Infoset *m_infoset;
+  wxTextCtrl *m_infosetName, *m_actionName, *m_actionProb;
+  wxString m_actionProbValue;
+  wxListBox *m_actionList;
+  wxButton *m_addBeforeButton, *m_addAfterButton, *m_deleteButton;
+  gBlock<gText> m_actionNames;
+  gBlock<gNumber> m_actionProbs;
+  gBlock<Action *> m_actions;
+  int m_lastSelection;
+
+  // Event handlers
+  void OnActionChanged(wxCommandEvent &);
+  void OnAddActionBefore(wxCommandEvent &);
+  void OnAddActionAfter(wxCommandEvent &);
+  void OnDeleteAction(wxCommandEvent &);
+  void OnOK(wxCommandEvent &);
+
+public:
+  // Lifecycle
+  dialogEditMove(wxWindow *p_parent, Infoset *p_infoset);
+
+  // Data access (only valid when ShowModal() returns with wxID_OK)
+  wxString GetInfosetName(void) const { return m_infosetName->GetValue(); }
+  int NumActions(void) const { return m_actions.Length(); }
+  const gBlock<Action *> &GetActions(void) const { return m_actions; }
+  gText GetActionName(int p_act) const { return m_actionNames[p_act]; }
+  gNumber GetActionProb(int p_act) const { return m_actionProbs[p_act]; }
+
+  DECLARE_EVENT_TABLE()
+};
+
+#endif   // DLEDITMOVE_H
