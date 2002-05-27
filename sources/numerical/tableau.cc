b34349003f38cc1b6569193cdafa54951b48b0d5
magyar <magyar@3fbfd45b-e82e-0410-bb8e-8470640bbd7f>
2002-05-27 17:34:55 +0000

    File reorganization and conversion to autotools

---
+++ sources/numerical/tableau.cc
@@ -0,0 +1,14 @@
+//
+// FILE: tableau.cc -- tableau and basis class instantiations
+//
+// $Id$
+//
+
+#include "math/rational.h"
+#include "tableau.imp"
+#include "math/gmatrix.imp"
+#include "base/grarray.imp"
+
+
+
+
