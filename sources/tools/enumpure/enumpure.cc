c5909095e743e658974b6651d1a897a92370203d
drarbiter <drarbiter@3fbfd45b-e82e-0410-bb8e-8470640bbd7f>
2006-01-05 21:56:42 +0000

    Combining enumpure programs into one binary

--- sources/tools/enumpure/enumpure.cc
+++ sources/tools/enumpure/enumpure.cc
@@ -4,7 +4,7 @@
 // $Revision$
 //
 // DESCRIPTION:
-// Algorithm to compute pure strategy equilibria on normal forms
+// Compute pure-strategy equilibria in extensive form games
 //
 // This file is part of Gambit
 // Copyright (c) 2002, The Gambit Project
@@ -30,6 +30,76 @@
 
 template <class T>
 void PrintProfile(std::ostream &p_stream,
+		  const Gambit::MixedBehavProfile<T> &p_profile)
+{
+  for (int i = 1; i <= p_profile.Length(); i++) {
+    p_stream << p_profile[i];
+    if (i < p_profile.Length()) {
+      p_stream << ',';
+    }
+  }
+
+  p_stream << std::endl;
+}
+
+void SolveBehav(Gambit::Game p_efg)
+{
+  Gambit::BehavIterator citer(p_efg);
+  gbtPVector<gbtRational> probs(p_efg->NumInfosets());
+
+  int contNumber = 1;
+  try {
+    do {
+      bool flag = true;
+      // Commenting this out means we don't take advantage of reachability
+      // information; fix this in future.
+      //citer.GetProfile().InfosetProbs(probs);
+
+      for (int pl = 1; flag && pl <= p_efg->NumPlayers(); pl++)  {
+	gbtRational current = citer.GetPayoff(pl);
+	Gambit::PureBehavProfile p = citer.GetProfile();
+	
+	Gambit::GamePlayer player = p_efg->GetPlayer(pl);
+
+	for (int iset = 1; flag && iset <= player->NumInfosets(); iset++)  {
+	  Gambit::GameInfoset infoset = player->GetInfoset(iset);
+
+	  for (int act = 1; act <= infoset->NumActions(); act++)  {
+	    p.SetAction(infoset->GetAction(act));
+	    if (p.GetPayoff(pl) > current)  {
+	      flag = false;
+	      break;
+	    }
+	  }
+	}
+      }
+      
+      if (flag)  {
+	Gambit::MixedBehavProfile<gbtRational> temp(p_efg);
+	// zero out all the entries, since any equilibria are pure
+	((gbtVector<gbtRational> &) temp).operator=(gbtRational(0));
+	const Gambit::PureBehavProfile &profile = citer.GetProfile();
+	for (int pl = 1; pl <= p_efg->NumPlayers(); pl++)  {
+	  for (int iset = 1;
+	       iset <= p_efg->GetPlayer(pl)->NumInfosets();
+	       iset++) {
+	    int act = profile.GetAction(p_efg->GetPlayer(pl)->GetInfoset(iset))->GetNumber();
+	    temp(pl, iset, act) = 1;
+	  }
+	}
+
+	PrintProfile(std::cout, temp);
+      }
+      contNumber++;
+    }  while (citer.NextContingency());
+  }
+  catch (...) {
+    // catch exception; return solutions computed (if any)
+  }
+}
+
+template <class T>
+void PrintProfile(std::ostream &p_stream,
 		  const Gambit::MixedStrategyProfile<T> &p_profile)
 {
   p_stream << "NE,";
@@ -43,7 +113,7 @@ void PrintProfile(std::ostream &p_stream,
   p_stream << std::endl;
 }
 
-void Solve(Gambit::Game p_nfg)
+void SolveMixed(Gambit::Game p_nfg)
 {
   Gambit::StrategyIterator citer(p_nfg);
 
@@ -82,10 +152,11 @@ void Solve(Gambit::Game p_nfg)
   }  while (citer.NextContingency());
 }
 
+
 void PrintBanner(std::ostream &p_stream)
 {
   p_stream << "Search for Nash equilibria in pure strategies\n";
-  p_stream << "Gambit version " VERSION ", Copyright (C) 2005, The Gambit Project\n\n";
+  p_stream << "Gambit version " VERSION ", Copyright (C) 2005, The Gambit Project\n";
   p_stream << "This is free software, distributed under the GNU GPL\n\n";
 }
 
@@ -93,23 +164,28 @@ void PrintHelp(char *progname)
 {
   PrintBanner(std::cerr);
   std::cerr << "Usage: " << progname << " [OPTIONS]\n";
-  std::cerr << "Accepts strategic game on standard input.\n";
+  std::cerr << "Accepts game on standard input.\n";
   std::cerr << "With no options, locates all Nash equilibria in pure strategies.\n\n";
 
   std::cerr << "Options:\n";
+  std::cerr << "  -S               use strategic game\n";
   std::cerr << "  -h               print this help message\n";
   std::cerr << "  -q               quiet mode (suppresses banner)\n";
   exit(1);
 }
 
+
 int main(int argc, char *argv[])
 {
   opterr = 0;
-  bool quiet = false;
+  bool quiet = false, useStrategic = false;
 
   int c;
-  while ((c = getopt(argc, argv, "hq")) != -1) {
+  while ((c = getopt(argc, argv, "hqS")) != -1) {
     switch (c) {
+    case 'S':
+      useStrategic = true;
+      break;
     case 'h':
       PrintHelp(argv[0]);
       break;
@@ -133,20 +209,27 @@ int main(int argc, char *argv[])
     PrintBanner(std::cerr);
   }
 
-  Gambit::Game nfg;
-
   try {
-    nfg = Gambit::ReadGame(std::cin);
+    Gambit::Game game = Gambit::ReadGame(std::cin);
+    if (!game->IsTree() || useStrategic) {
+      SolveMixed(game);
+    }
+    else {
+      SolveBehav(game);
+    }
+    return 0;
   }
-  catch (...) {
+  catch (Gambit::InvalidFileException) {
+    std::cerr << "Error: Game not in a recognized format.\n";
     return 1;
   }
-
-  Solve(nfg);
-
-  return 0;
-
+  catch (...) {
+    std::cerr << "Error: An internal error occurred.\n";
+    return 1;
+  }
 }
+
+
 
 
 
