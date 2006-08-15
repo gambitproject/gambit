233daf99b753d6180797d71a07b0b0f43ff38da5
drarbiter <drarbiter@3fbfd45b-e82e-0410-bb8e-8470640bbd7f>
2006-08-15 22:57:38 +0000

    Tidying up documentation and refactoring LP solver to make it easier to drop in another LP solver

--- sources/tools/lp/nfglp.cc
+++ sources/tools/lp/nfglp.cc
@@ -30,11 +30,60 @@
 #include "libgambit/libgambit.h"
 #include "lpsolve.h"
 
+using namespace Gambit;
+
 extern int g_numDecimals;
+
+//
+// The routine to actually solve the LP
+// This routine takes an LP of the form
+//    maximize c x subject to Ax>=b and x>=0,
+// except the last 'nequals' constraints in A hold with equality.
+// It expects the array p_primal to be the same length as the
+// number of columns in A, and the routine returns the primal solution;
+// similarly, the array p_dual should have the same length as the
+// number of rows in A, and the routine returns the dual solution.
+//
+// To implement your own custom solver for this problem, simply
+// replace this function.
+//
+template <class T> bool
+SolveLP(const Matrix<T> &A, const Vector<T> &b, const Vector<T> &c,
+	int nequals,
+	Array<T> &p_primal, Array<T> &p_dual)
+{
+  LPSolve<T> LP(A, b, c, nequals);
+  if (!LP.IsAborted()) {
+    BFS<T> cbfs((T) 0);
+    LP.OptBFS(cbfs);
+
+    for (int i = 1; i <= A.NumColumns(); i++) {
+      if (cbfs.IsDefined(i)) {
+	p_primal[i] = cbfs(i);
+      }
+      else {
+	p_primal[i] = (T) 0;
+      }
+    }
+
+    for (int i = 1; i <= A.NumRows(); i++) {
+      if (cbfs.IsDefined(-i)) {
+	p_dual[i] = cbfs(-i);
+      }
+      else {
+	p_dual[i] = (T) 0;
+      }
+    }
+    return true;
+  }
+  else {
+    return false;
+  }
+}
 
 void PrintProfile(std::ostream &p_stream,
 		  const std::string &p_label,
-		  const Gambit::MixedStrategyProfile<double> &p_profile)
+		  const MixedStrategyProfile<double> &p_profile)
 {
   p_stream << p_label;
   for (int i = 1; i <= p_profile.Length(); i++) {
@@ -46,7 +95,7 @@ void PrintProfile(std::ostream &p_stream,
 
 void PrintProfile(std::ostream &p_stream,
 		  const std::string &p_label,
-		  const Gambit::MixedStrategyProfile<Gambit::Rational> &p_profile)
+		  const MixedStrategyProfile<Rational> &p_profile)
 {
   p_stream << p_label;
   for (int i = 1; i <= p_profile.Length(); i++) {
@@ -57,132 +106,78 @@ void PrintProfile(std::ostream &p_stream,
   p_stream << std::endl;
 }
 
-template <class T> class nfgLp {
-private:
-  int Add_BFS(const Gambit::StrategySupport &, /*const*/ LPSolve<T> &B,
-	      Gambit::List<BFS<T> > &);
-  void GetSolutions(const Gambit::StrategySupport &, const Gambit::List<BFS<T> > &,
-		    const T &) const;
 
-public:
-  nfgLp(void);
-  virtual ~nfgLp() { }
-
-  void Solve(const Gambit::StrategySupport &);
-};
-
-//-------------------------------------------------------------------------
-//                      nfgLp<T>: Member functions
-//-------------------------------------------------------------------------
-
-template <class T> nfgLp<T>::nfgLp(void)
-{ }
-
-template <class T> 
-void nfgLp<T>::Solve(const Gambit::StrategySupport &p_support)
-{
-  BFS<T> cbfs((T) 0);
-  
-  if (p_support.GetGame()->NumPlayers() != 2 || 
-      !p_support.GetGame()->IsConstSum()) {
-    return; 
-  }
-  
-  Gambit::List<BFS<T> > bfsList;
-  
-  int i,j,m,k;
-  
-  m = p_support.NumStrategies(1);
-  k = p_support.NumStrategies(2);
-  Gambit::Matrix<T> A(1,k+1,1,m+1);
-  Gambit::Vector<T> b(1,k+1);
-  Gambit::Vector<T> c(1,m+1);
-  Gambit::PureStrategyProfile profile(p_support.GetGame());
-
-  T minpay = p_support.GetGame()->GetMinPayoff() - Gambit::Rational(1);
-
-  for (i = 1; i <= k; i++)  {
-    profile.SetStrategy(p_support.GetStrategy(2, i));
-    for (j = 1; j <= m; j++)  {
-      profile.SetStrategy(p_support.GetStrategy(1, j));
-      A(i, j) = Gambit::Rational(minpay) - profile.GetPayoff<Gambit::Rational>(1);
-    }
-    A(i,m+1) = (T)1;
-  }
-  for (j = 1;j<=m;j++)  {
-    A(k+1,j)= (T)1;
-  }
-  A(k+1,m+1) = (T)0;
-
-  b = (T)0;
-  b[k+1] = (T)1;
-  c = (T)0;
-  c[m+1] = (T)1;
-
-  LPSolve<T> LP(A,b,c,1);
-
-  if (!LP.IsAborted()) {
-    Add_BFS(p_support, LP, bfsList); 
-  }
-
-  GetSolutions(p_support, bfsList, LP.Epsilon());
-}
-
-template <class T> int nfgLp<T>::Add_BFS(const Gambit::StrategySupport &p_support,
-					 /*const*/ LPSolve<T> &lp,
-					 Gambit::List<BFS<T> > &p_list)
-{
-  BFS<T> cbfs((T) 0);
-
-  // LPSolve<T>::GetAll() does not currently work correctly; for now,
-  // LpSolve is restricted to returning only one equilibrium
-  lp.OptBFS(cbfs);
-  cbfs.Remove(p_support.GetGame()->GetPlayer(1)->NumStrategies()+1);
-  cbfs.Remove(-p_support.GetGame()->GetPlayer(2)->NumStrategies()-1);
-  if (p_list.Contains(cbfs))  return 0;
-  p_list.Append(cbfs);
-  return 1;
-}
+//
+// Convert the LP solution represented by the vectors
+// (p_primal, p_dual) back to a strategy profile.
+// The primal vector is the mixed strategy for player 1, and the
+// dual the mixed strategy for player 2.
+//
 
 template <class T>
-void nfgLp<T>::GetSolutions(const Gambit::StrategySupport &p_support,
-			    const Gambit::List<BFS<T> > &p_list,
-			    const T &p_epsilon) const
+void PrintSolution(const StrategySupport &p_support,
+		   const Array<T> &p_primal,
+		   const Array<T> &p_dual)
 {
   int n1 = p_support.NumStrategies(1);
   int n2 = p_support.NumStrategies(2);
 
-  for (int i = 1; i <= p_list.Length(); i++)    {
-    Gambit::MixedStrategyProfile<T> profile(p_support);
-    int j;
-    for (j = 1; j <= n1; j++) {
-      if (p_list[i].IsDefined(j)) {
-	profile[p_support.GetStrategy(1, j)] = p_list[i](j);
-      }
-      else {
-	profile[p_support.GetStrategy(1, j)] = (T) 0;
-      }
-    }
+  MixedStrategyProfile<T> profile(p_support);
 
-    for (j = 1; j <= n2; j++) {
-      if (p_list[i].IsDefined(-j)) {
-	profile[p_support.GetStrategy(2, j)] = p_list[i](-j);
-      }
-      else {
-	profile[p_support.GetStrategy(2, j)] = (T) 0;
-      }
-    }
+  for (int j = 1; j <= n1; j++) {
+    profile[p_support.GetStrategy(1, j)] = p_primal[j];
+  }
 
-    PrintProfile(std::cout, "NE", profile);
+  for (int j = 1; j <= n2; j++) {
+    profile[p_support.GetStrategy(2, j)] = p_dual[j];
+  }
+
+  PrintProfile(std::cout, "NE", profile);
+}
+
+
+//
+// Compute and print one equilibrium by solving a linear program based
+// on the strategic game representation.
+//
+template <class T>
+void SolveStrategic(const Game &p_game)
+{
+  StrategySupport support(p_game);
+
+  int m = support.NumStrategies(1);
+  int k = support.NumStrategies(2);
+
+  Matrix<T> A(1,k+1,1,m+1);
+  Vector<T> b(1,k+1);
+  Vector<T> c(1,m+1);
+  PureStrategyProfile profile(support.GetGame());
+
+  T minpay = p_game->GetMinPayoff() - Rational(1);
+
+  for (int i = 1; i <= k; i++)  {
+    profile.SetStrategy(support.GetStrategy(2, i));
+    for (int j = 1; j <= m; j++)  {
+      profile.SetStrategy(support.GetStrategy(1, j));
+      A(i, j) = Rational(minpay) - profile.GetPayoff<Rational>(1);
+    }
+    A(i,m+1) = (T) 1;
+  }
+  for (int j = 1; j <= m; j++) {
+    A(k+1,j) = (T) 1;
+  }
+  A(k+1,m+1) = (T) 0;
+
+  b = (T) 0;
+  b[k+1] = (T) 1;
+  c = (T) 0;
+  c[m+1] = (T) 1;
+  
+  Array<T> primal(A.NumColumns()), dual(A.NumRows());
+  if (SolveLP(A, b, c, 1, primal, dual)) {
+    PrintSolution(support, primal, dual);
   }
 }
 
-template <class T>
-void SolveStrategic(const Gambit::Game &p_game)
-{
-  nfgLp<T> algorithm;
-  algorithm.Solve(p_game);
-}
-
-template void SolveStrategic<double>(const Gambit::Game &);
-template void SolveStrategic<Gambit::Rational>(const Gambit::Game &);
+template void SolveStrategic<double>(const Game &);
+template void SolveStrategic<Rational>(const Game &);
