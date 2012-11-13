5e43124e59b4e34656e446f70fec4707c20a7f0e
Ted Turocy <ted.turocy@gmail.com>
2012-11-13 12:02:14 +0000

    Update gambit-shell embedding for IPython 0.13.

--- src/python/gambit/__init__.py
+++ src/python/gambit/__init__.py
@@ -1,5 +1,7 @@
 import gambit.lib.libgambit
+import gte
 
 new_tree = gambit.lib.libgambit.new_tree
 new_table = gambit.lib.libgambit.new_table
 read_game = gambit.lib.libgambit.read_game
+read_game_xml = gte.read_game_xml
