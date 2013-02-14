93a6eb84ab8059fd4cc4e150423ce21ba0c17a08
Haojian Wu <hokein.wu@gmail.com>
2013-02-14 10:57:29 +0800

    Extend Outcome object to access payoffs by text string and player object.

--- src/python/gambit/tests/test_outcomes.py
+++ src/python/gambit/tests/test_outcomes.py
@@ -5,6 +5,12 @@ import warnings
 class TestGambitOutcomes(object):
     def setUp(self):
         self.game = gambit.new_table([2,2])
+        self.game.players[0].label = "joe"
+        self.game.players[1].label = "dan"
+        self.game.outcomes[0][0] = 1
+        self.game.outcomes[0][1] = 2
+        self.game.outcomes[1][0] = 3
+        self.game.outcomes[1][1] = 4
     
     def tearDown(self):
         del self.game
@@ -47,6 +53,20 @@ class TestGambitOutcomes(object):
     def test_game_ouctome_index_exception_invalid_input(self):
         "Test to verify when attempting to retrieve an outcome with invalid input"
         assert_raises(TypeError, self.game.outcomes.__getitem__, 1.3)
+
+    def test_getting_payoff_by_label_string(self):
+        assert(self.game.outcomes[0]['joe'] == 1)
+        assert(self.game.outcomes[0]['dan'] == 2)
+        assert(self.game.outcomes[1]['joe'] == 3)
+        assert(self.game.outcomes[1]['dan'] == 4)
+
+    def test_getting_payoff_by_player(self):
+        player1 = self.game.players[0]
+        player2 = self.game.players[1]
+        assert(self.game.outcomes[0][player1] == 1)
+        assert(self.game.outcomes[0][player2] == 2)
+        assert(self.game.outcomes[1][player1] == 3)
+        assert(self.game.outcomes[1][player2] == 4)
 
     def test_outcome_delete(self):
         "Test to ensure it is possible to delete an outcome from the game"
