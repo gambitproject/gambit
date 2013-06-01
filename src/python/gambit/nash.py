ae0637cbf47c14839e82758fe1b1fa63991961f4
Albert Xin Jiang <albertjiang@gmail.com>
2013-06-01 14:29:57 -0700

    nash.py: fix runtime error with rational not assigned
    Now the external solvers should work.

--- src/python/gambit/nash.py
+++ src/python/gambit/nash.py
@@ -132,7 +132,7 @@ class ExternalGlobalNewtonSolver(ExternalSolver):
     def solve(self, game):
         command_line = "gambit-gnm -d 10"
         return self._parse_output(self.launch(command_line, game),
-                                  game, rational)
+                                  game, rational=False)
 
 class ExternalEnumPolySolver(ExternalSolver):
     """
@@ -144,7 +144,7 @@ class ExternalEnumPolySolver(ExternalSolver):
         if use_strategic and game.is_tree:
             command_line += " -S"
         return self._parse_output(self.launch(command_line, game),
-                                  game, rational,
+                                  game, rational=False,
                                   extensive=game.is_tree and not use_strategic)
 
 class ExternalLyapunovSolver(ExternalSolver):
@@ -157,7 +157,7 @@ class ExternalLyapunovSolver(ExternalSolver):
         if use_strategic and game.is_tree:
             command_line += " -S"
         return self._parse_output(self.launch(command_line, game),
-                                  game, rational,
+                                  game, rational=False,
                                   extensive=game.is_tree and not use_strategic)
 
 class ExternalIteratedPolymatrixSolver(ExternalSolver):
@@ -168,7 +168,7 @@ class ExternalIteratedPolymatrixSolver(ExternalSolver):
     def solve(self, game):
         command_line = "gambit-ipa -d 10"
         return self._parse_output(self.launch(command_line, game),
-                                  game, rational)
+                                  game, rational=False)
 
 class ExternalLogitSolver(ExternalSolver):
     """
@@ -181,5 +181,5 @@ class ExternalLogitSolver(ExternalSolver):
         if use_strategic and game.is_tree:
             command_line += " -S"
         return self._parse_output(self.launch(command_line, game),
-                                  game, rational,
+                                  game, rational=False,
                                   extensive=game.is_tree and not use_strategic)
