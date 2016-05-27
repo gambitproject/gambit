ae6f43e00e455b84ba33721d6af27a1573a8ede8
Ted Turocy <ted.turocy@gmail.com>
2016-05-27 12:45:09 +0100

    Updated version number to 15.0.0.

--- src/python/setup.py
+++ src/python/setup.py
@@ -51,7 +51,7 @@ libgame = Extension("gambit.lib.libgambit",
                     include_dirs=[ "../..", ".." ] )
 
 setup(name="gambit",
-      version="15.0.0",
+      version="15.1.0",
       description="Software tools for game theory",
       author="Theodore Turocy",
       author_email="ted.turocy@gmail.com",
