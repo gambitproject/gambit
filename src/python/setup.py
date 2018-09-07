15e019e3f48c704aa49fdb37805009dc107b29c0
rhalbersma <rhalbersma@gmail.com>
2018-09-07 23:59:08 +0200

    use cythonize in setup.py

--- src/python/setup.py
+++ src/python/setup.py
@@ -23,14 +23,8 @@
 from setuptools import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
+from Cython.Build import cythonize
 
-# setuptools DWIM monkey-patch madness
-# http:#mail.python.org/pipermail/distutils-sig/2007-September/thread.html#8204
-import sys
-if 'setuptools.extension' in sys.modules:
-    m = sys.modules['setuptools.extension']
-    m.Extension.__dict__ = m._Extension.__dict__
-    
 import glob
 libgame = Extension("gambit.lib.libgambit",
                     sources=[ "gambit/lib/libgambit.pyx" ] +
@@ -53,7 +47,7 @@ setup(name="gambit",
       author_email="ted.turocy@gmail.com",
       url="http://www.gambit-project.org",
       packages=['gambit', 'gambit.games', 'gambit.lib'],
-      ext_modules=[libgame],
+      ext_modules=cythonize([libgame]),
       cmdclass = {'build_ext': build_ext},
       entry_points="""
       [console_scripts]
