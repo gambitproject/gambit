d9c671bc1010032ca5adc6763196bb3b2928dbe3
Ted Turocy <ted.turocy@gmail.com>
2023-09-29 13:34:39 +0100

    Explicitly set Cython language level in setup.py

--- setup.py
+++ setup.py
@@ -101,5 +101,7 @@ setuptools.setup(
     libraries=[cppgambit, lrslib],
     package_dir={'': 'src'},
     packages=['pygambit', 'pygambit.games'],
-    ext_modules=Cython.Build.cythonize(libgambit, compiler_directives={'binding': True})
+    ext_modules=Cython.Build.cythonize(libgambit,
+                                       language_level="3str",
+                                       compiler_directives={'binding': True})
 )
