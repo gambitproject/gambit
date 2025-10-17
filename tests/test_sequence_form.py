e203b961d9c63e4caf5d81b9b2dc228ed041dad7
StephenPasteris <stephen.pasteris@gmail.com>
2025-10-17 17:24:06 +0100

    added tester

--- tests/test_sequence_form.py
+++ tests/test_sequence_form.py
@@ -1,24 +1,56 @@
 import pygambit as gbt
+import numpy as np
 
+def test_sequence_form(game, correspondence, payoffs):
+    test_object = TestSequenceForm(game, correspondence, payoffs)
+    test_object.check_subtensor(0)
 
-def check_subtensor(game, payoffs, seq_dict, player_index, sequence_form):
-    if player_index == len(game.players):
-        for player in game.players:
-            assert sequence_form.get_payoff(seq_dict, player) == payoffs[player.label]
-    else:
-        player = game.players[player_index]
-        seq_dict[player] = None
-        sub_payoffs = payoffs[None]
-        check_subtensor(game, sub_payoffs, seq_dict, player_index + 1, sequence_form)
+class TestSequenceForm:
+
+    def __init__(self, game, correspondence, payoffs):
+        self.correspondence = correspondence
+        self.payoffs = payoffs
+        self.sequence_form = gbt.GameSequenceForm(game)
+        self.players = game.players
+        self.seq_dict = {}
+        self.seq_idx_list = [0]*len(self.players)
+
+    def test(self):
+        self.check_subtensor(0)
+
+    def get_seq_idx(self, player_idx, action):
+        for i in range(len(self.correspondence[player_idx])):
+            if action.label == self.correspondence[player_idx][i]:
+                return i + 1
+        return None
+
+    def check_component(self):
+        for player_idx in range(len(self.players)):
+            player = self.players[player_idx]
+            payoff_tensor = self.payoffs[player_idx]
+            assert self.sequence_form.get_payoff(self.seq_dict, player) == payoff_tensor[tuple(self.seq_idx_list)]
+
+    def check_subtensor(self, player_idx):
+        if player_idx == len(self.players):
+            self.check_component()
+        else:
+            self.check_empty_sequence_subtensor(player_idx)
+            self.check_non_empty_sequence_subtensors(player_idx)
+
+    def check_empty_sequence_subtensor(self, player_idx):
+        player = self.players[player_idx]
+        self.seq_dict[player] = None
+        self.seq_idx_list[player_idx] = 0
+        self.check_subtensor(player_idx + 1)
+
+    def check_non_empty_sequence_subtensors(self,player_idx):
+        player = self.players[player_idx]
         for action in player.actions:
-            seq_dict[player] = action
-            sub_payoffs = payoffs[action.label]
-            check_subtensor(game, sub_payoffs, seq_dict, player_index + 1, sequence_form)
+            self.seq_dict[player] = action
+            seq_idx = self.get_seq_idx(player_idx, action)
+            self.seq_idx_list[player_idx] = seq_idx
+            self.check_subtensor(player_idx + 1)
 
-def test_sequence_form(game, payoffs):
-    sequence_form = gbt.GameSequenceForm(game)
-    seq_dict = {}
-    check_subtensor(game, payoffs, seq_dict, 0, sequence_form)
 
 
 g = gbt.Game.new_tree(
@@ -48,11 +80,10 @@ g.set_outcome(
     )
 )
 
-p1 = g.players[0]
-p2 = g.players[1]
+cor = [["A", "B"], []]
 
+buyer_payoffs = np.array([[0], [4], [4]])
+seller_payoffs = np.array([[0],[1],[1]])
+poffs = [buyer_payoffs, seller_payoffs]
 
-names = {p1 : ["A", "B"], p2 : []}
-pffs = {None: {None : {"Buyer" : 0, "Seller" : 0}}, "A" : {None : {"Buyer" : 4, "Seller" : 1}}, "B" : {None : {"Buyer" : 4, "Seller" : 1}}}
-
-test_sequence_form(g, pffs)
+test_sequence_form(g,cor,poffs)
