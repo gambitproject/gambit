7b3442dd092a556acc486547f7022b77e0d7cb68
Theodore Turocy <ted.turocy@gmail.com>
2025-10-27 16:20:26 +0000

    Wrote simple layout abstraction that at least compiles...

---
+++ src/gui/layout.cc
@@ -0,0 +1,78 @@
+//
+// This file is part of Gambit
+// Copyright (c) 1994-2025, The Gambit Project (https://www.gambit-project.org)
+//
+// FILE: src/gui/efglayout.cc
+// Implementation of tree layout representation
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+//
+
+#include <algorithm> // for std::min, std::max
+#include <numeric>   // for std::partial_sum
+
+#include "layout.h"
+
+namespace Gambit {
+
+void Layout::LayoutSubtree(const GameNode &p_node, const BehaviorSupportProfile &p_support,
+                           int p_level, double &p_offset)
+{
+  const auto entry = std::make_shared<LayoutEntry>(p_level);
+  m_nodeMap[p_node] = entry;
+  try {
+    entry->m_sublevel = m_infosetSublevels.at({entry->m_level, p_node->GetInfoset()});
+  }
+  catch (std::out_of_range &) {
+    entry->m_sublevel = ++m_numSublevels[entry->m_level];
+    m_infosetSublevels[{entry->m_level, p_node->GetInfoset()}] = entry->m_sublevel;
+  }
+
+  if (p_node->IsTerminal()) {
+    entry->m_offset = p_offset;
+    p_offset += 1;
+    return;
+  }
+  if (p_node->GetInfoset() && !p_node->GetInfoset()->GetPlayer()->IsChance()) {
+    const auto actions = p_support.GetActions(p_node->GetInfoset());
+    for (const auto &action : actions) {
+      LayoutSubtree(p_node->GetChild(action), p_support, p_level + 1, p_offset);
+    }
+    entry->m_offset = (m_nodeMap.at(p_node->GetChild(actions.front()))->m_offset +
+                       m_nodeMap.at(p_node->GetChild(actions.back()))->m_offset) /
+                      2;
+  }
+  else {
+    for (const auto &child : p_node->GetChildren()) {
+      LayoutSubtree(child, p_support, p_level + 1, p_offset);
+    }
+    entry->m_offset = (m_nodeMap.at(p_node->GetChildren().front())->m_offset +
+                       m_nodeMap.at(p_node->GetChildren().back())->m_offset) /
+                      2;
+  }
+}
+
+void Layout::LayoutTree(const BehaviorSupportProfile &p_support)
+{
+  m_nodeMap.clear();
+  m_maxLevel = 0;
+  m_numSublevels.clear();
+  m_infosetSublevels.clear();
+
+  double ycoord = 0;
+  LayoutSubtree(m_game->GetRoot(), p_support, 0, ycoord);
+}
+
+} // namespace Gambit
---
+++ src/gui/layout.h
@@ -0,0 +1,62 @@
+//
+// This file is part of Gambit
+// Copyright (c) 1994-2025, The Gambit Project (https://www.gambit-project.org)
+//
+// FILE: src/gui/efglayout.h
+// Interface to tree layout representation
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+//
+
+#ifndef GAMBIT_LAYOUT_H
+#define GAMBIT_LAYOUT_H
+
+#include "gambit.h"
+
+#include <map>
+
+namespace Gambit {
+struct LayoutEntry {
+  friend class Layout;
+  double m_offset{-1};        // Cartesian coordinates of node
+  int m_level, m_sublevel{0}; // depth of the node in tree
+  bool m_inSupport{true};
+
+  explicit LayoutEntry(int p_level) : m_level(p_level) {}
+};
+
+class Layout {
+  Game m_game;
+  std::map<GameNode, std::shared_ptr<LayoutEntry>> m_nodeMap;
+  std::vector<int> m_numSublevels;
+  std::map<std::pair<int, GameInfoset>, int> m_infosetSublevels;
+
+  float m_maxOffset{0};
+  int m_maxLevel{0};
+
+  void LayoutSubtree(const GameNode &, const BehaviorSupportProfile &, int, double &);
+
+public:
+  explicit Layout(const Game &p_game) : m_game(p_game) {}
+  ~Layout() = default;
+
+  void LayoutTree(const BehaviorSupportProfile &);
+
+  const std::map<GameNode, std::shared_ptr<LayoutEntry>> &GetNodeMap() const { return m_nodeMap; }
+  float GetMinOffset() const { return 0; }
+  float GetMaxOffset() const { return m_maxOffset; }
+};
+} // namespace Gambit
+#endif // GAMBIT_LAYOUT_H
