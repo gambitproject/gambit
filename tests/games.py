917630bd2f45159ca59cb49e6c955832fac8425d
StephenPasteris <stephen.pasteris@gmail.com>
2025-12-14 13:48:34 +0000

    added chance in middle game

--- tests/games.py
+++ tests/games.py
@@ -211,6 +211,47 @@ def create_three_action_internal_outcomes_efg() -> gbt.Game:
     return g
 
 
+def create_chance_in_middle_efg() -> gbt.Game:
+    g = gbt.Game.new_tree(players=["1", "2"],
+                          title="Chance in middle game")
+    g.append_move(g.root, "1", ["A", "B"])
+    g.append_move(g.root.children[0], g.players.chance, ["H", "L"])
+    g.set_chance_probs(g.root.children[0].infoset, ["1/5", "4/5"])
+    g.append_move(g.root.children[1], g.players.chance, ["H", "L"])
+    g.set_chance_probs(g.root.children[1].infoset, ["7/10", "3/10"])
+    g.set_outcome(g.root.children[0].children[0], g.add_outcome([-1, 1], label="a"))
+    for i in range(2):
+        g.append_move(g.root.children[0].children[i], "2", ["X", "Y"])
+        ist = g.root.children[0].children[i].infoset
+        g.append_infoset(g.root.children[1].children[i], ist)
+    for i in range(2):
+        for j in range(2):
+            g.append_move(g.root.children[i].children[0].children[j], "1", ["C", "D"])
+            ist = g.root.children[i].children[0].children[j].infoset
+            g.append_infoset(g.root.children[i].children[1].children[j], ist)
+    o_1 = g.add_outcome([1, -1], label="1")
+    o_m1 = g.add_outcome([-1, 1], label="-1")
+    o_h = g.add_outcome(["1/2", "-1/2"], label="0.5")
+    o_mh = g.add_outcome(["-1/2", "1/2"], label="-0.5")
+    g.set_outcome(g.root.children[0].children[0].children[0].children[0], o_1)
+    g.set_outcome(g.root.children[0].children[0].children[0].children[1], o_m1)
+    g.set_outcome(g.root.children[0].children[0].children[1].children[0], o_h)
+    g.set_outcome(g.root.children[0].children[0].children[1].children[1], o_mh)
+    g.set_outcome(g.root.children[0].children[1].children[0].children[0], o_h)
+    g.set_outcome(g.root.children[0].children[1].children[0].children[1], o_mh)
+    g.set_outcome(g.root.children[0].children[1].children[1].children[0], o_1)
+    g.set_outcome(g.root.children[0].children[1].children[1].children[1], o_m1)
+    g.set_outcome(g.root.children[1].children[0].children[0].children[0], o_h)
+    g.set_outcome(g.root.children[1].children[0].children[0].children[1], o_mh)
+    g.set_outcome(g.root.children[1].children[0].children[1].children[0], o_1)
+    g.set_outcome(g.root.children[1].children[0].children[1].children[1], o_m1)
+    g.set_outcome(g.root.children[1].children[1].children[0].children[0], o_1)
+    g.set_outcome(g.root.children[1].children[1].children[0].children[1], o_m1)
+    g.set_outcome(g.root.children[1].children[1].children[1].children[0], o_h)
+    g.set_outcome(g.root.children[1].children[1].children[1].children[1], o_mh)
+    return g
+
+
 def create_large_payoff_game_efg() -> gbt.Game:
     g = gbt.Game.new_tree(players=["1", "2"], title="Large payoff game")
     g.append_move(g.root, g.players.chance, ["L", "R"])
