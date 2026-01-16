fb66b81b9c0adf7242a7b4b9982e85a3903d4aae
Ed Chalstrey <edwardchalstrey@gmail.com>
2026-01-16 15:39:30 +0000

    add test_catalog_game_not_instantiable

--- tests/test_catalog.py
+++ tests/test_catalog.py
@@ -1,3 +1,5 @@
+import pytest
+
 from pygambit import Game
 from pygambit.catalog import CatalogGame, PrisonersDilemma
 
@@ -20,14 +22,13 @@ class TestGame(CatalogGame):
         )
 
 
-class TestCatalogGameBase:
+class TestCatalogGame:
     """Tests for CatalogGame base class and subclassing."""
 
-    # This test will only work if CatalogGame added to __init__.py
-    # def test_catalog_game_not_instantiable(self):
-    #     """CatalogGame should not be directly instantiable."""
-    #     with pytest.raises(NotImplementedError):
-    #         gbt.catalog.CatalogGame()
+    def test_catalog_game_not_instantiable(self):
+        """CatalogGame should not be directly instantiable."""
+        with pytest.raises(NotImplementedError):
+            CatalogGame()
 
     def test_custom_game_subclass_extracts_metadata(self):
         """Custom CatalogGame subclasses should extract metadata from _game()."""
