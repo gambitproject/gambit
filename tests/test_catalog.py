861da2c7552c0833671fb23594066d5b283ad0e8
Ed Chalstrey <edwardchalstrey@gmail.com>
2026-01-16 14:14:11 +0000

    initial catalog test ideas

---
+++ tests/test_catalog.py
@@ -0,0 +1,19 @@
+# import pytest
+
+from pygambit.catalog import OneShotTrust
+
+
+class TestCatalogGameBase:
+    """Tests for CatalogGame base class and subclassing."""
+
+    # This test will only work if CatalogGame added to __init__.py
+    # def test_catalog_game_not_instantiable(self):
+    #     """CatalogGame should not be directly instantiable."""
+    #     with pytest.raises(NotImplementedError):
+    #         gbt.catalog.CatalogGame()
+
+    def test_custom_game_subclass_extracts_metadata(self):
+        """Custom CatalogGame subclasses should extract metadata from _game()."""
+        assert OneShotTrust.num_players == 2
+        assert OneShotTrust.game_type == "efg"
+        assert OneShotTrust.title == "One-shot trust game, after Kreps (1990)"
