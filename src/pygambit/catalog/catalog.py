355ac3e1a9e96eb2ca371cfb6018ec41f5942527
Ed Chalstrey <edwardchalstrey@gmail.com>
2026-01-19 11:21:36 +0000

    move coded games to their own module

--- src/pygambit/catalog/catalog.py
+++ src/pygambit/catalog/catalog.py
@@ -5,7 +5,6 @@ from typing import Literal
 
 import yaml
 
-# import numpy as np
 from pygambit.gambit import Game, read_efg, read_nfg
 
 _GAMEFILES_DIR = Path(__file__).parent.parent.parent.parent / "contrib/games"
@@ -172,10 +171,6 @@ def games(
     return get_all_subclasses(CatalogGame)
 
 
-############################
-# Catalog games from files #
-############################
-
 _CATALOG_YAML = Path(__file__).parent / "catalog.yml"
 
 
@@ -228,37 +223,5 @@ def _generate_contrib_game_classes(catalog: dict[str, dict]) -> None:
 _catalog_data = _load_catalog_from_yaml(_CATALOG_YAML)
 _generate_contrib_game_classes(_catalog_data)
 
-##########################################
-# Catalog games defined programmatically #
-##########################################
-
-
-class OneShotTrust(CatalogGame):
-    """
-    The unique_NE_variant makes Trust a dominant strategy, replacing the
-    non-singleton equilibrium component from the standard version of the game
-    where the Buyer plays "Not Trust" and the seller can play any mixture with
-    < 0.5 probability on Honor with a unique NE where the Buyer plays Trust and
-    the Seller plays Abuse.
-    """
-    game_type = "efg"
-    # test_suite = True
-
-    @staticmethod
-    def _game(unique_NE_variant: bool = False):
-        g = Game.new_tree(
-            players=["Buyer", "Seller"], title="One-shot trust game, after Kreps (1990)"
-        )
-        g.append_move(g.root, "Buyer", ["Trust", "Not trust"])
-        g.append_move(g.root.children[0], "Seller", ["Honor", "Abuse"])
-        g.set_outcome(g.root.children[0].children[0], g.add_outcome([1, 1], label="Trustworthy"))
-        if unique_NE_variant:
-            g.set_outcome(
-                g.root.children[0].children[1], g.add_outcome(["1/2", 2], label="Untrustworthy")
-            )
-        else:
-            g.set_outcome(
-                g.root.children[0].children[1], g.add_outcome([-1, 2], label="Untrustworthy")
-            )
-        g.set_outcome(g.root.children[1], g.add_outcome([0, 0], label="Opt-out"))
-        return g
+# Import all coded games to ensure they are registered in the catalog
+from .coded_games import *  # noqa: E402, F403, F401
---
+++ src/pygambit/catalog/coded_games.py
@@ -0,0 +1,35 @@
+from pygambit.gambit import Game
+
+from .catalog import CatalogGame
+
+
+class OneShotTrust(CatalogGame):
+    """
+    The unique_NE_variant makes Trust a dominant strategy, replacing the
+    non-singleton equilibrium component from the standard version of the game
+    where the Buyer plays "Not Trust" and the seller can play any mixture with
+    < 0.5 probability on Honor with a unique NE where the Buyer plays Trust and
+    the Seller plays Abuse.
+    """
+
+    game_type = "efg"
+    # test_suite = True
+
+    @staticmethod
+    def _game(unique_NE_variant: bool = False):
+        g = Game.new_tree(
+            players=["Buyer", "Seller"], title="One-shot trust game, after Kreps (1990)"
+        )
+        g.append_move(g.root, "Buyer", ["Trust", "Not trust"])
+        g.append_move(g.root.children[0], "Seller", ["Honor", "Abuse"])
+        g.set_outcome(g.root.children[0].children[0], g.add_outcome([1, 1], label="Trustworthy"))
+        if unique_NE_variant:
+            g.set_outcome(
+                g.root.children[0].children[1], g.add_outcome(["1/2", 2], label="Untrustworthy")
+            )
+        else:
+            g.set_outcome(
+                g.root.children[0].children[1], g.add_outcome([-1, 2], label="Untrustworthy")
+            )
+        g.set_outcome(g.root.children[1], g.add_outcome([0, 0], label="Opt-out"))
+        return g
