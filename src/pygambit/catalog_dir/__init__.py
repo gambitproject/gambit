d16557ce89acec47ff4b20fd637eec23b2674e04
Ed Chalstrey <edwardchalstrey@gmail.com>
2026-01-21 17:21:10 +0000

    remove pointless init file

--- src/pygambit/catalog_dir/__init__.py
+++
@@ -1,9 +0,0 @@
-# from .catalog import CatalogGame, games
-
-# __all__ = ["games", "CatalogGame"]
-
-
-# def __getattr__(name: str):
-#     """Lazy load catalog games on access."""
-#     from . import catalog as _catalog_module
-#     return getattr(_catalog_module, name)
