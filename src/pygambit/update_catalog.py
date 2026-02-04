8cf0396977b3e148ebfc444126ff65de73c31ede
Ed Chalstrey <edwardchalstrey@gmail.com>
2026-02-04 13:52:17 +0000

    refactor update script so its run from repo root

--- src/pygambit/update_catalog.py
+++ src/pygambit/update_catalog.py
@@ -1,7 +1,7 @@
 import pygambit as gbt
 
-DOC = "../../doc"
+CATALOG_CSV = "doc/catalog.csv"
 
 if __name__ == "__main__":
     # Create CSV used by RST docs page
-    gbt.catalog.games().to_csv(DOC + "/catalog.csv", index=False)
+    gbt.catalog.games().to_csv(CATALOG_CSV, index=False)
