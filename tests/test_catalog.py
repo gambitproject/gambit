82e5154b5e6f682b387441b20909103ac79d4fac
Ed Chalstrey <edwardchalstrey@gmail.com>
2026-02-19 11:35:42 +0000

    add tests for filtering options based on game object attributes

--- tests/test_catalog.py
+++ tests/test_catalog.py
@@ -49,3 +49,91 @@ def test_catalog_games():
     assert "myerson_fig_4_2" not in slugs
     # Check family game present
     assert "one_shot_trust" in slugs
+
+
+def test_catalog_games_filter_n_actions():
+    """Test games() function can filter on length of gbt.Game attribute 'actions'"""
+    all_games = gbt.catalog.games()
+    filtered_games = gbt.catalog.games(n_actions=2)
+    assert len(filtered_games) < len(all_games)
+    assert all(len(g.actions) == 2 for g in filtered_games)
+
+
+def test_catalog_games_filter_n_contingencies():
+    """Test games() function can filter on length of gbt.Game attribute 'contingencies'"""
+    all_games = gbt.catalog.games()
+    filtered_games = gbt.catalog.games(n_contingencies=2)
+    assert len(filtered_games) < len(all_games)
+    assert all(len(g.contingencies) == 2 for g in filtered_games)
+
+
+def test_catalog_games_filter_n_info_sets():
+    """Test games() function can filter on length of gbt.Game attribute 'info_sets'"""
+    all_games = gbt.catalog.games()
+    filtered_games = gbt.catalog.games(n_info_sets=2)
+    assert len(filtered_games) < len(all_games)
+    assert all(len(g.info_sets) == 2 for g in filtered_games)
+
+
+def test_catalog_games_filter_is_constant_sum():
+    """Test games() function can filter on boolean gbt.Game attribute 'is_constant_sum'"""
+    all_games = gbt.catalog.games()
+    filtered_games = gbt.catalog.games(is_constant_sum=True)
+    assert len(filtered_games) < len(all_games)
+    assert all(filtered_games.is_constant_sum)
+
+
+def test_catalog_games_filter_is_perfect_recall():
+    """Test games() function can filter on boolean gbt.Game attribute 'is_perfect_recall'"""
+    all_games = gbt.catalog.games()
+    filtered_games = gbt.catalog.games(is_perfect_recall=True)
+    assert len(filtered_games) < len(all_games)
+    assert all(filtered_games.is_perfect_recall)
+
+
+def test_catalog_games_filter_is_tree():
+    """Test games() function can filter on boolean gbt.Game attribute 'is_tree'"""
+    all_games = gbt.catalog.games()
+    filtered_games = gbt.catalog.games(is_tree=True)
+    assert len(filtered_games) < len(all_games)
+    assert all(filtered_games.is_tree)
+
+
+def test_catalog_games_filter_min_payoff_and_max_payoff():
+    """Test games() function can filter on min and max payoff values"""
+    all_games = gbt.catalog.games()
+    filtered_games = gbt.catalog.games(min_payoff=0, max_payoff=10)
+    assert len(filtered_games) < len(all_games)
+    assert all((filtered_games.min_payoff >= 0) & (filtered_games.max_payoff <= 10))
+
+
+def test_catalog_games_filter_n_nodes():
+    """Test games() function can filter on length of gbt.Game attribute 'nodes'"""
+    all_games = gbt.catalog.games()
+    filtered_games = gbt.catalog.games(n_nodes=5)
+    assert len(filtered_games) < len(all_games)
+    assert all(len(g.nodes) == 5 for g in filtered_games)
+
+
+def test_catalog_games_filter_n_outcomes():
+    """Test games() function can filter on length of gbt.Game attribute 'outcomes'"""
+    all_games = gbt.catalog.games()
+    filtered_games = gbt.catalog.games(n_outcomes=3)
+    assert len(filtered_games) < len(all_games)
+    assert all(len(g.outcomes) == 3 for g in filtered_games)
+
+
+def test_catalog_games_filter_n_players():
+    """Test games() function can filter on length of gbt.Game attribute 'players'"""
+    all_games = gbt.catalog.games()
+    filtered_games = gbt.catalog.games(n_players=2)
+    assert len(filtered_games) < len(all_games)
+    assert all(len(g.players) == 2 for g in filtered_games)
+
+
+def test_catalog_games_filter_n_strategies():
+    """Test games() function can filter on length of gbt.Game attribute 'strategies'"""
+    all_games = gbt.catalog.games()
+    filtered_games = gbt.catalog.games(n_strategies=4)
+    assert len(filtered_games) < len(all_games)
+    assert all(len(g.strategies) == 4 for g in filtered_games)
