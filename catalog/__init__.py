0e81a040f70eda6110a9120f6913fceb001b9ddc
Ed Chalstrey <edwardchalstrey@gmail.com>
2026-03-24 15:39:55 +0000

    remove family games code

--- catalog/__init__.py
+++ catalog/__init__.py
@@ -43,11 +43,6 @@ def load(slug: str) -> gbt.Game:
             with as_file(resource_path) as path:
                 return reader(str(path))
 
-    # Try loading from family games
-    fg = family_games()
-    if slug in fg:
-        return fg[slug]
-
     # Raise error if game does not exist
     raise FileNotFoundError(f"No catalog entry called {slug}")
 
@@ -175,76 +170,8 @@ def games(
                 if check_filters(game):
                     append_record(slug, game)
 
-    # Add all the games from families
-    for slug, game in family_games().items():
-        # Throw an error if there's a slug collision between family games and file-based games
-        if slug in [r["Game"] for r in records]:
-            raise ValueError(
-                f"Slug collision: {slug} is present in both file-based and family games."
-            )
-        if check_filters(game):
-            append_record(slug, game)
-
     if include_descriptions:
         return pd.DataFrame.from_records(
             records, columns=["Game", "Title", "Description", "Download", "Format"]
         )
     return pd.DataFrame.from_records(records, columns=["Game", "Title"])
-
-
-def family_games() -> dict[str, gbt.Game]:
-    """
-    Generate a dict of games for inclusion in the catalog,
-    using the game families in this module.
-
-    Returns
-    -------
-    dict[str, gbt.Game]
-        A dictionary mapping slugs to game objects for family games.
-    """
-    return {
-        "one_shot_trust": one_shot_trust(),
-        "oneshot_trust_unique_NE": one_shot_trust(unique_NE_variant=True),
-    }
-
-
-################################################################################################
-# Families
-
-
-def one_shot_trust(unique_NE_variant: bool = False) -> gbt.Game:
-    """
-    The unique_NE_variant makes Trust a dominant strategy, replacing the
-    non-singleton equilibrium component from the standard version of the game
-    where the Buyer plays "Not Trust" and the seller can play any mixture with
-    < 0.5 probability on Honor with a unique NE where the Buyer plays Trust and
-    the Seller plays Abuse.
-
-    Parameters
-    ----------
-    unique_NE_variant : bool, optional
-        Whether to modify the game so that it has a unique Nash equilibrium.
-        Defaults to False.
-
-    Returns
-    -------
-    gbt.Game
-        The constructed extensive-form game.
-    """
-    g = gbt.Game.new_tree(players=["Buyer", "Seller"])
-    g.description = "One-shot trust game with binary actions, originally from [Kre90]_."
-    g.append_move(g.root, "Buyer", ["Trust", "Not trust"])
-    g.append_move(g.root.children[0], "Seller", ["Honor", "Abuse"])
-    g.set_outcome(g.root.children[0].children[0], g.add_outcome([1, 1], label="Trustworthy"))
-    if unique_NE_variant:
-        g.title = "One-shot trust game with unique NE"
-        g.set_outcome(
-            g.root.children[0].children[1], g.add_outcome(["1/2", 2], label="Untrustworthy")
-        )
-    else:
-        g.title = "One-shot trust game"
-        g.set_outcome(
-            g.root.children[0].children[1], g.add_outcome([-1, 2], label="Untrustworthy")
-        )
-    g.set_outcome(g.root.children[1], g.add_outcome([0, 0], label="Opt-out"))
-    return g
