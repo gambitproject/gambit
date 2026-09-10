6a865b8007057ce85765bf58e1656e51e4680a4f
Ted Turocy <ted.turocy@gmail.com>
2026-09-10 18:46:32 +0100

    Correct regression that was showing payoffs of 0 for null outcomes in extensive form

--- src/gui/efglayout.cc
+++ src/gui/efglayout.cc
@@ -339,7 +339,7 @@ void TreeLayout::DrawOutcome(wxDC &p_dc, const std::shared_ptr<NodeEntry> &p_ent
                              bool p_noHints) const
 {
   const GameOutcome outcome = p_entry->m_node->GetOutcome();
-  if (!outcome) {
+  if (outcome->IsNull()) {
     if (p_noHints) {
       return;
     }
@@ -389,7 +389,7 @@ void TreeLayout::ComputeOutcomeGeometry(wxDC &p_dc,
   wxPoint point(p_entry->GetX() + nodeSize + 20, p_entry->GetY());
 
   const GameOutcome outcome = p_entry->m_node->GetOutcome();
-  if (!outcome) {
+  if (outcome->IsNull()) {
     int width, height;
     p_dc.SetFont(wxFont(9, wxFONTFAMILY_SWISS, wxFONTSTYLE_ITALIC, wxFONTWEIGHT_BOLD));
     p_dc.GetTextExtent(wxT("(u)"), &width, &height);
